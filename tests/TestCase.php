0d15ea914f935f3afedc2522021d4fd0dc0c3462
Adam Boduch <adam@boduch.net>
2015-10-19 07:58:47 +0200

    psr-2

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -18,6 +18,7 @@ class TestCase extends Illuminate\Foundation\Testing\TestCase
     {
         $app = require __DIR__.'/../bootstrap/app.php';
         $app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
+
         return $app;
     }
 }
--- tests/acceptance/WelcomeCept.php
+++ tests/acceptance/WelcomeCept.php
@@ -1,6 +1,5 @@
-<?php 
+<?php
 $I = new AcceptanceTester($scenario);
 $I->wantTo('czy dziala strona glowna?');
 $I->amOnPage('/');
 $I->see('Co nowego na forum?');
-?>
