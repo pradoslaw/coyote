3fc1221f0783fcee006e0ab22502f88e9af293e0
Adam Boduch <adam@boduch.net>
2015-11-08 16:42:43 +0100

    ups, zapomnialem o "table" w nazwie

--- database/migrations/2015_11_04_195740_create_acl_data_table.php
+++ database/migrations/2015_11_04_195740_create_acl_data_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateAclData extends Migration
+class CreateAclDataTable extends Migration
 {
     /**
      * Run the migrations.
