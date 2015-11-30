3bbff47fd770c19ceef0525d4e7fd2e7a27f52d8
Adam Boduch <adam@boduch.net>
2015-11-30 22:38:54 +0100

    zapisanie czasu generowania dziennika aktywnosci

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace Coyote\Http\Controllers;
 
 use Coyote\Repositories\Contracts\MicroblogRepositoryInterface as Microblog;
 use Coyote\Stream\Stream;
+use Debugbar;
 
 class HomeController extends Controller
 {
@@ -11,8 +12,11 @@ class HomeController extends Controller
     {
         $viewers = app('Session\Viewers');
 
+        Debugbar::startMeasure('stream', 'Stream activities');
         // tymczasowo naglowki tylko dla mikroblogow, a nie dla forum
         $activities = $stream->take(10, 0, ['Microblog', 'Comment'], ['Create', 'Update']);
+        Debugbar::stopMeasure('stream');
+
         $microblogs = $microblog->take(10);
 
         return view('home', [
