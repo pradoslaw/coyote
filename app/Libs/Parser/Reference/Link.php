761a82e7f299882c7f56abbd603a4ff153b9a4c7
Adam Boduch <adam@boduch.net>
2015-12-03 11:29:15 +0100

    favicon

---
+++ app/Libs/Parser/Reference/Link.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Coyote\Parser\Reference;
+
+class Link
+{
+    public function filter($html)
+    {
+        $links = [];
+
+        $regexp = "<a\s[^>]*href=(\"??)([^\" >]*?)\\1[^>]*>(.*)<\/a>";
+        if (preg_match_all("/$regexp/siU", $html, $matches, PREG_SET_ORDER)) {
+            $links[] = $matches[2];
+        }
+
+        return array_unique($links);
+    }
+}
