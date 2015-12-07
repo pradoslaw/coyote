26136d5ab2e9efeb1712bafb41d18104ec566f56
Adam Boduch <adam@boduch.net>
2015-12-07 19:49:18 +0100

    zalazek strony umozliwiajacej ustawianie powiadomien oraz dostepow z okreslonych ip

---
+++ app/Http/Controllers/User/SecurityController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Coyote\Http\Controllers\User;
+
+use Coyote\Http\Controllers\Controller;
+use Coyote\User;
+use Illuminate\Http\Request;
+
+class SecurityController extends Controller
+{
+    /**
+     * @return $this
+     */
+    public function index()
+    {
+        $this->breadcrumb->push('Moje konto', route('user.home'));
+        $this->breadcrumb->push('Bezpieczeństwo', route('user.security'));
+
+
+
+        return parent::view('user.security');
+    }
+
+    /**
+     * @param Request $request
+     * @return $this
+     */
+    public function save(Request $request)
+    {
+
+    }
+}
