79c292e552d16b95236d645a115fe6fb16989aea
Adam Boduch <adam@boduch.net>
2015-12-15 18:01:08 +0100

    mozliwosc zwrocenia jedynie samego odmienionego slowa (bez liczebnika)

--- app/Libs/Declination/Declination.php
+++ app/Libs/Declination/Declination.php
@@ -9,19 +9,40 @@ namespace Coyote;
  */
 class Declination
 {
-    public static function format($value, $declination)
+    /**
+     * @param int $value
+     * @param array $declination
+     * @param bool|false $absolute True zwraca jedynie sam tekst bez liczebnika
+     * @return string
+     */
+    public static function format($value, $declination, $absolute = false)
     {
         if ($value == 1) {
-            return $value . ' ' . $declination[0];
+            return self::abs($value, $declination[0], $absolute);
         } else {
             $unit = $value % 10;
             $decimal = round(($value % 100) / 10);
 
             if (($unit == 2 || $unit == 3 || $unit == 4) && ($decimal != 1)) {
-                return $value . ' ' . $declination[1];
+                return self::abs($value, $declination[1], $absolute);
             } else {
-                return $value . ' ' . $declination[2];
+                return self::abs($value, $declination[2], $absolute);
             }
         }
+    }
+
+    /**
+     * @param int $value
+     * @param string $declination
+     * @param bool $flag
+     * @return string
+     */
+    private static function abs($value, $declination, $flag)
+    {
+        if ($flag) {
+            return $declination;
+        }
+
+        return $value . ' ' . $declination;
     }
 }
