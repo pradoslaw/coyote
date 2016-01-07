66d8a3162d985a57704e36d609ef061d140dd13a
Adam Boduch <adam@boduch.net>
2016-01-07 17:36:51 +0100

    zapis ustawien

--- app/Http/Controllers/User/SecurityController.php
+++ app/Http/Controllers/User/SecurityController.php
@@ -16,8 +16,6 @@ class SecurityController extends Controller
         $this->breadcrumb->push('Moje konto', route('user.home'));
         $this->breadcrumb->push('Bezpieczeństwo', route('user.security'));
 
-
-
         return parent::view('user.security');
     }
 
@@ -27,6 +25,12 @@ class SecurityController extends Controller
      */
     public function save(Request $request)
     {
-        //
+        $user = auth()->user();
+
+        $user->alert_login = (bool) $request->get('alert_login');
+        $user->alert_failure = (bool) $request->get('alert_failure');
+        $user->save();
+
+        return back()->with('success', 'Zmiany zostały poprawie zapisane');
     }
 }
