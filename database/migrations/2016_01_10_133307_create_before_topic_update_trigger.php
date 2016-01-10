11fe6b4dc03d8a7f849d276e846f5f57dfb9d733
Adam Boduch <adam@boduch.net>
2016-01-10 18:14:45 +0100

    refaktoryzacja

--- database/migrations/2016_01_10_133307_create_before_topic_update_trigger.php
+++ database/migrations/2016_01_10_133307_create_before_topic_update_trigger.php
@@ -14,27 +14,21 @@ class CreateBeforeTopicUpdateTrigger extends Migration
     {
         DB::unprepared('
 CREATE FUNCTION before_topic_update() RETURNS trigger LANGUAGE plpgsql AS $$
-DECLARE
-	postCount INTEGER;
 BEGIN
  	IF NEW.forum_id != OLD.forum_id THEN
  		NEW.prev_forum_id = OLD.forum_id;
 
  		UPDATE posts SET forum_id = NEW.forum_id WHERE topic_id = OLD."id";
- 		--UPDATE topic_track SET forum_id = NEW.forum_id WHERE topic_id = OLD."id";
- 		DELETE FROM topic_track WHERE topic_id = OLD."id";
+ 		UPDATE topic_track SET forum_id = NEW.forum_id WHERE topic_id = OLD."id";
+ 		--DELETE FROM topic_track WHERE topic_id = OLD."id";
 
- 		postCount = (SELECT COUNT("id") FROM posts WHERE topic_id = NEW."id" AND deleted_at IS NOT NULL);
+		UPDATE forums
+		SET topics = topics -1, posts = posts - (NEW.replies + 1), last_post_id = get_forum_last_post_id(OLD.forum_id)
+		WHERE "id" = OLD.forum_id;
 
-		IF OLD.deleted_at IS NULL THEN
-	 		UPDATE forums
-	 		SET topics = topics -1, posts = posts - postCount, last_post_id = get_forum_last_post_id(OLD.forum_id)
-	 		WHERE "id" = OLD.forum_id;
-
-	 		UPDATE forums
-	 		SET topics = topics +1, posts = posts + postCount, last_post_id = get_forum_last_post_id(NEW.forum_id)
-	 		WHERE "id" = NEW.forum_id;
-	 	END IF;
+		UPDATE forums
+		SET topics = topics +1, posts = posts + (NEW.replies + 1), last_post_id = get_forum_last_post_id(NEW.forum_id)
+		WHERE "id" = NEW.forum_id;
 
 		UPDATE post_votes SET forum_id = NEW.forum_id
 		WHERE post_id IN(
