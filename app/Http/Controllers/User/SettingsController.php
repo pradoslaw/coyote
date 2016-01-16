5737cf7598ad6b71c2f53fd6a855e295e18baf23
Adam Boduch <adam@boduch.net>
2016-01-16 12:43:12 +0100

    informacja o zapiscie danych laduje w stream activity

--- app/Http/Controllers/User/SettingsController.php
+++ app/Http/Controllers/User/SettingsController.php
@@ -4,6 +4,8 @@ namespace Coyote\Http\Controllers\User;
 
 use Coyote\Http\Controllers\Controller;
 use Coyote\Repositories\Contracts\SettingRepositoryInterface;
+use Coyote\Stream\Activities\Update;
+use Coyote\Stream\Objects\Person;
 use Coyote\User;
 use Coyote\Actkey;
 use Coyote\Group;
@@ -38,34 +40,37 @@ class SettingsController extends Controller
      */
     public function save(UserSettingsRequest $request)
     {
-        $user = auth()->user();
+        \DB::transaction(function () use ($request) {
+            $user = auth()->user();
 
-        if ($user->email !== $request->get('email')) {
-            $email = $request->get('email');
+            if ($user->email !== $request->get('email')) {
+                $email = $request->get('email');
 
-            // kasujemy poprzednie rekordu zwiazane z tym userem
-            Actkey::where('user_id', $user->id)->delete();
-            // przed zmiana e-maila trzeba wyslac link potwierdzajacy
-            $actkey = Actkey::create([
-                'actkey'   => str_random(),
-                'user_id'  => $user->id,
-                'email'    => $email
-            ]);
+                // kasujemy poprzednie rekordu zwiazane z tym userem
+                Actkey::where('user_id', $user->id)->delete();
+                // przed zmiana e-maila trzeba wyslac link potwierdzajacy
+                $actkey = Actkey::create([
+                    'actkey'   => str_random(),
+                    'user_id'  => $user->id,
+                    'email'    => $email
+                ]);
 
-            // taki format linku zachowany jest ze wzgledu na wsteczna kompatybilnosc.
-            // z czasem mozemy zmienic ten format aby wskazywal na /User/Confirm/Email/<id>/<actkey>
-            $url = route('user.email') . '?id=' . $user->id . '&actkey=' . $actkey->actkey;
+                // taki format linku zachowany jest ze wzgledu na wsteczna kompatybilnosc.
+                // z czasem mozemy zmienic ten format aby wskazywal na /User/Confirm/Email/<id>/<actkey>
+                $url = route('user.email') . '?id=' . $user->id . '&actkey=' . $actkey->actkey;
 
-            Mail::queue('emails.email', ['url' => $url], function ($message) use ($email) {
-                $message->to($email);
-                $message->subject('Prosimy o potwierdzenie nowego adresu e-mail');
-            });
+                Mail::queue('emails.email', ['url' => $url], function ($message) use ($email) {
+                    $message->to($email);
+                    $message->subject('Prosimy o potwierdzenie nowego adresu e-mail');
+                });
 
-            if ($user->is_confirm) {
-                $request['email'] = $user->email;
+                if ($user->is_confirm) {
+                    $request['email'] = $user->email;
+                }
             }
-        }
-        $user->fill($request->all())->save();
+            $user->fill($request->all())->save();
+            stream(Update::class, new Person());
+        });
 
         return back()->with('success', 'Zmiany zostały poprawie zapisane');
     }
