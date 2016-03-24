1866876652c1e13f359331ccf4b819c89fbd7ebf
adam-boduch <adam@boduch.net>
2016-03-24 18:59:09 +0100

    zmiana nazwy tabeli

--- database/migrations/2016_03_16_204538_create_job_subscribers_table.php
+++ database/migrations/2016_03_16_204538_create_job_subscribers_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateJobFavoritesTable extends Migration
+class CreateJobSubscribersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,7 +12,7 @@ class CreateJobFavoritesTable extends Migration
      */
     public function up()
     {
-        Schema::create('job_favorites', function (Blueprint $table) {
+        Schema::create('job_subscribers', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('job_id');
             $table->integer('user_id');
@@ -32,6 +32,6 @@ class CreateJobFavoritesTable extends Migration
      */
     public function down()
     {
-        Schema::drop('job_favorites');
+        Schema::drop('job_subscribers');
     }
 }
