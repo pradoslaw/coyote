41ffa4c144b3615cdd6acc00c5d99a87b020427f
adam-boduch <adam@boduch.net>
2016-04-11 19:37:56 +0200

    dodawanie uprawnien

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -24,6 +24,7 @@ class DatabaseSeeder extends Seeder
         $this->call(MicroblogsTableSeeder::class);
         $this->call(AlertTypesTableSeeder::class);
         $this->call(ForumsTableSeeder::class);
+        $this->call(JobsTableSeeder::class);
         $this->call(FlagTypesSeeder::class);
         $this->call(ForumReasonsTableSeeder::class);
         $this->call(CurrenciesTableSeeder::class);
---
+++ database/seeds/JobsTableSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class JobsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $permissions = [           
+            [
+                'name' => 'job-update',
+                'description' => 'Edycja ofert pracy',
+                'default' => false
+            ],
+            [
+                'name' => 'job-delete',
+                'description' => 'Usuwanie ofert pracy',
+                'default' => false
+            ],
+            [
+                'name' => 'firm-update',
+                'description' => 'Edycja firm',
+                'default' => false
+            ],
+            [
+                'name' => 'firm-delete',
+                'description' => 'Usuwanie firm',
+                'default' => false
+            ],
+        ];
+
+        foreach ($permissions as $permission) {
+            \DB::table('permissions')->insert($permission);
+        }
+    }
+}
