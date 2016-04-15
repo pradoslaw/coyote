f97c10396ecfec32028b71a9bd56342b36d7d106
adam-boduch <adam@boduch.net>
2016-04-15 17:47:35 +0200

    wydzielenie geshi do osobnego repozytorium

--- app/Libs/Parser/Providers/Geshi.php
+++ app/Libs/Parser/Providers/Geshi.php
@@ -18,7 +18,7 @@ class Geshi implements ProviderInterface
 
         $alias = ['html' => 'html5'];
 
-        $geshi = new \Coyote\Parser\geshi('', '');
+        $geshi = new \Boduch\Geshi\Geshi();
         $geshi->line_ending = "\n";
         /* tekst bedzie zawarty w	znaczniku <pre>	*/
         $geshi->set_header_type(GESHI_HEADER_NONE);
