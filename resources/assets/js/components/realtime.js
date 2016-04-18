bd6542910b9740624776518c275861d07d22ec88
adam-boduch <adam@boduch.net>
2016-04-18 18:23:14 +0200

    literowka w nazwie

--- resources/assets/js/components/realtime.js
+++ resources/assets/js/components/realtime.js
@@ -3,16 +3,16 @@ function Realtime() {
 
     var self = this;
     var handler = null;
-    this._callbaks = {};
+    this._callbacks = {};
 
     this.on = function(event, fn) {
-        (self._callbaks[event] = self._callbaks[event] || []).push(fn);
+        (self._callbacks[event] = self._callbacks[event] || []).push(fn);
         return self;
     };
 
     this.emit = function(event, data) {
-        if (self._callbaks[event]) {
-            var callbacks = self._callbaks[event].slice(0);
+        if (self._callbacks[event]) {
+            var callbacks = self._callbacks[event].slice(0);
             for (var i = 0, len = callbacks.length; i < len; ++i) {
                 callbacks[i].apply(this, [data]);
             }
