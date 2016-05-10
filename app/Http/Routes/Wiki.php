9b4c729b92f4a070eabc5accdd08131e56c81af0
adam-boduch <adam@boduch.net>
2016-05-10 16:52:55 +0200

    wyswietlanie info o autorze

--- app/Http/Routes/Wiki.php
+++ app/Http/Routes/Wiki.php
@@ -7,5 +7,6 @@ Route::get('/Delphi', ['as' => 'page', 'uses' => 'Wiki\WikiController@category']
 Route::get('/Delphi/Lorem_ipsum', ['as' => 'article', 'uses' => 'Wiki\WikiController@article']);
 
 Route::group(['namespace' => 'Wiki', 'prefix' => '', 'as' => 'wiki.'], function () {
+    Route::get('Edit/{slug}', ['as' => 'submit', 'uses' => 'SubmitController@index', 'middleware' => ['auth', 'wiki.exists']])->where('slug', '.*');
     Route::get('{slug}', ['as' => 'show', 'uses' => 'ShowController@index', 'middleware' => 'wiki.exist'])->where('slug', '.*');
 });
