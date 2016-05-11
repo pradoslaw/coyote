bdb55286e37ca7bae58c75f3fc2ce2db4bc1fc84
adam-boduch <adam@boduch.net>
2016-05-11 08:16:01 +0200

    w przypadku przyklejonego paska menu, musimy przewinac do wlasciwego elementu strony gdy obecny jest window.location.hash

--- resources/assets/js/main.js
+++ resources/assets/js/main.js
@@ -6,4 +6,16 @@ $(function() {
             'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
         }
     });
+
+    // if navbar class navbar-fixed-top class, we must scroll to appropriate page element according to
+    // window.location.hash.
+    $(window).load(function() {
+        if (window.location.hash !== '' && $('header.navbar-default').hasClass('navbar-fixed-top')) {
+            var target = $(window.location.hash);
+
+            if (target.length) {
+                $('html, body').animate({ scrollTop: target.offset().top - 60 });
+            }
+        }
+    });
 });
