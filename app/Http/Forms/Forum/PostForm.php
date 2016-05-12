73bd084dca744868064918874a79f9b25491169d
adam-boduch <adam@boduch.net>
2016-05-12 23:20:38 +0200

    dodanie autofocus oraz poprawi w tresci pomocy

--- app/Http/Forms/Forum/PostForm.php
+++ app/Http/Forms/Forum/PostForm.php
@@ -236,6 +236,16 @@ class PostForm extends Form
                 'data-submit-state' => 'Zapisywanie...'
             ]
         ]);
+
+        $this->setAutofocus();
+    }
+
+    protected function setAutofocus()
+    {
+        // if subject is not present, set autofocus to textarea
+        if ($this->get('subject') === null) {
+            $this->get('text')->setAttr($this->get('text')->getAttr() + ['autofocus' => 'autofocus']);
+        }
     }
 
     /**
