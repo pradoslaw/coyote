44889d9c131dfc049400f52469810815575a7a60
adam-boduch <adam@boduch.net>
2016-05-12 18:34:47 +0200

    przeniesienie czesci kodu do bloku statycznego

--- database/seeds/BlocksTableSeeder.php
+++ database/seeds/BlocksTableSeeder.php
@@ -55,5 +55,70 @@ EOF;
             'name' => 'job_ads',
             'content' => sprintf($content, route('job.home', [], false))
         ]);
+
+        $content = <<<EOF
+<div class="row max-width">
+    <div class="col-md-2 col-sm-4">
+        <ul>
+            <li><h6>Programowanie</h6></li>
+            <li><a title="Programowanie w Delphi. Kurs programowania oraz encyklopedia biblioteki VCL" href="/Delphi">Delphi</a></li>
+            <li><a title="Zagadnienia z dziedziny programowania w C#" href="/C_sharp">C#</a></li>
+            <li><a title="Programowanie w językach C i C++" href="/C">C++</a></li>
+            <li><a title="Programowanie Java" href="/Java">Java</a></li>
+            <li><a title="Wszystko o XHTML oraz HTML" href="/(X)HTML">(X)HTML</a></li>
+        </ul>
+    </div>
+
+    <div class="col-md-2 col-sm-4">
+        <ul>
+            <li><h6><a title="Forum dla programistów" href="/Forum">Forum dyskusyjne</a></h6></li>
+            <li><a title="Forum dla początkujących programistów" href="/Forum/Newbie">Newbie</a></li>
+            <li><a title="Forum dla programistów Javy" href="/Forum/Java">Java</a></li>
+            <li><a title="Programowanie w C oraz C++" href="/Forum/C_i_C++">C/C++</a></li>
+            <li><a title="Kariera programisty" href="/Forum/Kariera">Kariera</a></li>
+            <li><a title="Praca dla programisty" href="/Forum/Edukacja">Edukacja</a></li>
+
+        </ul>
+    </div>
+
+    <div class="col-md-2 col-sm-4">
+        <ul>
+            <li><h6>Sprawy administracyjne</h6></li>
+            <li><a title="Jeżeli znalazłeś błąd w oprogramowaniu, tu jest odpowiednie miejsce, aby to zgłosić" href="/Forum/Coyote">Zgłoś błąd w oprogramowaniu</a></li>
+            <li><a title="4programmers.net na facebooku" href="https://www.facebook.com/4programmers.net">4programmers.net na facebooku</a></li>
+            <li><a title="Blog 4programmers.net" href="/Blog">Blog 4programmers.net</a></li>
+            <li><a title="Prawa autorskie" href="/Prawa_autorskie">Prawa autorskie</a></li>
+            <li><a title="Logo 4programmers.net" href="/Pomoc/Nasze_logo">Nasze logo</a></li>
+        </ul>
+    </div>
+
+    <div class="col-md-2 col-sm-4">
+        <ul>
+            <li><h6>O nas</h6></li>
+            <li><a title="Kontakt z serwisem 4programmers.net" href="/Kontakt">Kontakt</a></li>
+            <li><a title="Regulamin korzystania z serwisu 4programmers.net" href="/Regulamin">Regulamin</a></li>
+            <li><a title="Zareklamuj się w serwisie 4programmers.net" href="/Reklama">Reklama</a></li>
+            <li><a title="Pomoc w korzystaniu z serwisu 4programmers.net" href="/Pomoc">Pomoc</a></li>
+            <li><a title="Odpowiedzi na najczęściej zadawane pytania" href="/Patronat">Patronat</a></li>
+        </ul>
+    </div>
+
+    <div class=" col-sm-4">
+        <div class="footer-bubble">
+            <h6>Skontaktuj się z nami</h6>
+
+            <ol>
+                <li><a title="Skontaktuj się poprzez email" href="/Kontakt"><i class="fa fa-info-circle fa-fw"></i>  Napisz do nas</a></li>
+                <li><a title="Skontaktuj się poprzez email" href="https://www.facebook.com/4programmers.net"><i class="fa fa-facebook fa-fw"></i> Odwiedź nas na Facebooku</a></li>
+            </ol>
+        </div>
+    </div>
+</div>
+EOF;
+
+        \Coyote\Block::create([
+            'name' => 'footer',
+            'content' => $content
+        ]);
     }
 }
