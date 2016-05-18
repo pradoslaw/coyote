594edc142cbd54a7a0863a05c85e25f002c3481f
adam-boduch <adam@boduch.net>
2016-05-18 13:31:52 +0200

    przeniesienie czesci wspolnej dla widokow, do osobnego pliku twig

--- app/Http/Routes/Wiki.php
+++ app/Http/Routes/Wiki.php
@@ -1,11 +1,5 @@
 <?php
 
-/*
- * Tymczasowe reguly
- */
-Route::get('/Delphi', ['as' => 'page', 'uses' => 'Wiki\WikiController@category']);
-Route::get('/Delphi/Lorem_ipsum', ['as' => 'article', 'uses' => 'Wiki\WikiController@article']);
-
 Route::group(['namespace' => 'Wiki', 'prefix' => '', 'as' => 'wiki.'], function () {
     Route::get('Edit/{wiki?}', ['as' => 'submit', 'uses' => 'SubmitController@index', 'middleware' => ['auth', 'wiki.lock']]);
     Route::post('Edit/{wiki?}', ['uses' => 'SubmitController@save', 'middleware' => ['auth', 'wiki.lock']]);
