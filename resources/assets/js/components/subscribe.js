5bad5c6e84680d5c334a4240c53f981f670d539e
adam-boduch <adam@boduch.net>
2016-05-29 15:00:15 +0200

    w przypadku gdy uzytkownik nie jest zalogowany - wyswietlamy komunikat ze nie mozna dodac strony do obserwowanych

--- resources/assets/js/components/subscribe.js
+++ resources/assets/js/components/subscribe.js
@@ -4,10 +4,10 @@ $(() => {
     $('.btn-subscribe').click((e) => {
         let self = $(e.currentTarget);
 
-        self.toggleClass('on');
-        self.find('span').text(self.data(self.hasClass('on') ? 'off' : 'on'));
-
-        $.post(self.attr('href')).fail((e) => {
+        $.post(self.attr('href'), () => {
+            self.toggleClass('on');
+            self.find('span').text(self.data(self.hasClass('on') ? 'off' : 'on'));
+        }).fail((e) => {
             $('#modal-unauthorized').modal('show');
         });
 
