431c4707b4ababa3a77be56664112785b9d924fb
adam-boduch <adam@boduch.net>
2016-06-29 19:52:39 +0200

    drobna refaktoryzacja

--- app/Services/Reputation/Wiki/Update.php
+++ app/Services/Reputation/Wiki/Update.php
@@ -27,22 +27,32 @@ class Update extends Wiki
         $diff = $logs[0]->diff;
 
         if ($diff > 0) {
-            // roznica zmian w stosunku do poprzedniej wersji (w procentach)
-            $percentage = round($diff / $length * 100);
-            $value = $length > 1200 ? (min(25, max(1, $percentage))) : 1;
-
-            if ($this->isFlooded($logs)) {
-                $this->setValue($value);
-            } elseif ($length > 1200 && $percentage >= 10) {
-                $this->setValue(1);
-            } else {
-                $this->setValue(0);
-            }
+            $this->setupValue($logs, $diff, $length);
         } else {
             $this->setValue(null); // don't save reputation
         }
 
         return $this;
+    }
+
+    /**
+     * @param \Coyote\Wiki\Log[] $logs
+     * @param int $diff
+     * @param int $length
+     */
+    private function setupValue($logs, $diff, $length)
+    {
+        // roznica zmian w stosunku do poprzedniej wersji (w procentach)
+        $percentage = round($diff / $length * 100);
+        $value = $length > 1200 ? (min(25, max(1, $percentage))) : 1;
+
+        if ($this->isFlooded($logs)) {
+            $this->setValue($value);
+        } elseif ($length > 1200 && $percentage >= 10) {
+            $this->setValue(1);
+        } else {
+            $this->setValue(0);
+        }
     }
 
     /**
