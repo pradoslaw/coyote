6b7e997aed77b06efc6f51d52018510da56e0099
adam-boduch <adam@boduch.net>
2016-07-25 20:54:42 +0200

    zmiana rozmiaru formularza

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -2,10 +2,30 @@
 
 namespace Coyote\Http\Controllers;
 
+use Illuminate\Http\Request;
 use Lavary\Menu\Menu;
 
 class SearchController extends Controller
 {
+    /**
+     * @var Request
+     */
+    private $request;
+
+    /**
+     * SearchController constructor.
+     * @param Request $request
+     */
+    public function __construct(Request $request)
+    {
+        parent::__construct();
+
+        $this->request = $request;
+    }
+
+    /**
+     * @return \Illuminate\View\View
+     */
     public function index()
     {
         $this->breadcrumb->push('Szukaj', route('search'));
@@ -15,13 +35,25 @@ class SearchController extends Controller
         ]);
     }
 
+    /**
+     * @return mixed
+     */
     private function tabs()
     {
         return app(Menu::class)->make('tabs', function ($menu) {
-            $menu->add('Posty na forum', route('search', ['type' => 'posts']));
-            $menu->add('Oferty pracy', route('search', ['type' => 'offers']));
-            $menu->add('Mikroblogi', route('search', ['type' => 'microblogs']));
-            $menu->add('Artykuły', route('search', ['type' => 'wiki']));
+            $menu->add('Posty na forum', $this->route('posts'));
+            $menu->add('Oferty pracy', $this->route('offers'));
+            $menu->add('Mikroblogi', $this->route('microblogs'));
+            $menu->add('Artykuły', $this->route('wiki'));
         });
+    }
+
+    /**
+     * @param string$type
+     * @return string
+     */
+    private function route($type)
+    {
+        return route('search', ['type' => $type, 'q' => $this->request->input('q')]);
     }
 }
