f92b8322f0d100b1bd70cce421bb0ca26f88d658
adam-boduch <adam@boduch.net>
2016-07-25 20:17:40 +0200

    wyswietlenie zakladek

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -2,12 +2,26 @@
 
 namespace Coyote\Http\Controllers;
 
+use Lavary\Menu\Menu;
+
 class SearchController extends Controller
 {
     public function index()
     {
         $this->breadcrumb->push('Szukaj', route('search'));
 
-        return $this->view('search');
+        return $this->view('search', [
+            'tabs' => $this->tabs()
+        ]);
+    }
+
+    private function tabs()
+    {
+        return app(Menu::class)->make('tabs', function ($menu) {
+            $menu->add('Posty na forum', route('search', ['type' => 'posts']));
+            $menu->add('Oferty pracy', route('search', ['type' => 'offers']));
+            $menu->add('Mikroblogi', route('search', ['type' => 'microblogs']));
+            $menu->add('Artykuły', route('search', ['type' => 'wiki']));
+        });
     }
 }
