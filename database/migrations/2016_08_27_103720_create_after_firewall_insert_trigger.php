2b7925f68da03089865ffd62be7e6fd0f4fffcd9
adam-boduch <adam@boduch.net>
2016-08-27 10:48:33 +0200

    w przypadku zalozenia bana ustawiamy wartosc 1 w kolumnie is_blocked, tabeli users

---
+++ database/migrations/2016_08_27_103720_create_after_firewall_insert_trigger.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAfterFirewallInsertTrigger extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::unprepared('
+CREATE FUNCTION after_firewall_insert() RETURNS trigger LANGUAGE plpgsql AS $$
+BEGIN
+    IF NEW.user_id IS NOT NULL THEN
+ 	    UPDATE users SET is_blocked = 1 WHERE "id" = NEW.user_id;
+ 	END IF;
+
+	RETURN NEW;
+END;$$;
+
+CREATE TRIGGER after_firewall_insert AFTER INSERT ON firewall FOR EACH ROW EXECUTE PROCEDURE "after_firewall_insert"();
+        ');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::unprepared('DROP TRIGGER IF EXISTS "after_firewall_insert" ON firewall;');
+        DB::unprepared('DROP FUNCTION after_firewall_insert();');
+    }
+}
---
+++ database/migrations/2016_08_27_103729_create_after_firewall_delete_trigger.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAfterFirewallDeleteTrigger extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::unprepared('
+CREATE FUNCTION after_firewall_delete() RETURNS trigger LANGUAGE plpgsql AS $$
+BEGIN
+ 	IF OLD.user_id IS NOT NULL THEN
+ 	    IF (SELECT COUNT(*) FROM firewall WHERE user_id = OLD.user_id) = 0 THEN
+ 	        UPDATE users SET is_blocked = 0 WHERE "id" = OLD.user_id;
+ 	    END IF;
+    END IF;
+
+	RETURN NEW;
+END;$$;
+
+CREATE TRIGGER after_firewall_delete AFTER DELETE ON firewall FOR EACH ROW EXECUTE PROCEDURE "after_firewall_delete"();
+        ');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::unprepared('DROP TRIGGER IF EXISTS "after_firewall_delete" ON firewall;');
+        DB::unprepared('DROP FUNCTION after_firewall_delete();');
+    }
+}
