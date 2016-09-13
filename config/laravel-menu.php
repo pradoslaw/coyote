6e5762b4bcdbb8a92a898352e0e7cc5aeaaeabef
adam-boduch <adam@boduch.net>
2016-09-13 16:53:17 +0200

    konfiguracja menu

---
+++ config/laravel-menu.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'settings' => [
+        'default' => [
+            'auto_activate'    => true,
+            'activate_parents' => true,
+            'active_class'     => 'active',
+            'restful'          => false,
+            'cascade_data'     => true,
+            'rest_base'        => '',      // string|array
+            'active_element'   => 'item',  // item|link
+        ]
+    ]
+];
