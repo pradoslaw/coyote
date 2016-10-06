7c6d5440147016c8f8d2bee654b1ad12ee4192e9
adam-boduch <adam@boduch.net>
2016-10-06 19:13:05 +0200

    suniecie providera z tablicy

--- config/compile.php
+++ config/compile.php
@@ -17,7 +17,6 @@ return [
 
         realpath(__DIR__ . '/../app/Providers/AppServiceProvider.php'),
         realpath(__DIR__ . '/../app/Providers/AuthServiceProvider.php'),
-        realpath(__DIR__ . '/../app/Providers/ConfigServiceProvider.php'),
         realpath(__DIR__ . '/../app/Providers/EventServiceProvider.php'),
         realpath(__DIR__ . '/../app/Providers/RouteServiceProvider.php'),
         realpath(__DIR__ . '/../app/Providers/SessionServiceProvider.php'),
