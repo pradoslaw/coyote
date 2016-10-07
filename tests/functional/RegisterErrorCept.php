572c0def4d5de460060aeebc3a3fc6e5d12c721e
adam-boduch <adam@boduch.net>
2016-10-07 08:36:43 +0200

    psr

--- tests/functional/RegisterErrorCept.php
+++ tests/functional/RegisterErrorCept.php
@@ -8,15 +8,15 @@ $I->click('Utwórz konto', '.btn-register');
 $I->seeCurrentUrlEquals('/Register');
 $I->click('button[type=submit]');
 $I->seeFormHasErrors();
-$I->seeFormErrorMessages(array(
+$I->seeFormErrorMessages([
     'name' => 'Pole nazwa użytkownika jest wymagane.',
     'email' => 'Pole email jest wymagane.',
     'password' => 'Pole hasło jest wymagane.'
-));
+]);
 
 $I->fillField('name', '^%&%%$^$');
 $I->fillField('email', 'johndoe@example.com');
-$I->fillField('password',  'password');
-$I->fillField('password_confirmation',  'password');
+$I->fillField('password', 'password');
+$I->fillField('password_confirmation', 'password');
 $I->click('button[type=submit]');
 $I->seeFormErrorMessage('name', 'Nazwa użytkownika może zawierać litery, cyfry oraz znaki ._ -');
