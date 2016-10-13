5bf0b22e7f16941aa707148894b11ca3fbae61a7
adam-boduch <adam@boduch.net>
2016-10-13 19:14:20 +0200

    poprawiony bug przy zwezaniu/rozszerzaniu ekranu

--- resources/assets/js/pages/forum/sidebar.js
+++ resources/assets/js/pages/forum/sidebar.js
@@ -2,6 +2,14 @@ $(function() {
     "use strict";
 
     $('#btn-toggle-sidebar').click(function () {
-        $('#sidebar').toggle();
+        var sidebar = $('#sidebar');
+
+        if (sidebar.css('display') === 'block') {
+            // usuwamy atrybut "style" aby przegladarka "wziela" wartosc z pliku css
+            sidebar.removeAttr('style');
+        }
+        else {
+            sidebar.css('display', 'block');
+        }
     });
 });
