b5303cbcea62ca14e397aaa62d99d75c719277c9
adam-boduch <adam@boduch.net>
2016-11-13 17:22:42 +0100

    disable autoescape

--- src/Decorators/Ip.php
+++ src/Decorators/Ip.php
@@ -12,6 +12,9 @@ class Ip extends Decorator
      */
     public function decorate(Cell $cell)
     {
+        // disable auto escape so we can display <a> html tag in cell
+        $cell->getColumn()->setAutoescape(false);
+
         $cell->setValue(
             $cell->getColumn()->getGrid()->getGridHelper()->getHtmlBuilder()->tag('abbr', (string) $cell->getUnescapedValue())
         );
