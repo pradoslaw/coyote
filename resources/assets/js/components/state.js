c4487b146bd7bfbf321a5a0e6fe5939127e4f09d
adam-boduch <adam@boduch.net>
2016-11-14 21:22:18 +0100

    zawijanie wierszy w etykiecie grupy

--- resources/assets/js/components/state.js
+++ resources/assets/js/components/state.js
@@ -5,7 +5,7 @@ $(function() {
         var submit = $(this).find(':submit');
 
         if (typeof submit.data('submit-state') !== 'undefined') {
-            submit.text(submit.data('submit-state')).attr('disabled', 'disabled');
+            submit.html('<i class="fa fa-spinner fa-spin fa-fw"></i> ' + submit.data('submit-state')).attr('disabled', 'disabled');
         }
     });
 });
