794725070a4f13e341736fd169634eac8bf32e7e
adam-boduch <adam@boduch.net>
2016-11-20 11:54:31 +0100

    sprawdzanie praw do zapisu

--- app/Http/Controllers/Adm/DashboardController.php
+++ app/Http/Controllers/Adm/DashboardController.php
@@ -9,6 +9,43 @@ class DashboardController extends BaseController
      */
     public function index()
     {
-        return $this->view('adm.dashboard');
+        return $this->view('adm.dashboard', ['checklist' => [
+            'Katalog <tt>storage</tt> z prawami do zapisu' => $this->isStorageWriteable(),
+            'Katalog <tt>uploads</tt> z prawami do zapisu' => $this->isPublicWriteable(),
+            'Redis włączony' => $this->isRedisEnabled(),
+            'Redis aktywny' => $this->isRedisWorking()
+        ]]);
+    }
+
+    /**
+     * @return bool
+     */
+    private function isStorageWriteable()
+    {
+        return is_writeable(storage_path());
+    }
+
+    /**
+     * @return bool
+     */
+    private function isPublicWriteable()
+    {
+        return is_writeable(public_path());
+    }
+
+    /**
+     * @return bool
+     */
+    private function isRedisEnabled()
+    {
+        return config('cache.default') === 'redis';
+    }
+
+    /**
+     * @return bool
+     */
+    private function isRedisWorking()
+    {
+        return (string) app('redis')->ping() === 'PONG';
     }
 }
