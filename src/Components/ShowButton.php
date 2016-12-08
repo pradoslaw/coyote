9934cce6d8b0ef4aa117b7cba57e8a148872ce84
adam-boduch <adam@boduch.net>
2016-12-08 10:23:15 +0100

    show button

---
+++ src/Components/ShowButton.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Boduch\Grid\Components;
+
+class ShowButton extends RowAction
+{
+    /**
+     * @return string
+     */
+    public function render()
+    {
+        return (string) $this->tag(
+            'a',
+            (string) $this->tag('i', '', ['class' => 'fa fa-eye']),
+            ['href' => $this->buildActionUrl($this->data), 'class' => 'btn btn-default btn-xs', 'title' => 'Pokaż ten rekord']
+        );
+    }
+}
