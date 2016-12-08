f86f1088d270a90ea9711cb7ff0495a2d72e0b18
adam-boduch <adam@boduch.net>
2016-12-08 07:17:13 +0100

    https

--- resources/assets/js/components/geo-ip.js
+++ resources/assets/js/components/geo-ip.js
@@ -5,7 +5,7 @@ $(function() {
         return this.each(function() {
             $(this).hover(() => {
                 if (!$(this).data('original-title')) {
-                    $.getJSON('http://geo-ip.pl/1.0/ip/' + $(this).text() + '?callback=?', {}, (json) => {
+                    $.getJSON('//geo-ip.pl/1.0/ip/' + $(this).text() + '?callback=?', {}, (json) => {
                         $(this).attr('data-original-title', `${json.country}, ${json.city}`).tooltip('show');
                     });
                 }
