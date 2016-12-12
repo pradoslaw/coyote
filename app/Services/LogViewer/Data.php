630cfd70bedbac7a0b8e0955209778c8cf4e6aac
adam-boduch <adam@boduch.net>
2016-12-12 11:56:36 +0100

    escapowanie danych dla wzgledow bezpieczenstwa

--- app/Services/LogViewer/Data.php
+++ app/Services/LogViewer/Data.php
@@ -22,7 +22,7 @@ class Data
     public function __construct(array $data)
     {
         $this->count = count($data);
-        $this->data = $data;
+        $this->data = $this->escape($data);
     }
 
     /**
@@ -35,7 +35,7 @@ class Data
         usort($this->data, function ($item1, $item2) use ($column, $order) {
             return 'asc' == $order ? ($item1[$column] <=> $item2[$column]) : ($item2[$column] <=> $item1[$column]);
         });
-        
+
         return $this;
     }
 
@@ -46,5 +46,19 @@ class Data
     public function paginate($perPage = 15)
     {
         return new LengthAwarePaginator($this->data, $this->count, $perPage);
+    }
+
+    /**
+     * @param array $data
+     * @return array
+     */
+    protected function escape(array $data): array
+    {
+        // escape stack due to security reasons. log may contain html code.
+        if (isset($data['stack'])) {
+            $data['stack'] = array_map('htmlspecialchars', $data['stack']);
+        }
+
+        return $data;
     }
 }
