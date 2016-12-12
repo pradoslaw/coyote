9fe0612273a3116ab1b77981da1aa6dced5df897
adam-boduch <adam@boduch.net>
2016-12-12 08:13:20 +0100

    nie usuwaj kodu html przed zapisem do mongo

--- app/Services/Stream/Objects/Block.php
+++ app/Services/Stream/Objects/Block.php
@@ -20,7 +20,9 @@ class Block extends ObjectAbstract
         $this->id = $block->id;
         $this->url = route('adm.blocks.save', [$block->id], false);
         $this->displayName = $block->name;
-        $this->content = str_limit(htmlspecialchars($block->content), 200);
+        // block content is most likely html code. do not strip html code before saving. we do that before showing
+        // this fragment to user.
+        $this->content = str_limit($block->content, 200);
 
         return $this;
     }
