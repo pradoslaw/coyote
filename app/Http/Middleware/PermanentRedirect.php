029dfa54151126dce1a3ad96d9c658dfbb10a78d
adam-boduch <adam@boduch.net>
2016-12-14 12:44:12 +0100

    zapomniany plik middleware

---
+++ app/Http/Middleware/PermanentRedirect.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Coyote\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class PermanentRedirect
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if ($request->has('skills') || $request->has('cities') || is_array($request->get('salary'))) {
+            return $this->makeRedirection($request);
+        }
+
+        return $next($request);
+    }
+
+    /**
+     * @param Request $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    private function makeRedirection(Request $request)
+    {
+        $all = $request->query->all();
+
+        $all = $this->rename($all, 'skills', 'tag');
+        $all = $this->rename($all, 'cities', 'city');
+
+        if (isset($all['salary']['from'])) {
+            $value = $all['salary']['from'];
+
+            $all['salary'] = $value;
+        }
+
+        return redirect()->route('job.home', $all, 301);
+    }
+
+    /**
+     * @param array $array
+     * @param $oldKey
+     * @param $newKey
+     * @return array
+     */
+    private function rename(array $array, $oldKey, $newKey)
+    {
+        if (isset($array[$oldKey])) {
+            $array[$newKey] = $array[$oldKey];
+            unset($array[$oldKey]);
+        }
+
+        return $array;
+    }
+}
