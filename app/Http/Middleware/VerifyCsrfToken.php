0170412a2533ebfde57afe4d634c3439d0ec6328
adam-boduch <adam@boduch.net>
2016-12-16 14:54:03 +0100

    dla komentarzy, wylaczamy ochrone przed csrf

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,7 @@ class VerifyCsrfToken extends BaseVerifier
      * @var array
      */
     protected $except = [
-        '/User/Settings/Ajax'
+        '/User/Settings/Ajax',
+        '/Microblog/Comment/Show/*'
     ];
 }
