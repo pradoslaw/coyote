389c6c143eed3f3fcb7638cd09700b2113623191
adam-boduch <adam@boduch.net>
2016-12-16 14:58:25 +0100

    phpdoc

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -55,7 +55,7 @@ class Handler extends ExceptionHandler
      *
      * @param  \Illuminate\Http\Request $request
      * @param  \Exception $e
-     * @return \Illuminate\Http\Response|\Illuminate\Http\JsonResponse
+     * @return \Illuminate\Http\Response|\Illuminate\Http\JsonResponse|\Symfony\Component\HttpFoundation\Response
      */
     public function render($request, Exception $e)
     {
