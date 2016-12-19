54c7b944f9733841216b9dba1cd56b21ad1dd9e4
adam-boduch <adam@boduch.net>
2016-12-19 12:18:02 +0100

    dwa regiony

--- app/Http/Forms/BlockForm.php
+++ app/Http/Forms/BlockForm.php
@@ -54,6 +54,6 @@ class BlockForm extends Form implements ValidatesWhenSubmitted
      */
     private function regions()
     {
-        return ['header' => 'Nagłówek', 'footer' => 'Stopka'];
+        return ['header' => 'Nagłówek', 'footer' => 'Stopka', 'head' => 'Znacznik <head>', 'body' => 'Znacznik <body>'];
     }
 }
