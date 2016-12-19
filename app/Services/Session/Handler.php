57d47ddda91519e5d35a396c264335b8b85f1b48
adam-boduch <adam@boduch.net>
2016-12-19 20:35:03 +0100

    refaktoryzacja

--- app/Services/Session/Handler.php
+++ app/Services/Session/Handler.php
@@ -2,65 +2,117 @@
 
 namespace Coyote\Services\Session;
 
+use Carbon\Carbon;
 use Illuminate\Database\Query\Expression;
 use Illuminate\Http\Request;
 use Illuminate\Session\DatabaseSessionHandler;
+use Illuminate\Contracts\Auth\Guard;
 use Jenssegers\Agent\Agent;
 
 class Handler extends DatabaseSessionHandler
 {
     /**
-     * {@inheritDoc}
+     * {@inheritdoc}
+     */
+    public function read($sessionId)
+    {
+        $session = (object) $this->getQuery()->find($sessionId);
+
+        if (isset($session->updated_at)) {
+            if ($session->updated_at < Carbon::now()->subMinutes($this->minutes)) {
+                $this->exists = true;
+
+                return;
+            }
+        }
+
+        if (isset($session->payload)) {
+            $this->exists = true;
+
+            return base64_decode($session->payload);
+        }
+    }
+
+    /**
+     * {@inheritdoc}
      */
     public function write($sessionId, $data)
     {
-        $request = $this->container->make('request');
-        $auth    = $this->container->make('auth');
+        $payload = $this->getDefaultPayload($data);
 
-        $userId  = $auth->check() ? $auth->user()->id : null;
-        $url     = $request->fullUrl();
-        $ip      = $request->ip();
-        $browser = '';
-
-        // nie korzystamy ze standardowej klasy Request. Testy funkcjonalne "nie widza"
-        // metody browser() z klasy CostomRequest
-        if (method_exists($request, 'browser')) {
-            $browser = $request->browser();
+        if (!$this->exists) {
+            $this->read($sessionId);
         }
 
-        $data = [
-            'payload'       => base64_encode($data),
-            'updated_at'    => new Expression('NOW()'),
-            'user_id'       => $userId,
-            'url'           => $url,
-            'ip'            => $ip,
-            'browser'       => $browser
-        ];
-
         if ($this->exists) {
-            $this->getQuery()->where('id', $sessionId)->update($this->filterUrl($request, $data));
+            $this->getQuery()->where('id', $sessionId)->update($payload);
         } else {
-            $agent = new Agent();
-            $agent->setUserAgent($browser);
+            $payload['id'] = $sessionId;
 
-            if ($agent->isRobot()) {
-                $data['robot'] = $agent->robot();
-            }
-
-            $this->insert(['id' => $sessionId] + $data);
+            $this->insert($payload);
         }
 
         $this->exists = true;
     }
 
     /**
-     * @param array $data
+     * Get the default payload for the session.
+     *
+     * @param  string  $data
+     * @return array
+     */
+    protected function getDefaultPayload($data)
+    {
+        $payload = ['payload' => base64_encode($data), 'updated_at' => new Expression('NOW()')];
+
+        if (!$container = $this->container) {
+            return $payload;
+        }
+
+        if ($container->bound(Guard::class)) {
+            $payload['user_id'] = $container->make(Guard::class)->id();
+        }
+
+        if ($container->bound('request')) {
+            $request = $container->make('request');
+
+            $payload['ip'] = $request->ip();
+            $payload['url'] = $request->fullUrl();
+            $payload['browser'] = substr((string) $request->header('User-Agent'), 0, 500);
+            $payload['robot'] = $this->robot($payload['browser']);
+
+            $payload = $this->filterUrl($request, $payload);
+        }
+
+        return $payload;
+    }
+
+    /**
+     * @param string $browser
+     * @return null|string
+     */
+    private function robot($browser)
+    {
+        if ($this->exists) {
+            return null;
+        }
+
+        $agent = new Agent();
+        if (!$agent->isRobot($browser)) {
+            return null;
+        }
+
+        return $agent->robot();
+    }
+
+    /**
+     * @param array $payload
      * @throws \Exception
      */
-    private function insert($data)
+    private function insert($payload)
     {
         try {
-            $this->getQuery()->insert($data);
+            $this->getQuery()->insert($payload);
         } catch (\PDOException $e) {
             // tutaj moze byc blad z zapisem sesji w przypadku zapytan ajax
             // @see https://github.com/laravel/framework/issues/9251
