3d18232c95c6adaed3970fb9bd950b68217d1ab2
adam-boduch <adam@boduch.net>
2016-12-23 13:14:28 +0100

    filtrowanie wartosci

--- app/Services/Elasticsearch/Filters/Range.php
+++ app/Services/Elasticsearch/Filters/Range.php
@@ -8,4 +8,23 @@ use Coyote\Services\Elasticsearch\Filter;
 class Range extends Filter implements DslInterface
 {
     const FILTER_NAME = 'range';
+
+    /**
+     * Filter constructor.
+     * @param string $field
+     * @param mixed $value
+     */
+    public function __construct($field, $value)
+    {
+        parent::__construct($field, array_map([&$this, 'filterValue'], $value));
+    }
+
+    /**
+     * @param mixed $value
+     * @return mixed
+     */
+    private function filterValue($value)
+    {
+        return (int) trim(str_replace(' ', '', $value));
+    }
 }
