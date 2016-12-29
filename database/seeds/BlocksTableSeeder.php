b44bd20a5ea740ffff7011b2ddd955df0b23fdf9
Adam Wójs <adam@wojs.pl>
2016-12-29 14:08:38 +0100

    Przycisk powrotu na górę strony #194

--- database/seeds/BlocksTableSeeder.php
+++ database/seeds/BlocksTableSeeder.php
@@ -80,7 +80,7 @@ EOF;
         </ul>
     </div>
 
-    <div class=" col-sm-4">
+    <div class="col-sm-4">
         <div class="footer-bubble">
             <h6>Skontaktuj się z nami</h6>
 
@@ -89,6 +89,8 @@ EOF;
                 <li><a title="Skontaktuj się poprzez email" href="https://www.facebook.com/4programmers.net"><i class="fa fa-facebook fa-fw"></i> Odwiedź nas na Facebooku</a></li>
             </ol>
         </div>
+        
+        <a id="back-to-top" href="#top" title="Powrót na górę">Powrót na górę</a>
     </div>
 </div>
 EOF;
--- resources/assets/js/main.js
+++ resources/assets/js/main.js
@@ -7,3 +7,22 @@ $(function() {
         }
     });
 });
+
+$(function() {
+    'use strict';
+
+    var AMOUNT_SCROLLED = 300;
+
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > AMOUNT_SCROLLED) {
+            $('#back-to-top').fadeIn('slow');
+        } else {
+            $('#back-to-top').fadeOut('slow');
+        }
+    });
+
+    $("#back-to-top").click(function () {
+        $("html, body").animate({scrollTop: 0}, "slow");
+        return false;
+    });
+});
