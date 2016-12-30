dee46c7bcf1de4e5a29ef3df609fd84605f19341
adam-boduch <adam@boduch.net>
2016-12-30 10:41:58 +0100

    zamiana ze slow na fast

--- resources/assets/js/components/scrolltop.js
+++ resources/assets/js/components/scrolltop.js
@@ -12,7 +12,8 @@ $(function() {
     });
 
     $(".back-to-top").click(function () {
-        $("html, body").animate({scrollTop: 0}, "slow");
+        $("html, body").animate({scrollTop: 0}, "fast");
+
         return false;
     });
 });
