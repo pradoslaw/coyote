51dd1e838481b1e1dc37d351a46b5cc0b192f000
adam-boduch <adam@boduch.net>
2016-12-31 17:12:42 +0100

    bugfix: zbyt agresywne parsowanie url

---
+++ tests/unit/AutolinkTest.php
@@ -0,0 +1,23 @@
+<?php
+
+use Coyote\Services\Parser\Parsers\Autolink;
+
+class AutolinkTest extends \Codeception\TestCase\Test
+{
+    /**
+     * @var \UnitTester
+     */
+    protected $tester;
+
+    // tests
+    public function testParseLinks()
+    {
+        $parser = new Autolink();
+
+        $input = '<a href="http://4programmers.net">4programmers</a>.net';
+        $this->tester->assertEquals($input, $parser->parse($input));
+
+        $input = 'www.4programmers.net';
+        $this->tester->assertEquals('<a href="http://www.4programmers.net">www.4programmers.net</a>', $parser->parse($input));
+    }
+}
