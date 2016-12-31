09c4dd0de48db2fedec30407ae57a08ad81f1c49
adam-boduch <adam@boduch.net>
2016-12-31 09:09:03 +0100

    nieuzywany kod

---
+++ tests/unit/PurifierTest.php
@@ -0,0 +1,28 @@
+<?php
+
+use Coyote\Services\Parser\Parsers\Purifier;
+
+class LinkTest extends \Codeception\TestCase\Test
+{
+    /**
+     * @var \UnitTester
+     */
+    protected $tester;
+
+    // tests
+    public function testParseLinks()
+    {
+        $parser = new Purifier();
+
+        $input = '<a href="http://www.wp.pl">http://www.wp.pl</a>';
+        $this->tester->assertEquals($input, $parser->parse($input));
+    }
+
+    public function testParseBlockquotes()
+    {
+        $parser = new Purifier();
+
+        $input = '<blockquote>lorem ipsum<blockquote>lorem ipsum</blockquote></blockquote>';
+        $this->tester->assertEquals($input, $parser->parse($input));
+    }
+}
