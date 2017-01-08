3a63beacf7937a8673ae4a36c39f5232af168ca6
adam-boduch <adam@boduch.net>
2017-01-08 11:17:37 +0100

    phpdoc

--- app/Repositories/Eloquent/PageRepository.php
+++ app/Repositories/Eloquent/PageRepository.php
@@ -7,7 +7,7 @@ use Coyote\Repositories\Contracts\PageRepositoryInterface;
 class PageRepository extends Repository implements PageRepositoryInterface
 {
     /**
-     * @return \Coyote\Page
+     * @return string
      */
     public function model()
     {
