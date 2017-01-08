3617abb0046ed8637eb3e53081e73a6b6092ee2e
adam-boduch <adam@boduch.net>
2017-01-08 13:50:36 +0100

    dopasowanie do istniejacej konwencji nazewnictwa

--- app/Http/Controllers/Microblog/ViewController.php
+++ app/Http/Controllers/Microblog/ViewController.php
@@ -3,16 +3,16 @@
 namespace Coyote\Http\Controllers\Microblog;
 
 use Coyote\Http\Controllers\Controller;
-use Coyote\Repositories\Contracts\MicroblogRepositoryInterface as Microblog;
+use Coyote\Repositories\Contracts\MicroblogRepositoryInterface as MicroblogRepository;
 
 class ViewController extends Controller
 {
     /**
      * @param $id
-     * @param Microblog $repository
+     * @param MicroblogRepository $repository
      * @return \Illuminate\View\View
      */
-    public function index($id, Microblog $repository)
+    public function index($id, MicroblogRepository $repository)
     {
         /** @var \Coyote\Microblog $microblog */
         $microblog = $repository->findOrFail($id);
