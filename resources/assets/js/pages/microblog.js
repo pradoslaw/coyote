f0167b04e2d8c2db095da719d5c99a48ff41b21e
adam-boduch <adam@boduch.net>
2017-01-08 16:19:35 +0100

    powiekszanie zalacznika

--- resources/assets/js/pages/microblog.js
+++ resources/assets/js/pages/microblog.js
@@ -258,11 +258,11 @@ $(function () {
         .on('click', 'a[data-toggle="lightbox"]', function(e) {
             e.preventDefault();
 
-            // require.ensure(['ekko-lightbox/ekko-lightbox'], () => {
-            //     require('ekko-lightbox/ekko-lightbox')($);
-            //
-            //     $(this).ekkoLightbox();
-            // });
+            require.ensure([], (require) => {
+                require('ekko-lightbox/dist/ekko-lightbox');
+
+                $(this).ekkoLightbox();
+            });
         })
         .on('click', '.read-more', function()
         {
