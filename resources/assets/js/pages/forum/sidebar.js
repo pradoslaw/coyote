a9ea5658fd436e48a44ea65a74a073b1168a4596
Adam Wójs <adam@wojs.pl>
2017-01-17 22:42:08 +0100

    Obsługę zamykania sidebara forum po kliknięciu poza jego obszarem na urządzeniach mobilnych.

--- resources/assets/js/pages/forum/sidebar.js
+++ resources/assets/js/pages/forum/sidebar.js
@@ -1,15 +1,28 @@
-$(function() {
+$(function () {
     "use strict";
 
-    $('#btn-toggle-sidebar').click(function () {
-        var sidebar = $('#sidebar');
+    let sidebar = $('#sidebar');
 
-        if (sidebar.css('display') === 'block') {
-            // usuwamy atrybut "style" aby przegladarka "wziela" wartosc z pliku css
-            sidebar.removeAttr('style');
+    let handler = function (e) {
+        if ($(e.target).closest(sidebar).length === 0) {
+            e.preventDefault();
+            close();
         }
-        else {
-            sidebar.css('display', 'block');
-        }
+    };
+
+    let close = function () {
+        // usuwamy atrybut "style" aby przegladarka "wziela" wartosc z pliku css
+        sidebar.removeAttr('style');
+        $(document).unbind("click touchstart", handler);
+    };
+
+    let open = function () {
+        sidebar.css('display', 'block');
+        $(document).bind("click touchstart", handler);
+    };
+
+    $('#btn-toggle-sidebar').click(function () {
+        sidebar.css('display') === 'block' ? close() : open();
+        return false;
     });
 });
