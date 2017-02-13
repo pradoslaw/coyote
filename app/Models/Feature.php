d155b7f6f1c07856ed177278bb726e719ec38703
adam-boduch <adam@boduch.net>
2017-02-13 17:20:48 +0100

    nieuzywany namespace

--- app/Models/Feature.php
+++ app/Models/Feature.php
@@ -3,13 +3,12 @@
 namespace Coyote;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\Pivot;
 
 /**
  * @property int $id
  * @property string $name
  * @property string $default
- * @property Pivot $pivot
+ * @property \Illuminate\Database\Eloquent\Relations\Pivot $pivot
  */
 class Feature extends Model
 {
