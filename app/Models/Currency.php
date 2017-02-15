53f762f0fbe22db940c3055f0955f24fee859d79
adam-boduch <adam@boduch.net>
2017-02-15 21:19:49 +0100

    pierwsze tabele

--- app/Models/Currency.php
+++ app/Models/Currency.php
@@ -6,6 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Currency extends Model
 {
+    const PLN = 1;
+    const EUR = 2;
+    const USD = 3;
+    const GBP = 4;
+    const CHF = 5;
+
     /**
      * The attributes that are mass assignable.
      *
