3478b9a08460c4079d3fae7d4875e67e069ab029
adam-boduch <adam@boduch.net>
2017-02-19 19:51:20 +0100

    modele

---
+++ app/Models/Invoice/Item.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Coyote\Invoice;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property string $description
+ * @property float $price
+ * @property int $currency_id
+ * @property int $vat_rate
+ */
+class Item extends Model
+{
+    /**
+     * @var string
+     */
+    protected $table = 'invoice_items';
+
+    /**
+     * @var array
+     */
+    protected $fillable = ['description', 'price', 'currency_id', 'vat_rate'];
+
+    /**
+     * @var bool
+     */
+    public $timestamps = false;
+
+    /**
+     * @return float
+     */
+    public function getGrossPriceAttribute()
+    {
+        return $this->grossPrice();
+    }
+
+    /**
+     * @return float
+     */
+    public function grossPrice()
+    {
+        return round($this->price + ($this->price * ($this->vat_rate / 100)), 2);
+    }
+}
