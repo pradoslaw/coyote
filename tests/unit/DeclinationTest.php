acb7d53dc6db11e0285e11c8983fbfadd7c59475
adam-boduch <adam@boduch.net>
2017-04-05 21:28:17 +0200

    poprawiona odmiana wyrazow

--- tests/unit/DeclinationTest.php
+++ tests/unit/DeclinationTest.php
@@ -20,6 +20,16 @@ class DeclinationTest extends \Codeception\TestCase\Test
         return Coyote\Services\Declination\Declination::format($value, ['sekunda', 'sekundy', 'sekund']);
     }
 
+    private function getApplication($value)
+    {
+        return \Coyote\Services\Declination\Declination::format($value, ['aplikacja', 'aplikacje', 'aplikacji']);
+    }
+
+    private function getViews($value)
+    {
+        return \Coyote\Services\Declination\Declination::format($value, ['odsłona', 'odsłony', 'odsłon']);
+    }
+
     // tests
     public function testIfDeclinationReturnsCorrectFormatOfSeconds()
     {
@@ -31,5 +41,19 @@ class DeclinationTest extends \Codeception\TestCase\Test
         $this->assertSame('99 sekund', $this->getSeconds(99));
         $this->assertSame('100 sekund', $this->getSeconds(100));
         $this->assertSame('0 sekund', $this->getSeconds(0));
+    }
+
+    public function testDeclination()
+    {
+        $this->assertSame('0 aplikacji', $this->getApplication(0));
+        $this->assertSame('1 aplikacja', $this->getApplication(1));
+        $this->assertSame('2 aplikacje', $this->getApplication(2));
+        $this->assertSame('3 aplikacje', $this->getApplication(3));
+        $this->assertSame('27 aplikacji', $this->getApplication(27));
+
+        $this->assertSame('0 odsłon', $this->getViews(0));
+        $this->assertSame('1 odsłona', $this->getViews(1));
+        $this->assertSame('2 odsłony', $this->getViews(2));
+        $this->assertSame('27 odsłon', $this->getViews(27));
     }
 }
