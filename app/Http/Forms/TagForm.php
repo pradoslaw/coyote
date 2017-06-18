5f44630a657517bf7aad213d695908715dc493a9
adam-boduch <adam@boduch.net>
2017-06-18 20:08:36 +0200

    dodanie filtrowania

--- app/Http/Forms/TagForm.php
+++ app/Http/Forms/TagForm.php
@@ -10,19 +10,24 @@ class TagForm extends Form implements ValidatesWhenSubmitted
 {
     public function buildForm()
     {
+        $categoriesList = Category::pluck('name', 'id')->toArray();
+
         $this
             ->add('name', 'text', [
                 'rules' => 'required|string|max:40|tag',
                 'label' => 'Nazwa (skrócona)',
+
             ])
             ->add('real_name', 'text', [
                 'rules' => 'string|max:100',
                 'label' => 'Nazwa (pełna)',
+
             ])
             ->add('category_id', 'select', [
                 'label' => 'Kategoria',
-                'choices' => Category::pluck('name', 'id')->toArray(),
-                'empty_value' => '--'
+                'choices' => $categoriesList,
+                'empty_value' => '--',
+
             ])
             ->add('submit', 'submit', [
                 'label' => 'Zapisz',
--- app/Http/Grids/Adm/TagsGrid.php
+++ app/Http/Grids/Adm/TagsGrid.php
@@ -5,6 +5,9 @@ namespace Coyote\Http\Grids\Adm;
 use Coyote\Services\Grid\Grid;
 use Boduch\Grid\Order;
 use Coyote\Tag;
+use Boduch\Grid\Filters\FilterOperator;
+use Boduch\Grid\Filters\Select;
+use Boduch\Grid\Filters\Text;
 
 class TagsGrid extends Grid
 {
@@ -17,7 +20,8 @@ class TagsGrid extends Grid
                 'sortable' => true,
                 'clickable' => function (Tag $tag) {
                     return link_to_route('adm.tags.save', $tag->name, [$tag->id]);
-                }
+                },
+                'filter' => new Text(['operator' => FilterOperator::OPERATOR_ILIKE, 'name' => 'tags.name'])
             ])
             ->addColumn('created_at', [
                 'placeholder' => '--',
@@ -27,7 +31,8 @@ class TagsGrid extends Grid
             ->addColumn('category', [
                 'title' => 'Kategoria',
                 'sortable' => true,
-                'placeholder' => '--'
+                'placeholder' => '--',
+                'filter' => new Select(['options' => Tag\Category::pluck('name', 'id')->toArray(), 'name' => 'category_id'])
             ]);
     }
 }
