98cbcf5f980fcc3c63000f560470ca0d4b08044d
adam-boduch <adam@boduch.net>
2017-06-18 10:17:05 +0200

    podmiana operatora

--- resources/assets/js/components/declination.js
+++ resources/assets/js/components/declination.js
@@ -6,14 +6,14 @@
  * @returns {*}
  */
 module.exports = function declination(value, declinationSet) {
-    if (value == 1) {
+    if (value === 1) {
         return declinationSet[0];
     }
     else {
         let unit = value % 10;
         let decimal = Math.round((value % 100) / 10);
 
-        if ((unit == 2 || unit == 2 || unit == 3 || unit == 4) && (decimal != 1)) {
+        if ((unit === 2 || unit === 2 || unit === 3 || unit === 4) && (decimal !== 1)) {
             return declinationSet[1];
         }
         else {
