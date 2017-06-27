de8fe64bd5178baa69a28e5837079517731c6545
adam-boduch <adam@boduch.net>
2017-06-27 21:47:42 +0200

    uaktualniony cennik

--- database/seeds/PlansTableSeeder.php
+++ database/seeds/PlansTableSeeder.php
@@ -11,6 +11,32 @@ class PlansTableSeeder extends Seeder
      */
     public function run()
     {
-        \Coyote\Plan::forceCreate(['name' => 'Promocja ogłoszenia', 'price' => 9, 'vat_rate' => 1.23]);
+        \Coyote\Plan::forceCreate([
+            'name' => 'Pakiet Mini',
+            'price' => 30,
+            'vat_rate' => 1.23,
+            'discount' => 0,
+            'length' => 30,
+            'benefits' => ['publish']
+        ]);
+
+        \Coyote\Plan::forceCreate([
+            'name' => 'Pakiet Standard',
+            'price' => 126,
+            'vat_rate' => 1.23,
+            'discount' => 0.45,
+            'length' => 30,
+            'benefits' => ['publish', 'ad'],
+            'is_default' => 1
+        ]);
+
+        \Coyote\Plan::forceCreate([
+            'name' => 'Pakiet Max',
+            'price' => 270,
+            'vat_rate' => 1.23,
+            'discount' => 0.5,
+            'length' => 30,
+            'benefits' => ['publish', 'ad', 'highlight', 'top_spot', 'boost']
+        ]);
     }
 }
