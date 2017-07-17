e763762af9fd1c9cd171c4c4bf07eb326ed5ad18
adam-boduch <adam@boduch.net>
2017-07-17 16:55:25 +0200

    uszczegolowienie komunikatu

--- resources/lang/pl/payment.php
+++ resources/lang/pl/payment.php
@@ -6,7 +6,7 @@ return [
     'timeout' => 'Płatność nie została pobrana. Usługa tymczasowo niedostępna. Prosimy spróbować za kilka minut.',
     'server_error' => 'Płatność nie została pobrana. Prosimy o kontakt pod adresem ' . config('mail.from.address'),
     'unhandled' => 'Wystąpił błąd. Płatność nie została pobrana. Prosimy o kontakt pod adresem ' . config('mail.from.address'),
-    'validation' => 'Płatność nie została pobrana. Prosimy o sprawdzenie poprawności wpisanych danych oraz sprawdzenie czy na karcie dostępne są wymagane środki. Jeżeli tak, to prosimy o kontakt pod adresem ' . config('mail.from.address'),
+    'validation' => 'Płatność nie została pobrana. Prosimy o sprawdzenie poprawności wpisanych danych (data ważności, CCV) oraz sprawdzenie czy na karcie dostępne są wymagane środki. Jeżeli tak, to prosimy o kontakt pod adresem ' . config('mail.from.address'),
 
     'success' => 'Dziękujemy! Płatność została zaksięgowana. Za chwilę zaczniemy promowanie ogłoszenia.'
 ];
