30515a7664301a3256f84c07abf6826581d5bc59
adam-boduch <adam@boduch.net>
2017-10-04 21:42:22 +0200

    zwiekszenie szerokosci faktury

--- app/Services/Invoice/Pdf.php
+++ app/Services/Invoice/Pdf.php
@@ -30,7 +30,6 @@ class Pdf
     public function create(Payment $payment)
     {
         $mpdf = new Mpdf(['tempDir' => storage_path('app')]);
-        $mpdf->SetDisplayMode('fullpage');
         $mpdf->WriteHTML($this->view($payment)->render());
 
         return $mpdf->Output('', 'S');
