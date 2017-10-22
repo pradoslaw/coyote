0d1cc47ccc9e0521a22879f45c889d1b55c47b5c
adam-boduch <adam@boduch.net>
2017-10-22 15:27:27 +0200

    refaktoryzacja - nie potrzebujemy tego parametru w metodzie

--- app/Http/Controllers/Forum/TopicController.php
+++ app/Http/Controllers/Forum/TopicController.php
@@ -105,7 +105,7 @@ class TopicController extends BaseController
 
         if ($markTime[Topic::class] < $dateTimeString && $markTime[Forum::class] < $dateTimeString) {
             // mark topic as read
-            $topic->markAsRead($dateTimeString, $this->userId, $this->guestId);
+            $topic->markAsRead($this->userId, $this->guestId);
             $isUnread = true;
 
             if ($markTime[Forum::class] < $dateTimeString) {
@@ -274,7 +274,7 @@ class TopicController extends BaseController
         $forumMarkTime = $topic->forum->markTime($this->userId, $this->guestId);
 
         // mark topic as read
-        $topic->markAsRead($topic->last_post_created_at, $this->userId, $this->guestId);
+        $topic->markAsRead($this->userId, $this->guestId);
         $isUnread = $this->topic->isUnread($topic->forum_id, $forumMarkTime, $this->userId, $this->guestId);
 
         if (!$isUnread) {
