6dac92f167c946f0ee8007ba2d24c86822be2a2e
adam-boduch <adam@boduch.net>
2017-12-08 21:16:20 +0100

    dodatkowy warunek sprawdzajacy czy oferta istnieje

--- app/Http/Controllers/Job/SubmitController.php
+++ app/Http/Controllers/Job/SubmitController.php
@@ -72,7 +72,7 @@ class SubmitController extends Controller
             $job = $request->session()->get(Job::class);
         } else {
             $job = $this->job->findOrNew($id);
-            abort_if($job->is_expired, 404);
+            abort_if($job->exists && $job->is_expired, 404);
 
             $job = $loader->init($job);
         }
