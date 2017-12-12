3d3f45e1bf18a536dd6feac7ced4b0d5f66582f2
adam-boduch <adam@boduch.net>
2017-12-12 18:04:48 +0100

    bugfix: w walidatorze nie bierzemy pod uwage asp.net czy java.io

---
+++ tests/unit/SpamValidatorTest.php
@@ -0,0 +1,33 @@
+<?php
+
+class SpamValidatorTest extends \Codeception\TestCase\Test
+{
+    /**
+     * @var \UnitTester
+     */
+    protected $tester;
+
+    // tests
+    public function testValidateSpamLink()
+    {
+        $auth = Mockery::mock(\Illuminate\Contracts\Auth\Guard::class);
+        $auth->shouldReceive('check')->andReturn(false);
+
+        $request = request();
+
+        $validator = new \Coyote\Http\Validators\SpamValidator($auth, $request);
+
+        // not spam link
+        $this->assertTrue($validator->validateSpamLink([], 'java.io', [1]));
+        $this->assertTrue($validator->validateSpamLink([], 'asp.net', [1]));
+        // spam links
+        $this->assertFalse($validator->validateSpamLink([], 'www.java.io', [1]));
+        $this->assertFalse($validator->validateSpamLink([], 'http://www.java.io', [1]));
+        $this->assertFalse($validator->validateSpamLink([], 'https://www.java.io', [1]));
+        $this->assertFalse($validator->validateSpamLink([], 'https://java.io', [1]));
+        $this->assertFalse($validator->validateSpamLink([], 'http://java.io', [1]));
+        $this->assertFalse($validator->validateSpamLink([], 'http://java.io/foo.html?x=y', [1]));
+
+        $this->assertTrue($validator->validateSpamLink([], 'foo@bar.net', [1]));
+    }
+}
