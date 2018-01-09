aaab4b857e1e6cb98e647006448c28d7c09bbe63
adam-boduch <adam@boduch.net>
2018-01-09 20:57:45 +0100

    element domyslnie ukryty

--- resources/assets/js/plugins/tags.js
+++ resources/assets/js/plugins/tags.js
@@ -6,7 +6,7 @@
             var $this = $(this);
             var name = $this.attr('name') + '[]';
 
-            var dropdown = $('<ol class="tag-dropdown"></ol>');
+            var dropdown = $('<ol class="tag-dropdown"></ol>').hide();
             var index = -1;
             var timeId;
 
