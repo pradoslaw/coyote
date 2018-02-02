64d749a3091ab34cd7e8ccbae8275c32bfa52ce2
adam-boduch <adam@boduch.net>
2018-02-02 07:08:03 +0100

    Revert "zmiana rozmiaru loga firm"
    This reverts commit 04e5107

--- app/Services/Media/Filters/Logo.php
+++ app/Services/Media/Filters/Logo.php
@@ -7,10 +7,10 @@ class Logo extends Thumbnail
     /**
      * @var int
      */
-    protected $width = 600;
+    protected $width = 140;
 
     /**
      * @var int
      */
-    protected $height = 600;
+    protected $height = 140;
 }
