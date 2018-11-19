cf2030607e567fdf831815e25a7d2e86d39d5a1f
adam-boduch <adam@boduch.net>
2018-11-19 08:22:00 +0100

    zmiana formatu adresu

--- config/vendor.php
+++ config/vendor.php
@@ -6,5 +6,6 @@ return [
     'city' => env('VENDOR_CITY'),
     'postal_code' => env('VENDOR_POSTAL_CODE'),
     'vat_id' => env('VENDOR_VAT_ID'),
+    'country' => env('VENDOR_COUNTRY'),
     'default_vat_rate' => 1
 ];
