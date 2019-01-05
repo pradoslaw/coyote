19d04bbcbddad2b6190f7f8ac7ea0b0435e60371
adam-boduch <adam@boduch.net>
2019-01-05 07:06:09 +0100

    zmiana typu kolumny na int

---
+++ database/migrations/2019_01_05_070410_change_job_referers_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeJobReferersTable extends Migration
+{
+    use SchemaBuilder;
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $this->schema->table('job_referers', function (Blueprint $table) {
+            $table->integer('count')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $this->schema->table('job_referers', function (Blueprint $table) {
+            $table->smallInteger('count')->change();
+        });
+    }
+}
