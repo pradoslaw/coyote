a1c2df9e6ff9cea0f02a453d1aa6af7959aacb04
adam-boduch <adam@boduch.net>
2019-01-05 13:18:29 +0000

    Apply fixes from StyleCI

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -3,7 +3,6 @@
 use Faker\Generator as Faker;
 
 $factory->define(\Coyote\Post::class, function (Faker $faker) {
-
     return [
         'text' => $faker->realText(),
         'ip' => $ip = $faker->ipv4,
