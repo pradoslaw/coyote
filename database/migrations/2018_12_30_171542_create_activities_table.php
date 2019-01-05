3ab46a154cf442707cc7a102b2d41ead06969e25
adam-boduch <adam@boduch.net>
2019-01-05 19:23:26 +0100

    przeniesienie kodu do seedera

--- database/migrations/2018_12_30_171542_create_activities_table.php
+++ database/migrations/2018_12_30_171542_create_activities_table.php
@@ -30,36 +30,6 @@ class CreateActivitiesTable extends Migration
 
             $table->index('forum_id');
         });
-
-        $posts = \Coyote\Post::limit(10)->get();
-        $comments = \Coyote\Post\Comment::limit(10)->get();
-
-        $items = collect($posts)->merge($comments)->sortBy('created_at');
-
-        foreach ($items as $item) {
-            $data = ['created_at' => $item->created_at->toDateTimeString(), 'user_id' => $item->user_id];
-
-            if (isset($item['topic_id'])) {
-                $data += [
-                    'topic_id' => $item->topic_id,
-                    'forum_id' => $item->forum_id,
-                    'excerpt' => excerpt($item->text),
-                    'content_id' => $item->id,
-                    'content_type' => \Coyote\Post::class,
-                    'user_name' => $item->user_name
-                ];
-            } else {
-                $data += [
-                    'topic_id' => $item->post->topic_id,
-                    'forum_id' => $item->post->topic->forum_id,
-                    'excerpt' => excerpt($item->text),
-                    'content_id' => $item->id,
-                    'content_type' => \Coyote\Post\Comment::class
-                ];
-            }
-
-            $this->db->table('activities')->insert($data);
-        }
     }
 
     /**
