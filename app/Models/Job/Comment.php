012fd1e941f4b857ef301ba3a6bbb6e2fd34e2e2
adam-boduch <adam@boduch.net>
2019-01-08 20:58:54 +0100

    model + tabela

---
+++ app/Models/Job/Comment.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Coyote\Models\Job;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['user_id', 'job_id', 'user_name', 'parent_id', 'text'];
+
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'job_comments';
+
+}
