185bcf29923d4ea785a0a6612dfac2c4a2f44284
adam-boduch <adam@boduch.net>
2019-01-14 14:00:57 +0100

    zapisujemy do tabeli a nie do sesji

--- app/Http/Controllers/Job/RenewController.php
+++ app/Http/Controllers/Job/RenewController.php
@@ -4,16 +4,19 @@ namespace Coyote\Http\Controllers\Job;
 
 use Coyote\Http\Controllers\Controller;
 use Coyote\Job;
+use Coyote\Services\Job\Draft;
 use Coyote\Services\Job\Loader;
 
 class RenewController extends Controller
 {
     /**
-     * @param Job $job
      * @param Loader $loader
+     * @param Job $job
+     * @param Draft $draft
      * @return \Illuminate\Http\RedirectResponse
+     * @throws \Illuminate\Auth\Access\AuthorizationException
      */
-    public function index(Loader $loader, Job $job)
+    public function index(Loader $loader, Job $job, Draft $draft)
     {
         abort_unless($job->is_expired, 404);
 
@@ -32,7 +35,7 @@ class RenewController extends Controller
         $this->authorize('update', $job);
         $this->authorize('update', $job->firm);
 
-        $this->request->session()->put(Job::class, $job);
+        $draft->put(Job::class, $job);
 
         return redirect()->route('job.submit');
     }
