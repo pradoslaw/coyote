f67b580c497987f04b51a10c1b89517135a88d64
adam-boduch <adam@boduch.net>
2019-01-20 14:49:52 +0100

    selenium

--- tests/acceptance/RegisterCept.php
+++ tests/acceptance/RegisterCept.php
@@ -1,13 +1,17 @@
 <?php
 use Faker\Factory;
 
+
+
 $fake = Factory::create();
 $name = $fake->userName;
 
 $I = new AcceptanceTester($scenario);
 $I->wantTo('register a user');
+//$I->getModule('WebDriver')->webDriver->manage()->deleteAllCookies();
 
 $I->amOnPage('/Register');
+
 $I->wait(1);
 $I->fillField('name', $name);
 $I->fillField('email', $fake->email);
@@ -16,6 +20,6 @@ $I->fillField('password_confirmation', 'password');
 $I->canSeeCheckboxIsChecked('input[name=human]');
 $I->click('button[type=submit]');
 
-$I->seeInCurrentUrl('/');
+$I->seeInCurrentUrl('/User');
 $I->click('.dropdown-toggle', '.avatar');
 $I->see($name, '.dropdown-username');
