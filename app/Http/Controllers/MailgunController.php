c9a614bd6587a1dc394f698871e88d2a708e26dc
adam-boduch <adam@boduch.net>
2019-01-29 08:00:02 +0100

    mailgun webhook: deaktywacja nieprawidlwoych adresow e-mail

--- app/Http/Controllers/MailgunController.php
+++ app/Http/Controllers/MailgunController.php
@@ -10,17 +10,34 @@ class MailgunController extends Controller
     /**
      * @param UserRepository $user
      * @param Request $request
+     * @return string
      */
     public function permanentFailure(UserRepository $user, Request $request)
     {
-        logger()->debug($request->input()); // event-data->recipient
+        abort_unless(
+            $this->verify($request->input('signature.timestamp') , $request->input('signature.token'), $request->input('signature.signature')),
+            500
+        );
 
         /** @var \Coyote\User $result */
-//        $result = $user->findBy('email', $request->input('event-data.recipient'), ['id', 'name', 'email', 'is_confirm']);
-//
-//        if ($result->is_confirm) {
-//            $result->is_confirm = false;
-//            $result->save();
-//        }
+        $result = $user->findBy('email', $request->input('event-data.recipient'), ['id', 'name', 'email', 'is_confirm']);
+
+        if ($result->is_confirm) {
+            $result->is_confirm = false;
+            $result->save();
+        }
+
+        return 'Deactivate ' . $request->input('event-data.recipient');
+    }
+
+    /**
+     * @param string $timestamp
+     * @param string $token
+     * @param string $signature
+     * @return bool
+     */
+    private function verify(string $timestamp, string $token, string $signature): bool
+    {
+        return $signature === hash_hmac('sha256', $timestamp . $token, config('services.mailgun.secret'));
     }
 }
