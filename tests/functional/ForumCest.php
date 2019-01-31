fdd207d2224989e5048fe60e1fa80e6714709326
adam-boduch <adam@boduch.net>
2019-01-31 07:55:43 +0100

    testy

--- tests/functional/ForumCest.php
+++ tests/functional/ForumCest.php
@@ -36,6 +36,79 @@ class ForumCest
         $I->seeResponseCodeIs(401);
     }
 
+    public function testTryToCreateNewPostWhileTopicWasMoved(FunctionalTester $I)
+    {
+        $forum = $I->createForum();
+        $topic = $I->createTopic(['forum_id' => $forum->id]);
+
+        $I->amOnRoute('forum.post.submit', [$forum, $topic]);
+        $I->seeInCurrentUrl($forum->slug);
+        $I->fillField('text', 'Lorem ipsum');
+
+        $newForum = $I->createForum();
+        $topic->forum_id = $newForum->id;
+        $topic->save(); // move topic to new category
+
+        $I->click('Zapisz');
+
+        $I->canSeeInCurrentUrl($newForum->slug); // see new slug
+    }
+
+    public function testTryToCreateNewPostWhileTopicWasMovedToLockedCategory(FunctionalTester $I)
+    {
+        $forum = $I->createForum();
+        $topic = $I->createTopic(['forum_id' => $forum->id]);
+
+        $I->amOnRoute('forum.post.submit', [$forum, $topic]);
+        $I->seeInCurrentUrl($forum->slug);
+        $I->fillField('text', 'Lorem ipsum');
+
+        $newForum = $I->createForum(['is_locked' => true]);
+        $topic->forum_id = $newForum->id;
+        $topic->save(); // move topic to new category
+
+        $I->click('Zapisz');
+
+        $I->canSeeResponseCodeIs(401);
+    }
+
+    public function testTryToCreateNewPostWhileCategoryIsNotAccessableAnymore(FunctionalTester $I)
+    {
+        $forum = $I->createForum();
+        $topic = $I->createTopic(['forum_id' => $forum->id]);
+
+        $I->amOnRoute('forum.post.submit', [$forum, $topic]);
+        $I->seeInCurrentUrl($forum->slug);
+        $I->fillField('text', 'Lorem ipsum');
+
+        $adminUser = $I->createUser();
+        $group = $I->haveRecord(\Coyote\Group::class, ['name' => 'Secret group']);
+
+        $I->haveRecord(\Coyote\Group\User::class, ['group_id' => $group->id, 'user_id' => $adminUser->id]);
+        $I->haveRecord(\Coyote\Forum\Access::class, ['forum_id' => $forum->id, 'group_id' => $group->id]);
+
+        $I->click('Zapisz');
+
+        $I->canSeeResponseCodeIs(403);
+    }
+
+    public function testTryToCreateNewPostWhileTopicWasLocked(FunctionalTester $I)
+    {
+        $forum = $I->createForum();
+        $topic = $I->createTopic(['forum_id' => $forum->id]);
+
+        $I->amOnRoute('forum.post.submit', [$forum, $topic]);
+        $I->seeInCurrentUrl($forum->slug);
+        $I->fillField('text', 'Lorem ipsum');
+
+        $topic->is_locked = true;
+        $topic->save();
+
+        $I->click('Zapisz');
+
+        $I->canSeeResponseCodeIs(401);
+    }
+
     public function testTryToCreateNewTopicInLockedForumAndSeeErrorMessageAsAdmin(FunctionalTester $I)
     {
         $I->logInAsAdmin();
