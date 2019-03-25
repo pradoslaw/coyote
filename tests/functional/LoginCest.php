a97bd5f79e443e3d603feb55ce70ae5307764ad0
adam-boduch <adam@boduch.net>
2019-03-25 18:15:11 +0100

    dodatkowy test

--- tests/functional/LoginCest.php
+++ tests/functional/LoginCest.php
@@ -3,7 +3,7 @@
 
 class LoginCest
 {
-    public function testLoginAsUser(FunctionalTester $I)
+    public function testLoginAsUserUsingName(FunctionalTester $I)
     {
         $I->wantTo('login as a user');
 
@@ -17,5 +17,35 @@ class LoginCest
         $I->amOnPage('/');
         $I->seeAuthentication();
         $I->see('Joe Doe', '.dropdown-username');
+    }
+
+    public function testLoginAsUserUsingEmail(FunctionalTester $I)
+    {
+        $I->wantTo('login as a user using email');
+
+        $user = factory(\Coyote\User::class)->create(['password' => bcrypt('123')]);
+
+        $I->amOnPage('/Login');
+        $I->fillField('name',  $user->email); // case insensitive test
+        $I->fillField('password', '123');
+        $I->click('button[type=submit]');
+
+        $I->amOnPage('/');
+        $I->seeAuthentication();
+        $I->see($user->name, '.dropdown-username');
+    }
+
+    public function testLoginAsUserUsingNotVerifiedEmail(FunctionalTester $I)
+    {
+        $I->wantTo('login as a user using not verified email');
+
+        $user = factory(\Coyote\User::class)->create(['password' => bcrypt('123'), 'is_confirm' => false]);
+
+        $I->amOnPage('/Login');
+        $I->fillField('name',  $user->email); // case insensitive test
+        $I->fillField('password', '123');
+        $I->click('button[type=submit]');
+
+        $I->seeFormHasErrors();
     }
 }
