350f4aced1accd91d50ec8480d705b5c419860a5
adam-boduch <adam@boduch.net>
2019-05-12 13:49:47 +0200

    nazwa uzytkownika

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -4,7 +4,7 @@ use Faker\Generator as Faker;
 
 $factory->define(\Coyote\User::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
+        'name' => $faker->userName,
         'email' => $faker->unique()->safeEmail,
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm',
         'is_active' => true,
