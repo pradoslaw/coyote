2b7a85fee725fa3f64a3c6477014476cdb13d450
adam-boduch <adam@boduch.net>
2019-08-05 19:44:46 +0200

    zmiana typu kolumny

---
+++ database/migrations/2019_08_05_194114_change_page_stats_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangePageStatsTable extends Migration
+{
+    use SchemaBuilder;
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $this->schema->table('page_stats', function (Blueprint $table) {
+            $table->integer('visits')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $this->schema->table('page_stats', function (Blueprint $table) {
+            $table->smallInteger('visits')->change();
+        });
+    }
+}
