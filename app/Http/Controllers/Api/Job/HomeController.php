73d43e7b20b63dadd99aa9feca69d4ffd7f2bc15
adam-boduch <adam@boduch.net>
2019-08-21 08:36:37 +0200

    usuniecie komentarza

--- app/Http/Controllers/Api/Job/HomeController.php
+++ app/Http/Controllers/Api/Job/HomeController.php
@@ -28,7 +28,7 @@ class HomeController extends Controller
         $job->applyCriteria();
 
         $data = $job->paginate();
-//dd($data);
+
         return JobResource::collection($data);
     }
 }
