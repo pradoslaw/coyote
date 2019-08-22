1a75c64225e79d5c28c5715f459e21eb7215b329
adam-boduch <adam@boduch.net>
2019-08-22 07:06:24 +0200

    dodatkowy warunek sprawdzajacy czy ustawiony jest host

--- routes/api.php
+++ routes/api.php
@@ -12,6 +12,8 @@ $this->prefix('v1')->group(function () {
     $this->get('user', ['uses' => 'Api\UserController@index', 'middleware' => 'auth:api']);
 });
 
-// catch all url's and redirect to correct URL (like from api.4programmers.net/Forum to 4programmers.net/Forum)
-$this->get('{any}', ['uses' => 'Api\PermanentRedirectController@redirect'])->where('any', '.*');
+if (config('services.api.host') && env('APP_ENV') !== 'testing') {
+    // catch all url's and redirect to correct URL (like from api.4programmers.net/Forum to 4programmers.net/Forum)
+    $this->get('{any}', ['uses' => 'Api\PermanentRedirectController@redirect'])->where('any', '.*');
+}
 
