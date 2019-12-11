1b9ba7289524cab324372b6bc78d4d11f02995f7
adam <adam@boduch.net>
2019-12-11 19:10:45 +0100

    przeniesienie do jednego pliku

--- resources/assets/js/plugins/sociale.js
+++ resources/assets/js/plugins/sociale.js
@@ -1,12 +1,15 @@
 import Config from '../libs/config';
 import Vue from 'vue';
 import VueNotification from '../components/notifications.vue';
+import VuePm from '../components/pms.vue';
 
 const NotificationWrapper = Vue.extend(Object.assign(VueNotification, {props: {counter: {default: Config.get('notifications_unread')}}}));
+const PmWrapper = Vue.extend(Object.assign(VuePm, {props: {counter: {default: Config.get('pm_unread')}}}));
 
-const wrapper = new NotificationWrapper().$mount();
 const el = document.getElementById('nav-auth');
 
 if (el !== null) {
-    el.appendChild(wrapper.$el);
+
+    el.appendChild(new NotificationWrapper().$mount().$el);
+    el.appendChild(new PmWrapper().$mount().$el);
 }
