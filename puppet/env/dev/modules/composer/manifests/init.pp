38d67cc76c81b7e20c2870d9f1707e3124206fc0
adam <adam@boduch.net>
2019-12-27 17:26:59 +0100

    mamy dockera - nie potrzeba vagranta

--- puppet/env/dev/modules/composer/manifests/init.pp
+++
@@ -1,8 +0,0 @@
-# taken from http://johnstonianera.com/install-composer-with-puppet-and-vagrant/
-# and fixed with https://github.com/puphpet/puphpet/issues/2098
-# and added sudo chmod 755 /usr/local/bin/composer
-class composer {
-	exec { 'install composer':
-		command => 'wget https://getcomposer.org/composer.phar -O composer.phar && sudo mv composer.phar /usr/local/bin/composer && sudo chmod 755 /usr/local/bin/composer'
-	}
-}
--- puppet/env/dev/modules/elasticsearch/files/install_elasticsearch.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-mkdir -p /etc/puppet/modules;
-
-if [ ! -d /etc/puppet/modules/elasticsearch ]; then
-	puppet module install elasticsearch-elasticsearch --version 0.4.0
-fi
