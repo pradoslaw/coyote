0a5f48122ef2deda17c0788c11d43ee8f336518a
adam <adam@boduch.net>
2020-01-11 16:51:43 +0100

    dodanie loginu usera w tytule

--- app/Services/Stream/Render/Person.php
+++ app/Services/Stream/Render/Person.php
@@ -11,4 +11,15 @@ class Person extends Render
             return 'E-mail: ' . array_get($this->stream, 'object.email');
         }
     }
+
+    /**
+     * @return string
+     */
+    protected function object()
+    {
+        return (string) trans('stream.nouns.' . array_get($this->stream, 'object.objectType')) . ' ' . link_to(
+            array_get($this->stream, 'object.url'),
+            array_get($this->stream, 'object.displayName')
+        );
+    }
 }
