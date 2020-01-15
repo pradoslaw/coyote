f380a8aee2d3d81de7ded9f198a2fc2694eceb25
adam <adam@boduch.net>
2020-01-15 18:49:41 +0100

    ustawienie zmiennej srodowiskowej ktora powoduje wykonanie migracji podczas uruchamiania kontenera

--- docker/coyote/start.sh
+++ docker/coyote/start.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-php artisan migrate
+if [ ! -z "$DB_MIGRATE" ]; then
+    echo "Running database migration..."
+    php artisan migrate --force
 
-cp -rp /app/. /var/www
-cd /var/www
+    cp -rp /app/. /var/www
+    cd /var/www
 
-php artisan config:cache
-php artisan route:cache
+    php artisan config:cache
+    php artisan route:cache
+fi
 
 exec php-fpm
