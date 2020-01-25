82c897b7b3d673415dabbd3ba8efb5d12dd747ae
adam <adam@boduch.net>
2020-01-25 12:50:30 +0100

    security fix

--- tests/Feature/PostApiTest.php
+++ tests/Feature/PostApiTest.php
@@ -34,6 +34,11 @@ class PostApiTest extends TestCase
      */
     private $post;
 
+    /**
+     * @var string
+     */
+    private $token;
+
     public function setUp()
     {
         parent::setUp();
@@ -48,17 +53,37 @@ class PostApiTest extends TestCase
         $this->forum->access()->create(['group_id' => $group->id]);
         $this->topic = factory(Topic::class)->create(['forum_id' => $this->forum->id]);
         $this->post = factory(Post::class)->create(['forum_id' => $this->forum->id, 'topic_id' => $this->topic->id]);
+
+        $this->token = $this->user->createToken('4programmers.net')->accessToken;
     }
 
-    public function testShowPostWhenAuthorized()
+    public function testShowAllTopics()
+    {
+        $request = $this->get('/v1/posts', ['Accept' => 'application/json']);
+
+        $data = $request->decodeResponseJson('data');
+
+        $this->assertNotEquals($data[0]['html'], $this->topic->posts()->first()->html);
+    }
+
+    public function testShowAllPostsAuthorized()
+    {
+        $request = $this->get('/v1/posts', ['Accept' => 'application/json', 'Authorization' => 'Bearer ' . $this->token]);
+        $data = $request->decodeResponseJson('data');
+
+        $this->assertEquals($data[0]['html'], $this->topic->posts()->first()->html);
+    }
+
+    public function testShowForbiddenWhenUnauthorized()
     {
         $request = $this->get('/v1/posts/' . $this->post->id, ['Accept' => 'application/json']);
 
         $request->assertForbidden();
+    }
 
-        $this->actingAs($this->user, 'api');
-
-        $request = $this->get('/v1/posts/' . $this->post->id, ['Accept' => 'application/json']);
+    public function testShowPostWhenAuthorized()
+    {
+        $request = $this->get('/v1/posts/' . $this->post->id, ['Accept' => 'application/json', 'Authorization' => 'Bearer ' . $this->token]);
 
         $request->assertJsonFragment([
             'user_name' => $this->post->user_name,
