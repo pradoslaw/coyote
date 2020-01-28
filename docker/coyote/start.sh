3d4eb09adca52cdd3840c938e299bfe1b95fa463
adam <adam@boduch.net>
2020-01-28 21:16:06 +0100

    obsluga xdebug

--- docker/coyote/start.sh
+++ docker/coyote/start.sh
@@ -18,10 +18,9 @@ if [[ "$ENABLE_XDEBUG" == "1" ]]; then
         echo "Xdebug enabled"
     else
         echo "Enabling xdebug"
-        echo "If you get this error, you can safely ignore it: /usr/local/bin/docker-php-ext-enable: line 83: nm: not found"
-        # see https://github.com/docker-library/php/pull/420
+
         docker-php-ext-enable xdebug
-        # see if file exists
+
         if [ -f $XdebugFile ]; then
             # See if file contains xdebug text.
             if grep -q xdebug.remote_enable "$XdebugFile"; then
@@ -29,11 +28,10 @@ if [[ "$ENABLE_XDEBUG" == "1" ]]; then
             else
                 echo "zend_extension=$(find /usr/local/lib/php/extensions/ -name xdebug.so)" >$XdebugFile # Note, single arrow to overwrite file.
                 echo "xdebug.remote_enable=1 " >>$XdebugFile
-                echo "xdebug.remote_host=host.docker.internal" >>$XdebugFile
-                echo "xdebug.remote_log=/tmp/xdebug.log" >>$XdebugFile
-                echo "xdebug.remote_autostart=false " >>$XdebugFile # I use the xdebug chrome extension instead of using autostart
-                # NOTE: xdebug.remote_host is not needed here if you set an environment variable in docker-compose like so `- XDEBUG_CONFIG=remote_host=192.168.111.27`.
-                #       you also need to set an env var `- PHP_IDE_CONFIG=serverName=docker`
+                echo "xdebug.remote_host=${XDEBUG_HOST}" >>$XdebugFile
+                echo "xdebug.remote_port=${XDEBUG_PORT}" >>$XdebugFile
+                echo "xdebug.profiler_enable_trigger=1" >>$XdebugFile
+                echo "xdebug.remote_autostart=false " >>$XdebugFile
             fi
         fi
     fi
