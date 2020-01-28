b9443c4e7b5380c363e8003aa1bc4f0ef6c1301f
adam <adam@boduch.net>
2020-01-28 18:08:40 +0100

    dodanie xdebug

--- docker/coyote/start.sh
+++ docker/coyote/start.sh
@@ -11,4 +11,37 @@ if [ ! -z "$DB_MIGRATE" ]; then
     php artisan route:cache
 fi
 
+XdebugFile='/usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini'
+
+if [[ "$ENABLE_XDEBUG" == "1" ]]; then
+    if [ -f $XdebugFile ]; then
+        echo "Xdebug enabled"
+    else
+        echo "Enabling xdebug"
+        echo "If you get this error, you can safely ignore it: /usr/local/bin/docker-php-ext-enable: line 83: nm: not found"
+        # see https://github.com/docker-library/php/pull/420
+        docker-php-ext-enable xdebug
+        # see if file exists
+        if [ -f $XdebugFile ]; then
+            # See if file contains xdebug text.
+            if grep -q xdebug.remote_enable "$XdebugFile"; then
+                echo "Xdebug already enabled... skipping"
+            else
+                echo "zend_extension=$(find /usr/local/lib/php/extensions/ -name xdebug.so)" >$XdebugFile # Note, single arrow to overwrite file.
+                echo "xdebug.remote_enable=1 " >>$XdebugFile
+                echo "xdebug.remote_host=host.docker.internal" >>$XdebugFile
+                echo "xdebug.remote_log=/tmp/xdebug.log" >>$XdebugFile
+                echo "xdebug.remote_autostart=false " >>$XdebugFile # I use the xdebug chrome extension instead of using autostart
+                # NOTE: xdebug.remote_host is not needed here if you set an environment variable in docker-compose like so `- XDEBUG_CONFIG=remote_host=192.168.111.27`.
+                #       you also need to set an env var `- PHP_IDE_CONFIG=serverName=docker`
+            fi
+        fi
+    fi
+else
+    if [ -f $XdebugFile ]; then
+        echo "Disabling Xdebug"
+        rm $XdebugFile
+    fi
+fi
+
 exec php-fpm
