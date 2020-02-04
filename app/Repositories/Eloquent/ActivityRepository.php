a5aab377495b97ac9c685ac332f2815fdb98491a
adam <adam@boduch.net>
2020-02-04 20:21:24 +0100

    nieuzywany kod

--- app/Repositories/Eloquent/ActivityRepository.php
+++ app/Repositories/Eloquent/ActivityRepository.php
@@ -4,8 +4,6 @@ namespace Coyote\Repositories\Eloquent;
 
 use Coyote\Activity;
 use Coyote\Repositories\Contracts\ActivityRepositoryInterface;
-use Coyote\Repositories\Criteria\EagerLoading;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class ActivityRepository extends Repository implements ActivityRepositoryInterface
 {
