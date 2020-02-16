33729f9463c04000e37af88cdaa46e4529010ee6
adam <adam@boduch.net>
2020-02-16 13:16:16 +0100

    #495

--- resources/assets/js/libs/notifications.js
+++ resources/assets/js/libs/notifications.js
@@ -1,6 +1,6 @@
 class DesktopNotifications {
   static requestPermission() {
-    if (this._isSupported() && Notification.permission !== 'granted') {
+    if (this._isSupported() && Notification.permission === 'default') {
       Notification.requestPermission(function (status) {
         if (Notification.permission !== status) {
           Notification.permission = status;
