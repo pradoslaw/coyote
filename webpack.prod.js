bca8e5a245034264388d09a281ef050b4d1d9831
adam <adam@boduch.net>
2020-02-19 19:04:43 +0100

    wyjatek

--- webpack.prod.js
+++ webpack.prod.js
@@ -24,6 +24,7 @@ module.exports = merge(common, {
       whitelist: [
         'footer-bubble',
         'line-numbers',
+        'line-numbers-rows',
         'token',
         'keyword',
         'comment',
