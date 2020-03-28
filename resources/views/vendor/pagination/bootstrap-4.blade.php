8a5e91f63c0ef433894f098b8b5efc6ea7f59d1d
adam <adam@boduch.net>
2020-03-28 08:50:02 +0100

    usuniecie nieaktywnych przyciskow z poczatku oraz konca

--- resources/views/vendor/pagination/bootstrap-4.blade.php
+++ resources/views/vendor/pagination/bootstrap-4.blade.php
@@ -1,9 +1,7 @@
 @if ($paginator->hasPages())
-    <ul class="pagination">
+    <ul class="pagination pagination-sm">
         {{-- Previous Page Link --}}
-        @if ($paginator->onFirstPage())
-            <li class="page-item disabled"><span class="page-link">&laquo;</span></li>
-        @else
+        @if (!$paginator->onFirstPage())
             <li class="page-item"><a class="page-link" href="{{ $paginator->previousPageUrl() }}" rel="prev">&laquo;</a></li>
         @endif
 
@@ -29,8 +27,6 @@
         {{-- Next Page Link --}}
         @if ($paginator->hasMorePages())
             <li class="page-item"><a class="page-link" href="{{ $paginator->nextPageUrl() }}" rel="next">&raquo;</a></li>
-        @else
-            <li class="page-item disabled"><span class="page-link">&raquo;</span></li>
         @endif
     </ul>
 @endif
