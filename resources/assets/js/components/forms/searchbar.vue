63e5a7c37ff09ef15626943581424e537bb24ece
adam <adam@boduch.net>
2020-04-04 12:52:36 +0200

    komponent vue

---
+++ resources/assets/js/components/forms/searchbar.vue
@@ -0,0 +1,134 @@
+<template>
+  <div v-on-clickaway="blurInput" class="nav-search">
+    <div :class="{'search-bar-active': isActive}" class="search-bar">
+      <i class="fas fa-search ml-2 mr-2"></i>
+
+      <form :action="url" role="search" class="flex-grow-1">
+        <input
+          @focus="showDropdown"
+          @keyup.esc="hideDropdown"
+          :value="query"
+          type="text"
+          name="q"
+          autocomplete="off"
+          placeholder="Kliknij, aby wyszukać"
+        >
+      </form>
+
+      <div v-if="isDropdownVisible" class="search-dropdown">
+<!--        <nav class="list-inline" style="margin: 10px 7px 10px 7px">-->
+<!--          <a class="list-inline-item active mr-2 text-primary" href="#" style="padding: 5px; font-size: 90%; border-bottom: 2px solid #80a41a">Forum</a>-->
+<!--          <a class="list-inline-item text-body" href="#" style="padding: 5px; font-size: 90%;">Praca</a>-->
+<!--        </nav>-->
+
+        <ul class="list-unstyled">
+          <li class="title"><span>Twoje wątki</span></li>
+
+          <li class="hover position-relative">
+            <a href="#">
+
+              <span>Które firmy IT dotknął kryzys? Sprawdź tutaj.</span> <small style="font-size: .65rem" class="text-muted">w Forum » Off-Topic</small>
+
+
+
+            </a>
+
+            <div class="position-absolute" style="right: 10px;  top: 20%;">
+              <i class="far fa-star ml-2"></i>
+              <i class="fas fa-search ml-2"></i>
+            </div>
+          </li>
+
+          <li>
+            <a href="#">
+
+              <span>Lorem ipsum lores.</span>
+              <small style="font-size: .65rem" class="text-muted">w Forum » Off-Topic</small>
+
+            </a>
+          </li>
+
+          <li class="more">
+            <a href="#">więcej ...</a>
+          </li>
+
+          <li class="title"><span>Twoje dyskusje</span></li>
+
+          <li>
+            <a href="#">
+
+              <span>Lorem ipsum lores.</span>
+
+
+            </a>
+          </li>
+
+          <li class="title"><span>Ostatnie wyszukiwania</span></li>
+
+          <li>
+            <a href="#">
+
+              <span>Lorem ipsum lores.</span>
+
+
+            </a>
+          </li>
+
+          <li class="title"><span>Użytkownicy</span></li>
+
+          <li class="position-relative">
+            <a href="#" class="d-flex align-content-center">
+              <object data="https://4programmers.net/uploads/photo/4ccbfa1158d19" style="width: 16px; height: 16px" type="image/png" class="media-object mr-2"><img src="/img/avatar.png"></object>
+              <span>Marooned</span>
+
+
+            </a>
+
+            <div class="position-absolute" style="right: 10px;  top: 20%;">
+              <i class="far fa-user ml-2"></i>
+              <i class="far fa-comment-alt ml-2"></i>
+              <i class="fas fa-search ml-2"></i>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import { mixin as clickaway } from 'vue-clickaway';
+
+  export default {
+    mixins: [clickaway],
+    props: {
+      url: {
+        type: String,
+        required: true
+      },
+      query: {
+        type: String
+      }
+    },
+    data() {
+      return {
+        isActive: false,
+        isDropdownVisible: false
+      }
+    },
+    methods: {
+      showDropdown() {
+        this.isActive = true;
+        this.isDropdownVisible = true;
+      },
+
+      hideDropdown() {
+        this.isDropdownVisible = false;
+      },
+
+      blurInput() {
+        this.isActive = false;
+      }
+    }
+  }
+</script>
--- resources/assets/js/plugins/sociale.js
+++ resources/assets/js/plugins/sociale.js
@@ -1,16 +1,21 @@
-import Config from '../libs/config';
 import Vue from 'vue';
 import VueNotification from '../components/notifications/notifications.vue';
+import VueSearchbar from '../components/forms/searchbar.vue';
 import VuePm from '../components/pm/inbox.vue';
 import store from '../store';
 import axios from "axios";
 import { default as setToken } from "../libs/csrf";
 
-const el = document.getElementById('nav-auth');
+let el = document.getElementById('js-searchbar');
+
+const SearchbarWrapper = Vue.extend(VueSearchbar);
+el.appendChild(new SearchbarWrapper({props: {url: el.dataset.url, query: el.dataset.query}}).$mount().$el);
+
+el = document.getElementById('nav-auth');
 
 if (el !== null) {
-  store.commit('inbox/init', Config.get('pm_unread'));
-  store.commit('notifications/init', {notifications: null, count: Config.get('notifications_unread')});
+  store.commit('inbox/init', store.state.user.pm_unread);
+  store.commit('notifications/init', {notifications: null, count: store.state.user.notifications_unread});
 
   const NotificationWrapper = Vue.extend(VueNotification);
   const PmWrapper = Vue.extend(VuePm);
@@ -19,13 +24,19 @@ if (el !== null) {
   el.appendChild(new PmWrapper().$mount().$el);
 }
 
-const sessionHandler = () => {
-  axios.get('/ping').then(response => setToken(response.data));
-};
+/**
+ * setInterval() handler. Retrieves CSRF token
+ *
+ * @returns {Promise<void>}
+ */
+const sessionHandler = () => axios.get('/ping').then(response => setToken(response.data));
 
-const keepSessionAlive = () => {
-  return setInterval(sessionHandler, 4 * 60 * 1000);
-};
+/**
+ * Keep session alive every 4 minutes
+ *
+ * @returns {number}
+ */
+const keepSessionAlive = () => setInterval(sessionHandler, 4 * 60 * 1000);
 
 let timer = keepSessionAlive();
 
