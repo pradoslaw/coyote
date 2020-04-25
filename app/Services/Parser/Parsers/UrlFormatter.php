5f4362add0f974661cacf4171eb0a15fc58b679b
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2020-04-25 17:43:01 +0200

    Clean UrlFormatter

--- app/Services/Parser/Parsers/UrlFormatter.php
+++ app/Services/Parser/Parsers/UrlFormatter.php
@@ -7,10 +7,10 @@ use Collective\Html\HtmlBuilder;
 class UrlFormatter
 {
     // http://daringfireball.net/2010/07/improved_regex_for_matching_urls
-    const REGEXP_URL = '#(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:\'".,<>?«»“”‘’]))#u';
+    private const REGEXP_URL = '#(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:\'".,<>?«»“”‘’]))#u';
 
-    const TITLE_LEN = 64;
-    const TITLE_DOTS = '[...]';
+    private const TITLE_LEN = 64;
+    private const TITLE_DOTS = '[...]';
 
     /** @var string */
     private $host;
@@ -53,15 +53,9 @@ class UrlFormatter
         return $text;
     }
 
-    /**
-     * @param string $text
-     * @param int $length
-     * @param string $dots
-     * @return string
-     */
-    private function truncate(string $text, int $length = self::TITLE_LEN, string $dots = self::TITLE_DOTS): string
+    private function truncate(string $text): string
     {
-        if (mb_strlen($text) < $length) {
+        if (mb_strlen($text) < self::TITLE_LEN) {
             return $text;
         }
 
@@ -69,10 +63,10 @@ class UrlFormatter
             return $text;
         }
 
-        $padding = ($length - mb_strlen($dots)) / 2;
+        $padding = (self::TITLE_LEN - mb_strlen(self::TITLE_DOTS)) / 2;
 
         $result = mb_substr($text, 0, $padding);
-        $result .= $dots;
+        $result .= self::TITLE_DOTS;
         $result .= mb_substr($text, -$padding);
 
         return $result;
