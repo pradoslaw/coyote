bcffb24aa4a4145b45753790748d7f80616302a4
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2020-05-06 19:41:35 +0200

    Migrate HelperTest from Codeception to PhpUnit

--- tests/Feature/Helpers/HelpersTest.php
+++ tests/Feature/Helpers/HelpersTest.php
@@ -9,6 +9,27 @@ class HelpersTest extends TestCase
     /**
      * @test
      */
+    public function testExcerpt()
+    {
+        $this->assertEquals('Praca w oparciu o "kontrakt" (czyli umowa B2B)', excerpt('Praca w oparciu o "kontrakt" (czyli umowa B2B)'));
+        $this->assertEquals('<xxx>', excerpt('&lt;xxx&gt;'));
+    }
+
+    /**
+     * @test
+     */
+    public function testPlain()
+    {
+        $this->assertEquals('"test"', plain('<b>"test"</b>'));
+        $this->assertEquals("'test'", plain("<b>'test'</b>"));
+        $this->assertEquals("a > b", plain("a > b"));
+        $this->assertEquals('<xxx>', plain('&lt;xxx&gt;'));
+        $this->assertEquals('&lt;xxx&gt;', plain('&amp;lt;xxx&amp;gt;'));
+    }
+
+    /**
+     * @test
+     */
     public function shouldGetKeywords()
     {
         // given
