828f51ed50995ad5f8a47c6ec51356072f054a0d
adam <adam@boduch.net>
2020-05-12 09:03:40 +0200

    bugfix: pobieranie nieprawidlowego ID usera

--- app/Http/Controllers/Profile/HomeController.php
+++ app/Http/Controllers/Profile/HomeController.php
@@ -142,7 +142,7 @@ class HomeController extends Controller
         /** @var Builder $builder */
         $builder = app(Builder::class);
 
-        $microblogs = $builder->forUser($this->auth)->orderById()->onlyMine()->paginate();
+        $microblogs = $builder->forUser($user)->orderById()->onlyMine()->paginate();
 
         return view('profile.partials.microblog', [
             'user'          => $user,
