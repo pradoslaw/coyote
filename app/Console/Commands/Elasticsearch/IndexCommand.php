982cf7330a779289155cfe83472196402a50cbc6
adam <adam@boduch.net>
2020-05-14 18:39:09 +0200

    indeksujemy poczawszy od ostatnich wynikow bo te sa wazniejsze

--- app/Console/Commands/Elasticsearch/IndexCommand.php
+++ app/Console/Commands/Elasticsearch/IndexCommand.php
@@ -88,7 +88,7 @@ class IndexCommand extends Command
         $model = $this->app->make($className);
         $this->line("Indexing $className ...");
 
-        $builder = $model->select()->orderBy('id', 'desc');
+        $builder = $model->select()->orderBy('id', 'asc');
         $objectName = get_class($model);
 
         // ugly hack for job offers...
