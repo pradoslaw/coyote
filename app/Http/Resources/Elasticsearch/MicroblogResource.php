679123fa465e48586f9947f784b42a1be84940a8
adam <adam@boduch.net>
2020-05-24 16:06:32 +0200

    indeksowanie komentarzy mikroblo

--- app/Http/Resources/Elasticsearch/MicroblogResource.php
+++ app/Http/Resources/Elasticsearch/MicroblogResource.php
@@ -7,9 +7,11 @@ use Coyote\Services\UrlBuilder\UrlBuilder;
 /**
  * @property int $id
  * @property int $user_id
+ * @property int $parent_id
  * @property \Carbon\Carbon $created_at
  * @property \Carbon\Carbon $updated_at
  * @property string $html
+ * @property \Coyote\Microblog $comments
  */
 class MicroblogResource extends ElasticsearchResource
 {
@@ -23,12 +25,13 @@ class MicroblogResource extends ElasticsearchResource
     {
         return [
             'id'            => $this->id,
-            'url'           => UrlBuilder::microblog($this->resource),
+            'url'           => $this->parent_id ? UrlBuilder::microblogComment($this->resource) : UrlBuilder::microblog($this->resource),
             'created_at'    => $this->created_at->toIso8601String(),
             'updated_at'    => $this->updated_at->toIso8601String(),
             'decay_date'    => $this->created_at->toIso8601String(),
             'text'          => strip_tags($this->html),
-            'user_id'       => $this->user_id
+            'user_id'       => $this->user_id,
+            'children'      => MicroblogResource::collection($this->comments)
         ];
     }
 }
