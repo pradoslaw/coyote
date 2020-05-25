d885ff188708cfc07d93abb6994747df31b01a89
adam <adam@boduch.net>
2020-05-25 07:19:58 +0200

    bugfix w testach

--- app/Listeners/TopicListener.php
+++ app/Listeners/TopicListener.php
@@ -2,14 +2,12 @@
 
 namespace Coyote\Listeners;
 
-
 ini_set('memory_limit', '10G');
 set_time_limit(0);
 
 use Coyote\Events\TopicWasDeleted;
 use Coyote\Events\TopicWasMoved;
 use Coyote\Events\TopicWasSaved;
-use Coyote\Post;
 use Coyote\Repositories\Contracts\TopicRepositoryInterface as TopicRepository;
 use Coyote\Services\Elasticsearch\Crawler;
 use Illuminate\Contracts\Queue\ShouldQueue;
