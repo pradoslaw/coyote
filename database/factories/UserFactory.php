44c93f6d777713bb29e75d3e07026046b9e2e3fa
adam <adam@boduch.net>
2020-10-01 08:33:33 +0200

    wieksza losowosc nazw uzytkownikow

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -4,7 +4,7 @@ use Faker\Generator as Faker;
 
 $factory->define(\Coyote\User::class, function (Faker $faker) {
     return [
-        'name' => $faker->userName,
+        'name' => $faker->userName . $faker->randomDigit,
         'email' => $faker->unique()->safeEmail,
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm',
         'is_confirm' => true,
