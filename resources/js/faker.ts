8e510caa53b35c824ac6641b78912b818b8d5b12
adam <adam@boduch.net>
2020-10-04 18:20:22 +0200

    poprawa testow

--- resources/js/faker.ts
+++ resources/js/faker.ts
@@ -1,4 +1,4 @@
-import {Post} from "./types/models";
+import { Post, Topic } from "./types/models";
 const faker = require('faker');
 
 export function post(props?: any): Post {
@@ -26,5 +26,18 @@ export function post(props?: any): Post {
     text: lorem,
     updated_at: new Date(),
     url: "",
+  }, props);
+}
+
+export function topic(props?: any): Topic {
+  const lorem = faker.lorem.text();
+
+  return Object.assign({
+    id: faker.random.number(),
+    subject: faker.lorem.string(),
+    is_locked: false,
+    is_read: false,
+    is_sticky: false,
+    first_post_id: faker.random.number()
   }, props);
 }
