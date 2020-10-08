09bd47a90d035ed6b4eb0126589f882b771c25a0
adam <adam@boduch.net>
2020-10-08 18:34:08 +0200

    usuniety zbedny kod

--- database/seeds/TopicsTableSeeder.php
+++ database/seeds/TopicsTableSeeder.php
@@ -11,19 +11,13 @@ class TopicsTableSeeder extends Seeder
      */
     public function run()
     {
+        \Coyote\Post\Log::reguard();
+
         factory(\Coyote\Topic::class, 50)->create()->each(function (\Coyote\Topic $topic) {
             for ($i = 1; $i <= rand(1, 10); $i++) {
                 $user = \Coyote\User::inRandomOrder()->first();
 
-                $post = $topic->posts()->save(factory(\Coyote\Post::class)->make(['forum_id' => $topic->forum_id, 'user_id' => $user->id]));
-                $post->logs()->create([
-                    'user_id' => $user->id,
-                    'subject' => $topic->subject,
-                    'text' => $post->text,
-                    'ip' => $post->ip,
-                    'host' => $post->host,
-                    'browser' => $post->browser
-                ]);
+                $topic->posts()->save(factory(\Coyote\Post::class)->make(['forum_id' => $topic->forum_id, 'user_id' => $user->id]));
             }
         });
     }
