11944b00ab9bf4f6d38c8cc09769dcd5aed45d4c
adam <adam@boduch.net>
2020-10-22 18:36:53 +0200

    refaktoryzacja

--- resources/js/components/thumbnail.vue
+++ resources/js/components/thumbnail.vue
@@ -1,19 +1,21 @@
 <template>
-  <div class="img-thumbnail editable-thumbnail">
-    <img v-if="url" :src="url" class="mw-100">
-    <div v-else class="d-block bg-light img-placeholder"></div>
+  <div class="editable-thumbnail">
+    <div class="position-relative img-thumbnail">
+      <img v-if="url" :src="url" class="mw-100">
+      <div v-else class="d-block bg-light img-placeholder"></div>
 
-    <a v-if="url" class="flush" @click="remove">
-      <i class="fas fa-times fa-2x"></i>
-    </a>
+      <a v-if="url" href="javascript:" class="flush text-decoration-none" @click="remove">
+        <i class="fas fa-times fa-2x"></i>
+      </a>
 
-    <div v-if="isPending" class="spinner">
-      <i class="fas fa-spinner fa-spin fa-2x"></i>
+      <div v-if="isPending" class="spinner">
+        <i class="fas fa-spinner fa-spin fa-2x"></i>
+      </div>
+
+      <a v-if="!url && !isPending" href="javascript:" class="upload text-decoration-none" @click="openDialog">
+        <i class="fas fa-plus-circle fa-2x"></i>
+      </a>
     </div>
-
-    <a v-if="!url && !isPending" class="upload" @click="openDialog">
-      <i class="fas fa-plus-circle fa-2x"></i>
-    </a>
 
     <input type="file" ref="input" @change="upload">
 
@@ -69,7 +71,6 @@
             this.error = error.response.data.message;
 
             this.$refs.error.open();
-
           })
           .finally(() => this.isPending = false);
       },
@@ -79,5 +80,4 @@
       }
     }
   }
-
 </script>
