7dcfb435f624c6acc0f2103c338918ff8308b7da
adam <adam@boduch.net>
2020-11-01 14:26:40 +0100

    kod przepisany na vanilla js

--- resources/js/legacy/state.js
+++ resources/js/legacy/state.js
@@ -1,32 +1,12 @@
-$(function() {
-    'use strict';
+document.addEventListener('submit', function (event) {
+  let button = event.target.querySelector('[data-submit-state]');
 
-    $.fn.disable = function() {
-        if (this.attr('disabled') === 'disabled') {
-            return;
-        }
+  if (!button) {
+    return;
+  }
 
-        let origin = this.html();
-        let text = origin;
+  const text = button.dataset.submitState;
 
-        if (typeof this.data('submit-state') !== 'undefined') {
-            text = this.data('submit-state');
-        }
-
-        this.html(`<i class="fa fa-spinner fa-spin fa-fw"></i> ${text}`).attr({'disabled': 'disabled', 'data-origin': origin});
-    };
-
-    $.fn.enable = function() {
-        this.html(this.data('origin')).removeAttr('disabled').removeAttr('data-origin');
-    };
-
-    $(document).on('submit', 'form', function() {
-        $(':submit', this).each(function () {
-            let submit = $(this);
-
-            if (typeof submit.data('submit-state') !== 'undefined') {
-                submit.disable();
-            }
-        });
-    });
+  button.innerHTML = `<i class="fa fa-spinner fa-spin fa-fw"></i> ${text}`;
+  button.classList.toggle('disabled');
 });
