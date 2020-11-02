9056caa38420fa73e56b40a35a08edb7901ce51b
adam <adam@boduch.net>
2020-11-02 19:59:18 +0100

    wyswietlanie informacji o sponsorowanym wpisie

--- app/Http/Resources/MicroblogResource.php
+++ app/Http/Resources/MicroblogResource.php
@@ -6,6 +6,9 @@ class MicroblogResource extends Api\MicroblogResource
 {
     public function toArray($request)
     {
-        return parent::toArray($request) + ['metadata' => encrypt(['permission' => 'microblog-delete', 'microblog_id' => $this->resource->id])];
+        return parent::toArray($request) + [
+            'is_sponsored' => $this->resource->is_sponsored,
+            'metadata' => encrypt(['permission' => 'microblog-delete', 'microblog_id' => $this->resource->id])
+        ];
     }
 }
