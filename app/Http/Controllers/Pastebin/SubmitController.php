72a2b315b42e60c1c8464f56c85a5ee1b658ee95
adam <adam@boduch.net>
2020-11-11 08:15:37 +0100

    usuniecie mozliwosci zapisu w pastebin

--- app/Http/Controllers/Pastebin/SubmitController.php
+++ app/Http/Controllers/Pastebin/SubmitController.php
@@ -3,10 +3,7 @@
 namespace Coyote\Http\Controllers\Pastebin;
 
 use Coyote\Http\Controllers\Controller;
-use Coyote\Http\Forms\PastebinForm;
 use Coyote\Repositories\Contracts\PastebinRepositoryInterface as PastebinRepository;
-use Coyote\Services\Stream\Objects\Pastebin as Stream_Pastebin;
-use Coyote\Services\Stream\Activities\Create as Stream_Create;
 
 class SubmitController extends Controller
 {
@@ -23,23 +20,5 @@ class SubmitController extends Controller
         parent::__construct();
 
         $this->pastebin = $pastebin;
-    }
-
-    /**
-     * Zapis tresci pastebin do bazy danych
-     *
-     * @param PastebinForm $form
-     * @return \Illuminate\Http\RedirectResponse
-     */
-    public function save(PastebinForm $form)
-    {
-        $pastebin = $this->transaction(function () use ($form) {
-            $pastebin = $this->pastebin->create(['user_id' => $this->userId] + $form->all());
-            stream(Stream_Create::class, (new Stream_Pastebin())->map($pastebin));
-
-            return $pastebin;
-        });
-
-        return redirect()->route('pastebin.show', [$pastebin->id])->with('success', 'Wpis został prawidłowo dodany.');
     }
 }
