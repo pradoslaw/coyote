0fb218ee850d42074e010abfe7eb3d25699fa472
adam <adam@boduch.net>
2020-11-20 07:50:22 +0100

    pobieranie rowniez skasowanych userow

--- app/Models/Microblog/Vote.php
+++ app/Models/Microblog/Vote.php
@@ -50,6 +50,6 @@ class Vote extends Model
      */
     public function user()
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class)->withTrashed();
     }
 }
--- app/Models/Post/Vote.php
+++ app/Models/Post/Vote.php
@@ -43,6 +43,6 @@ class Vote extends Model
 
     public function user()
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class)->withTrashed();
     }
 }
