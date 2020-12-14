8998fb209c3c466a404dfe2a89b2bd017a3df0a5
adam <adam@boduch.net>
2020-12-14 20:09:43 +0100

    test websocket

--- tests/Browser/MicroblogTest.php
+++ tests/Browser/MicroblogTest.php
@@ -5,28 +5,55 @@ namespace Tests\Browser;
 use Coyote\Microblog;
 use Coyote\User;
 use Faker\Factory;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
-use Illuminate\Support\Facades\Hash;
+use Illuminate\Foundation\Testing\WithFaker;
 use Laravel\Dusk\Browser;
 use Tests\DuskTestCase;
 
 class MicroblogTest extends DuskTestCase
 {
+    use WithFaker;
+
     public function testSubmitValid()
     {
         $user = factory(User::class)->create();
 
         $this->browse(function (Browser $browser) use ($user) {
             $faker = Factory::create();
+            $text = $faker->text();
 
             $browser
                 ->loginAs($user)
                 ->visit('/Mikroblogi')
-                ->type('text', $text = $faker->text())
+                ->type('text', $text)
                 ->press('Zapisz')
                 ->waitForText($text)
                 ->assertSee($text)
                 ->assertInputValueIsNot('text', $text);
+
+            $browser->logout();
+        });
+    }
+
+    public function testSubmitComment()
+    {
+        $microblog = factory(Microblog::class)->create();
+
+        $this->browse(function (Browser $browser, Browser $browser2) use ($microblog) {
+            $user = factory(User::class)->create();
+            $text = $this->faker->text();
+
+            $browser2->loginAs($user)->visit('/Mikroblogi/View/' . $microblog->id)->waitForText($microblog->text);
+
+            $browser
+                ->loginAs($user)
+                ->visit('/Mikroblogi/View/' . $microblog->id)
+                ->type('text', $text)
+                ->click('.btn-comment-submit')
+                ->waitForText($text)
+                ->assertSee($text)
+                ->assertInputValueIsNot('text', $text);
+
+            $browser2->waitForText($text, 10);
 
             $browser->logout();
         });
