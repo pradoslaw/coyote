15ae62a9b5770631f5a27c6193c76dbe45369c46
adam <adam@boduch.net>
2020-12-27 11:30:06 +0100

    usuniecie niepotrzebnych danych

---
+++ database/migrations/2020_12_27_110458_drop_host_in_posts_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class DropHostInPostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->dropColumn('host');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->string('host')->nullable();
+        });
+    }
+}
---
+++ database/migrations/2020_12_27_112311_drop_settings_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class DropSettingsTable extends Migration
+{
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::drop('settings');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::create('settings', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestampTz('created_at')->default(DB::raw('CURRENT_TIMESTAMP(0)'));
+            $table->string('name');
+            $table->text('value');
+            $table->integer('user_id')->nullable();
+            $table->string('session_id')->nullable();
+
+            $table->index(['name', 'user_id', 'session_id']);
+        });
+    }
+}
