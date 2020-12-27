3fc8063fd9cc7e0ddd4db9f3fd7f26bfc9f80daa
adam <adam@boduch.net>
2020-12-27 11:36:34 +0100

    usuniecie kolumny

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -6,7 +6,6 @@ $factory->define(\Coyote\Post::class, function (Faker $faker) {
     return [
         'text' => $faker->realText(),
         'ip' => $ip = $faker->ipv4,
-        'host' => $ip,
         'browser' => $faker->userAgent,
         'user_name' => $faker->userName,
         'created_at' => \Carbon\Carbon::now(),
