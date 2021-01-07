66f6b27c66eb1fca1441d41dc5809504caa5987c
adam <adam@boduch.net>
2021-01-07 17:07:10 +0100

    refaktoryzacja

--- resources/js/components/microblog/microblog.vue
+++ resources/js/components/microblog/microblog.vue
@@ -17,7 +17,7 @@
               <small v-if="microblog.is_sponsored" class="text-muted small">&bull; Sponsorowane</small>
             </div>
 
-            <vue-tags :tags="microblog.tags" class="flex-shrink-1 ml-auto mr-1 tag-clouds-md"></vue-tags>
+            <vue-tags :tags="microblog.tags" class="flex-shrink-1 ml-auto text-truncate mr-1 tag-clouds-md"></vue-tags>
 
             <div v-if="isAuthorized" class="dropdown">
               <button class="btn btn-xs border-0 text-muted" type="button" data-toggle="dropdown" aria-label="Dropdown"><i class="fa fa-ellipsis-h"></i></button>
