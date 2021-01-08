a89195e2f77fcb66c2f26b4c0f3cbf0186b0d718
adam <adam@boduch.net>
2021-01-08 17:14:18 +0100

    test

---
+++ tests/Browser/ApiTokenTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Browser;
+
+use Coyote\User;
+use Illuminate\Foundation\Testing\WithFaker;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class ApiTokenTest extends DuskTestCase
+{
+    use WithFaker;
+
+    public function testCreateToken()
+    {
+        $user = factory(User::class)->create();
+
+        $this->browse(function (Browser $browser) use ($user) {
+            $browser
+                ->loginAs($user)
+                ->visit('/User/Tokens')
+                ->type('token', $token = $this->faker->word)
+                ->press('Dodaj')
+                ->waitForText('Twój nowy token')
+                ->press('OK')
+                ->assertSee($token);
+
+            $browser->logout();
+        });
+    }
+}
