cb42a714b2094dc0893ae3a981782b566ad1338b
adam <adam@boduch.net>
2021-01-11 09:04:28 +0100

    wyswietlanie pola rowniez na malych ekranach

--- resources/js/components/microblog/form.vue
+++ resources/js/components/microblog/form.vue
@@ -13,7 +13,6 @@
         <div class="row no-gutters p-1">
           <vue-tags-inline
             :tags="microblog.tags"
-            class="d-none d-md-flex"
             placeholder="...inny? kliknij, aby wybrać tag"
             @change="toggleTag"
           ></vue-tags-inline>
