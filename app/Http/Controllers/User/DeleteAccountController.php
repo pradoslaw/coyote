bdc079e1deeecd381f928b76d776f7bb9cdc0da3
adam <adam@boduch.net>
2021-01-29 09:18:44 +0100

    skasowanie konta musi wylogowywac usera

--- app/Http/Controllers/User/DeleteAccountController.php
+++ app/Http/Controllers/User/DeleteAccountController.php
@@ -6,11 +6,17 @@ use Coyote\Events\UserDeleted;
 use Coyote\Rules\PasswordCheck;
 use Coyote\Services\Stream\Activities\Delete;
 use Coyote\Services\Stream\Objects\Person;
+use Illuminate\Contracts\Auth\Guard;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rule;
 
 class DeleteAccountController extends BaseController
 {
+    use AuthenticatesUsers{
+        logout as traitLogout;
+    }
+
     use HomeTrait;
 
     /**
@@ -26,7 +32,7 @@ class DeleteAccountController extends BaseController
      * @return \Illuminate\Http\RedirectResponse
      * @throws \Illuminate\Validation\ValidationException
      */
-    public function delete(Request $request)
+    public function delete(Request $request, Guard $guard)
     {
         $this->validate($request, [
             'password' => [
@@ -38,7 +44,22 @@ class DeleteAccountController extends BaseController
             ]
         ]);
 
-        $this->transaction(function () {
+        $this->auth->timestamps = false;
+        $this->auth->forceFill([
+            'ip'        => $request->ip(),
+            'browser'   => $request->browser(),
+            'visits'    => $this->auth->visits + 1,
+            'visited_at'=> now(),
+            'is_online' => false
+        ]);
+
+        $this->transaction(function () use ($request, $guard) {
+            $this->auth->save();
+
+            $request->session()->invalidate();
+            $request->session()->regenerateToken();
+
+            $guard->logout();
             $this->auth->delete();
 
             event(new UserDeleted($this->auth));
