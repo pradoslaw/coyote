7770d1030b070e3a3feb6c5eedaf5dee2393fa77
adam <adam@boduch.net>
2021-02-04 16:31:27 +0100

    zmniejszony margines

--- resources/js/components/job/job.vue
+++ resources/js/components/job/job.vue
@@ -25,7 +25,7 @@
           >
           </vue-salary>
 
-          <p class="pb-1 border-bottom" style="clear: left">
+          <p class="pb-1 mb-2 border-bottom" style="clear: left">
             <a class="employer" :title="'Zobacz oferty pracy z ' + job.firm.name" :href="job.firm.url">{{ job.firm.name }}</a>
 
             <vue-location :locations="job.locations" :remote="job.remote"></vue-location>
--- tests/Unit/Resources/Elasticsearch/JobResourceTest.php
+++ tests/Unit/Resources/Elasticsearch/JobResourceTest.php
@@ -5,11 +5,19 @@ namespace Tests\Unit\Resources\Elasticsearch;
 use Coyote\Http\Resources\Elasticsearch\JobResource;
 use Coyote\Job;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class JobResourceTest extends TestCase
 {
-    use DatabaseTransactions;
+    use DatabaseTransactions, WithFaker;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        JobResource::withoutWrapping();
+    }
 
     public function testMapModelWithFirm()
     {
@@ -17,7 +25,6 @@ class JobResourceTest extends TestCase
 
         $this->assertNotNull($job->firm_id);
 
-        JobResource::withoutWrapping();
         $result = JobResource::make($job)->toResponse(request())->getData(true);
 
         $this->assertNotEmpty($result['firm']);
@@ -27,13 +34,30 @@ class JobResourceTest extends TestCase
         $this->assertEquals($job->firm->name, $result['firm']['name']);
     }
 
+    public function testMapModelWithFirmLogo()
+    {
+        /** @var Job $job */
+        $job = factory(Job::class)->state('firm')->create();
+
+        $filename = uniqid() . '.jpg';
+
+        $job->firm->setAttribute('logo', $filename);
+        $job->firm->save();
+
+        $this->assertNotNull($job->firm->logo->getFilename());
+
+        $result = JobResource::make($job)->toResponse(request())->getData(true);
+
+        $this->assertNotEmpty($result['firm']['logo']);
+        $this->assertStringContainsString($filename, $result['firm']['logo']);
+    }
+
     public function testMapModelWithoutFirm()
     {
         $job = factory(Job::class)->create();
 
         $this->assertNull($job->firm_id);
 
-        JobResource::withoutWrapping();
         $result = JobResource::make($job)->toResponse(request())->getData(true);
 
         $this->assertArrayNotHasKey('firm', $result);
