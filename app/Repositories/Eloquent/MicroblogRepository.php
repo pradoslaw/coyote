febd69f304d26112ca13cc9f049e3c363e8c3e9b
adam <adam@boduch.net>
2021-02-05 10:51:09 +0100

    bugfix: niewyswietlamy zablokowanych userow

--- app/Repositories/Eloquent/MicroblogRepository.php
+++ app/Repositories/Eloquent/MicroblogRepository.php
@@ -184,7 +184,7 @@ class MicroblogRepository extends Repository implements MicroblogRepositoryInter
             ->join('microblogs', 'users.id', '=', 'user_id')
             ->when($userId, function ($builder) use ($userId) {
                 return $builder->where('user_id', '!=', $userId)->whereNotIn('user_id', function ($query) use ($userId) {
-                    return $query->select('related_user_id')->from('user_relations')->where('user_id', $userId)->where('is_blocked', false);
+                    return $query->select('related_user_id')->from('user_relations')->where('user_id', $userId);
                 });
             })
             ->limit(5)
