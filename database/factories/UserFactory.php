69d9a2d3a9e27c10c09fdb717fbde2f3f2460604
adam <adam@boduch.net>
2021-08-04 07:32:58 +0200

    wieksza losowosc loginow

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -6,7 +6,7 @@ use Faker\Generator as Faker;
 
 $factory->define(\Coyote\User::class, function (Faker $faker) {
     return [
-        'name' => $faker->userName . $faker->randomDigit,
+        'name' => $faker->userName . $faker->randomNumber(1000),
         'email' => $faker->unique()->safeEmail,
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm',
         'is_confirm' => true,
