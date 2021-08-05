8b0256bb84fe74d982648d10863bd2f6fcb23d82
adam <adam@boduch.net>
2021-08-05 07:46:53 +0200

    bugfix

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -6,7 +6,7 @@ use Faker\Generator as Faker;
 
 $factory->define(\Coyote\User::class, function (Faker $faker) {
     return [
-        'name' => $faker->userName . $faker->randomNumber(1000),
+        'name' => $faker->userName . $faker->randomNumber(3),
         'email' => $faker->unique()->safeEmail,
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm',
         'is_confirm' => true,
