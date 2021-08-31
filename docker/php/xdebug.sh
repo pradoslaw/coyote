6e49fff7baa83afd67e05c198cb48a1354091a1a
adam <adam@boduch.net>
2021-08-31 12:27:38 +0200

    upgrade

--- docker/php/xdebug.sh
+++ docker/php/xdebug.sh
@@ -16,10 +16,11 @@ if [[ "$ENABLE_XDEBUG" == "1" ]]; then
                 echo "Xdebug already enabled... skipping"
             else
                 echo "zend_extension=$(find /usr/local/lib/php/extensions/ -name xdebug.so)" >$XdebugFile # Note, single arrow to overwrite file.
-                echo "xdebug.remote_enable=1 " >>$XdebugFile
-                echo "xdebug.remote_host=${XDEBUG_HOST}" >>$XdebugFile
-                echo "xdebug.remote_port=${XDEBUG_PORT}" >>$XdebugFile
-                echo "xdebug.profiler_enable_trigger=1" >>$XdebugFile
+                echo "xdebug.mode=debug " >>$XdebugFile
+                echo "xdebug.client_host=${XDEBUG_HOST}" >>$XdebugFile
+                echo "xdebug.client_port=${XDEBUG_PORT}" >>$XdebugFile
+                echo "xdebug.mode=profile" >>$XdebugFile
+                echo "xdebug.start_with_request=trigger" >>$XdebugFile
                 echo "xdebug.remote_autostart=false " >>$XdebugFile
             fi
         fi
