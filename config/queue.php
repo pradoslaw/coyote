87cae51e59131a8406df400f8655d673d7cd4829
adam <adam@boduch.net>
2021-09-01 20:35:20 +0200

    dodatkowe zabezpieczenie na wypadek gdyby transakcja jeszcze sie nie zakonczyla

--- config/queue.php
+++ config/queue.php
@@ -39,33 +39,13 @@ return [
             'queue'  => 'default',
             'expire' => 60,
         ],
-        'beanstalkd' => [
-            'driver' => 'beanstalkd',
-            'host'   => 'localhost',
-            'queue'  => 'default',
-            'ttr'    => 60
-        ],
-        'sqs'        => [
-            'driver' => 'sqs',
-            'key'    => 'your-public-key',
-            'secret' => 'your-secret-key',
-            'queue'  => 'your-queue-url',
-            'region' => 'us-east-1',
-        ],
-        'iron'       => [
-            'driver'  => 'iron',
-            'host'    => 'mq-aws-us-east-1.iron.io',
-            'token'   => 'your-token',
-            'project' => 'your-project-id',
-            'queue'   => 'your-queue-name',
-            'encrypt' => true,
-        ],
         'redis'      => [
             'driver' => 'redis',
             'queue'  => 'default',
             'expire' => null,
             'retry_after' => 90,
             'block_for' => null,
+            'after_commit' => true
         ],
 
     ],
