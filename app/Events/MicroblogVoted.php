0c8c5a844dc6ba967deae5f88fe89d153bc70649
adam <adam@boduch.net>
2021-09-08 09:40:16 +0200

    brakujace traits

--- app/Events/MicroblogVoted.php
+++ app/Events/MicroblogVoted.php
@@ -3,10 +3,15 @@
 namespace Coyote\Events;
 
 use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
 
 class MicroblogVoted implements ShouldBroadcast
 {
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
     public function __construct(public $payload)
     {
     }
