3f2decd19178a9d3e6330c18e5d31aa13bd75806
adam <adam@boduch.net>
2021-09-14 13:21:32 +0200

    refaktoryzacja + nie rzucamy 500 w przypadku blednej walidacji

--- app/Models/Scopes/Sortable.php
+++ app/Models/Scopes/Sortable.php
@@ -10,36 +10,35 @@ trait Sortable
      * Filter order/direction values an apply it to the query
      *
      * @param Builder $builder
-     * @param $sort
-     * @param $order
+     * @param $defaultColumn
+     * @param $defaultDirection
      * @param array|null $accept
      * @param array $alias
      * @return mixed
      */
-    public function scopeSortable(Builder $builder, $sort, $order, array $accept = null, array $alias = [])
+    public function scopeSortable(Builder $builder, $defaultColumn, $defaultDirection, array $accept = null, array $alias = [])
     {
-        if (request()->anyFilled(['sort', 'order'])) {
-            $order = request('order') == 'asc' ? 'ASC' : 'DESC';
-            $sort = request('sort');
+        $direction = request('order', $defaultDirection) == 'asc' ? 'ASC' : 'DESC';
+        $column = request('sort', $defaultColumn);
 
-            if ($accept !== null) {
-                if (!in_array($sort, $accept)) {
-                    abort(500);
-                }
+        if (request()->anyFilled(['sort', 'order'])) {
+            if ($accept !== null && !in_array($column, $accept)) {
+                $column = $defaultColumn;
             }
+
 
             // column can has a value. for example we want to filter by column topics.last_post_id
             // we don't want to pass such a long column name in query string. instead  we can set up an alias
             // like last that can be mapped as topics.last_post_id
             if (!empty($alias)) {
                 foreach ($alias as $col => $val) {
-                    if ($sort === $col) {
-                        $sort = $val;
+                    if ($column === $col) {
+                        $column = $val;
                     }
                 }
             }
         }
 
-        return $builder->orderBy($sort, $order);
+        return $builder->orderBy($column, $direction);
     }
 }
