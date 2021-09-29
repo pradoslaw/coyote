fb3b8c5b6c0b6aca788a2e1c843c0ee59e1b5979
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2021-09-29 02:12:58 +0200

    Clean UrlFormatter

--- app/Services/Parser/Parsers/UrlFormatter.php
+++ app/Services/Parser/Parsers/UrlFormatter.php
@@ -4,15 +4,15 @@ namespace Coyote\Services\Parser\Parsers;
 
 use Collective\Html\HtmlBuilder;
 use Coyote\Services\Parser\Parsers\Parentheses\ParenthesesParser;
+use TRegx\CleanRegex\Match\Details\Detail;
 use TRegx\CleanRegex\Pattern;
-use TRegx\SafeRegex\preg;
 
 class UrlFormatter
 {
     // http://daringfireball.net/2010/07/improved_regex_for_matching_urls
     // modified - removed part with parsing of parentheses, because that's
     // done by ParenthesesParser
-    private const REGEXP_URL = '~\b(?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)[^\s<>]+[^\s`!(\[\]{};:\'".,<>?«»“”‘’]~i';
+    private const REGEXP_URL = '\b(?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)[^\s<>]+[^\s`!(\[\]{};:\'".,<>?«»“”‘’]';
     private const TITLE_LEN = 64;
 
     /** @var string */
@@ -21,17 +21,20 @@ class UrlFormatter
     private $html;
     /** @var ParenthesesParser */
     private $parser;
+    /** @var Pattern */
+    private $urlPattern;
 
     public function __construct(string $host, HtmlBuilder $html, ParenthesesParser $parser)
     {
         $this->host = $host;
         $this->html = $html;
         $this->parser = $parser;
+        $this->urlPattern = Pattern::of(self::REGEXP_URL, 'i');
     }
 
     public function parse(string $text): string
     {
-        return preg::replace_callback(self::REGEXP_URL, fn (array $match) => $this->processLink($match[0]), $text);
+        return $this->urlPattern->replace($text)->callback(fn(Detail $match) => $this->processLink($match->text()));
     }
 
     private function processLink(string $url): string
@@ -41,7 +44,7 @@ class UrlFormatter
 
     private function buildLink(string $url): string
     {
-        if (Pattern::pcre()->of(self::REGEXP_URL)->test($url)) {
+        if ($this->urlPattern->test($url)) {
             return $this->html->link($this->prependSchema($url), $this->truncate($url));
         }
 
