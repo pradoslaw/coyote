13d5afd3f456ac7e403b99a1a65367f4772781bb
adam <adam@boduch.net>
2021-10-04 19:40:30 +0200

    bugfix: zdublowane powiadomienia

--- app/Services/Notification/Notification.php
+++ app/Services/Notification/Notification.php
@@ -44,44 +44,47 @@ abstract class Notification extends BaseNotification implements NotificationInte
     {
         $this->broadcastChannel = $user->receivesBroadcastNotificationsOn();
 
-        /** @var \Illuminate\Support\Collection $channels */
-        $channels = $user->notificationSettings()->select('channel')->where('type_id', static::ID)->where('is_enabled', true)->pluck('channel');
+        /** @var array $channels */
+        $channels = $user->notificationSettings()->select('channel')->where('type_id', static::ID)->where('is_enabled', true)->pluck('channel')->toArray();
 
         if (empty($channels)) {
             return [];
         }
 
-        if ($channels->contains(Model::DB) && $this instanceof ShouldBroadcast) {
-            $channels->push('broadcast');
+        // extra channel: broadcast notification via web socket
+        if (in_array(Model::DB, $channels) && $this instanceof ShouldBroadcast) {
+            $channels[] = 'broadcast';
         }
 
-        if ($channels->contains(Model::MAIL) && !$user->canReceiveEmail()) {
-            $channels->forget(Model::MAIL);
+        // remove mail channel from the list if user can't get emails (email address is not verified)
+        if (in_array(Model::MAIL, $channels) && !$user->canReceiveEmail()) {
+            $channels = $this->forget($channels, Model::MAIL);
         }
 
         // do not send another notification if previous was not yet read
         if (!empty($user->getUnreadNotification($this->objectId()))) {
-            $channels->forget([Model::MAIL, Model::PUSH]);
+            $channels = $this->forget($channels, Model::MAIL, Model::PUSH, 'broadcast');
         }
 
         return $this->resolveChannels($channels);
     }
 
     /**
-     * @param \Illuminate\Support\Collection $channels
+     * @param array $channels
      * @return array
      */
-    private function resolveChannels($channels): array
+    private function resolveChannels(array $channels): array
     {
         $replacement = [Model::DB => DatabaseChannel::class, Model::PUSH => WebPushChannel::class];
 
         foreach ($replacement as $channel => $class) {
-            if ($channels->contains($channel)) {
-                $channels = $channels->reject(fn ($value) => $value === $channel)->push($class);
+            if (in_array($channel, $channels)) {
+                $channels = $this->forget($channels, $channel);
+                $channels[] = $class;
             }
         }
 
-        return $channels->toArray();
+        return $channels;
     }
 
     /**
@@ -90,5 +93,16 @@ abstract class Notification extends BaseNotification implements NotificationInte
     protected function notificationUrl()
     {
         return route('user.notifications.url', [$this->id]);
+    }
+
+    private function forget($channels, ...$values)
+    {
+        foreach ($values as $value) {
+            if (($key = array_search($value, $channels)) !== false) {
+                unset($channels[$key]);
+            }
+        }
+
+        return $channels;
     }
 }
