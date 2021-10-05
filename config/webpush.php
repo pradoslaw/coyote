9469611a0b9b2ec902f49bcf2f1e8c9cb26026dc
adam <adam@boduch.net>
2021-10-05 07:50:20 +0200

    konfiguracja secrets

--- config/webpush.php
+++ config/webpush.php
@@ -7,10 +7,10 @@ return [
      * These keys must be safely stored and should not change.
      */
     'vapid' => [
-        'subject' => env('VAPID_SUBJECT'),
-        'public_key' => env('VAPID_PUBLIC_KEY'),
-        'private_key' => env('VAPID_PRIVATE_KEY'),
-        'pem_file' => env('VAPID_PEM_FILE'),
+        'subject' => env('VAPID_SUBJECT', docker_secret('VAPID_SUBJECT_FILE')),
+        'public_key' => env('VAPID_PUBLIC_KEY', docker_secret('VAPID_PUBLIC_KEY_FILE')),
+        'private_key' => env('VAPID_PRIVATE_KEY', docker_secret('VAPID_PRIVATE_KEY_FILE')),
+        'pem_file' => env('VAPID_PEM_FILE', docker_secret('VAPID_PEM_FILE_FILE')),
     ],
 
     /**
