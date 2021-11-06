ef345ebf5be2177b59dc0690d0139ac938b2e4fa
adam <adam@boduch.net>
2021-11-06 13:04:48 +0100

    brakujacy z-index na produkcji

--- resources/js/components/popover.vue
+++ resources/js/components/popover.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div style="z-index: 1">
     <transition name="fade">
       <div :class="placement" class="alert alert-warning alert-dismissible mb-0">
         <button @click="closeMessage" type="button" class="close" data-dismiss="alert" aria-label="Close" title="Kliknij, aby zamknąć">
@@ -13,33 +13,32 @@
 </template>
 
 <script lang="ts">
+  import Vue from 'vue';
+  import Component from "vue-class-component";
+  import { Prop } from "vue-property-decorator";
+  import Session from '../libs/session';
 
-import Vue from 'vue';
-import Component from "vue-class-component";
-import { Prop } from "vue-property-decorator";
-import Session from '../libs/session';
+  type Placement = 'top' | 'bottom' | 'left' | 'right' | 'top-start';
 
-type Placement = 'top' | 'bottom' | 'left' | 'right' | 'top-start';
+  @Component
+  export default class Popover extends Vue {
+    @Prop()
+    readonly message!: string;
 
-@Component
-export default class Popover extends Vue {
-  @Prop()
-  readonly message!: string;
+    @Prop({default: 'bottom'})
+    readonly placement!: Placement;
 
-  @Prop({default: 'bottom'})
-  readonly placement!: Placement;
+    closeMessage() {
+      let popover = JSON.parse(Session.getItem('popover', '[]'));
+      popover.push(this.message);
 
-  closeMessage() {
-    let popover = JSON.parse(Session.getItem('popover', '[]'));
-    popover.push(this.message);
+      Session.setItem('popover', JSON.stringify(popover));
 
-    Session.setItem('popover', JSON.stringify(popover));
+      // destroy the vue listeners, etc
+      this.$destroy();
 
-    // destroy the vue listeners, etc
-    this.$destroy();
-
-    // remove the element from the DOM
-    this.$el.parentNode!.removeChild(this.$el);
+      // remove the element from the DOM
+      this.$el.parentNode!.removeChild(this.$el);
+    }
   }
-}
 </script>
