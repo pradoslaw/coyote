4a8562e8e44c42514043316144fc99c219d9548b
adam <adam@boduch.net>
2021-11-13 16:10:56 +0100

    zmiana sortowania

--- app/Repositories/Eloquent/GuideRepository.php
+++ app/Repositories/Eloquent/GuideRepository.php
@@ -18,7 +18,13 @@ class GuideRepository extends Repository implements GuideRepositoryInterface
     public function paginate($perPage = null, $columns = ['*'], $pageName = 'page', $page = null)
     {
         return $this->applyCriteria(function () use ($perPage) {
-            return $this->model->select(['id', 'title', 'created_at', 'user_id', 'views', 'votes', 'role'])->with(['user', 'tags'])->withCount('comments')->paginate($perPage);
+            return $this
+                ->model
+                ->select(['id', 'title', 'created_at', 'user_id', 'views', 'votes', 'role'])
+                ->with(['user', 'tags'])
+                ->withCount('comments')
+                ->orderByDesc('id')
+                ->paginate($perPage);
         });
     }
 }
