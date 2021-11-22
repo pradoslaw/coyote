e7e1934b71a95151946d9121885a9913cd5aa7d8
adam <adam@boduch.net>
2021-11-22 17:04:44 +0100

    dodanie bledu do ignorowanych

--- resources/js/sentry.ts
+++ resources/js/sentry.ts
@@ -4,6 +4,6 @@ import environment from '@/environment';
 Sentry.init({
   dsn: environment.sentryDsn,
   release: environment.release,
-  ignoreErrors: ['Network Error', 'Non-Error promise rejection captured'],
+  ignoreErrors: ['Network Error', 'Non-Error promise rejection captured', 'Request failed with status code 422'],
   tracesSampleRate: 0.0,
 });
