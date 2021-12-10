f6a078e93179824059ac4a8729308e3d42eb6975
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2021-12-10 20:56:22 +0100

    Integrate 4play editor

---
+++ resources/js/components/forms/editor.vue
@@ -0,0 +1,62 @@
+<template>
+  <div ref="view" style="min-height: 94px;">
+  </div>
+</template>
+
+<script>
+  import { Editor4Play } from "@riddled/4play/src/Editor.js";
+
+  export default {
+    props: {
+      value: {type: String, required: true},
+      placeholder: {type: String, require: true},
+      autocompleteSource: {type: Function, required: true},
+      smartPaste: {type: Boolean, required: true},
+    },
+
+    data() {
+      return {
+        editor: null
+      };
+    },
+
+    mounted() {
+      this.editor = new Editor4Play(
+        this.$refs.view,
+        this.placeholder,
+        this.value,
+        {
+          onChange: content => this.$emit('input', content),
+          onTextPaste: pasteAsMarkdown => pasteAsMarkdown(this.smartPaste),
+          onImagePaste: file => this.$emit('image', file),
+          onSubmit: content => this.$emit('submit', content),
+          onCancel: () => this.$emit('cancel'),
+          onStateChange: state => this.$emit('state', state),
+        },
+        username => this.autocompleteSource(username)
+      );
+    },
+
+    methods: {
+      insertImage(href, title) {
+        this.editor.insertImage(href, title);
+      },
+      makeBold() {
+        this.editor.makeBold();
+      },
+      makeItalics() {
+        this.editor.makeItalics();
+      },
+      makeStrikeThrough() {
+        this.editor.makeStrikeThrough();
+      },
+      focus() {
+        this.editor.focus();
+      }
+    },
+
+    destroy() {
+      this.editor.destroy();
+    }
+  };
+</script>
--- resources/js/components/forms/markdown.vue
+++ resources/js/components/forms/markdown.vue
@@ -3,63 +3,42 @@
     <vue-tabs @click="switchTab" :items="tabs" :current-tab="tabs.indexOf(currentTab)" type="pills" class="mb-2">
       <div v-if="isContent" class="btn-toolbar ml-auto mt-2 mt-sm-0">
         <div class="btn-group mr-2" role="group" aria-label="...">
-          <button @click="insertAtCaret('**', '**')" type="button" class="btn btn-sm" title="Pogrubienie"><i class="fas fa-bold fa-fw"></i></button>
-          <button @click="insertAtCaret('*', '*')" type="button" class="btn btn-sm" title="Kursywa"><i class="fas fa-italic fa-fw"></i></button>
-          <button @click="insertAtCaret('<u>', '</u>')" type="button" class="btn btn-sm" title="Przekreślenie"><i class="fas fa-underline fa-fw"></i></button>
-          <button @click="insertAtCaret('<br>', '## ')" type="button" class="btn btn-sm" data-open="<br>" data-close="## " title="Nagłówek h2"><i class="fas fa-heading fa-fw"></i></button>
-          <button @click="insertCitation" type="button" class="btn btn-sm btn-quote" title="Wstaw cytat"><i class="fas fa-quote-left fa-fw"></i></button>
-        </div>
-
-        <div class="btn-group mr-2" role="group">
-          <button @click="insertAtCaret('`', '`')" type="button" class="btn btn-sm" title="Instrukcja kodu"><i class="fas fa-text-width fa-fw"></i></button>
-          <button @click="insertAtCaret('```<br>', '<br>```')" type="button" class="btn btn-sm" title="Kod źródłowy"><i class="fas fa-code fa-fw"></i></button>
-          <button @click="toggleDropdown" type="button" class="btn btn-sm dropdown-toggle" aria-haspopup="true" aria-expanded="false">
-            <span class="caret"></span>
+          <button @click="makeBold" type="button" class="btn btn-sm"
+                  :title="state.canBold ? 'Dodaj pogrubienie' : 'Dodanie tutaj pogrubienia mogłoby uszkodzić składnię'"
+                  :style="{opacity: state.canBold ? '1.0' : '0.4', cursor: state.canStrikeThrough ? 'pointer' : 'default'}">
+            <i class="fas fa-bold fa-fw"></i>
           </button>
-
-          <vue-dropdown ref="dropdown" class="dropdown-menu select-menu">
-            <div class="select-menu-search">
-              <input v-model="searchText" @keyup.esc="hideDropdown" ref="search" type="text" class="form-control form-control-sm" placeholder="Filtruj..." autocomplete="off">
-            </div>
-
-            <div class="select-menu-wrapper">
-              <ul class="list-unstyled">
-                <li v-for="(value, key) in filteredLanguages">
-                  <a @click="insertAtCaret('```' + key + '<br>', '<br>```<br>')">{{ value }}</a>
-                </li>
-              </ul>
-            </div>
-          </vue-dropdown>
-        </div>
-
-        <div class="btn-group mr-2" role="group">
-          <button @click="insertAtCaret('<br>', '* ')" type="button" class="btn btn-sm" title="Wypunktowanie"><i class="fas fa-list-ul fa-fw"></i></button>
-          <button @click="insertAtCaret('<br>', '1. ')" type="button" class="btn btn-sm" title="Numerowanie"><i class="fas fa-list-ol fa-fw"></i></button>
-          <button @click="insertAtCaret('![', '](http://)')" type="button" class="btn btn-sm" title="Obraz"><i class="fas fa-image fa-fw"></i></button>
-          <button @click="insertAtCaret('[', '](http://)')" type="button" class="btn btn-sm" title="Link"><i class="fas fa-link fa-fw"></i></button>
-          <button @click="insertAtCaret('<br>Nagłówek 1 | Nagłówek 2<br>---------------- | -------------------<br>Kolumna1 | Kolumna 2<br>', '')" type="button" class="btn btn-sm" title="Tabela"><i class="fas fa-table fa-fw"></i></button>
+          <button @click="makeItalics" type="button" class="btn btn-sm"
+                  :title="state.canItalics ? 'Dodaj kursywę' : 'Dodanie tutaj kursywy mogłoby uszkodzić składnię'"
+                  :style="{opacity: state.canItalics ? '1.0' : '0.4', cursor: state.canStrikeThrough ? 'pointer' : 'default'}">
+            <i class="fas fa-italic fa-fw"></i>
+          </button>
+          <button @click="makeStrikeThrough" type="button" class="btn btn-sm"
+                  :title="state.canStrikeThrough ? 'Dodaj przekreślenie' : 'Dodanie tutaj przekreślenia mogłoby uszkodzić składnię'"
+                  :style="{opacity: state.canStrikeThrough ? '1.0' : '0.4', cursor: state.canStrikeThrough ? 'pointer' : 'default'}">
+            <i class="fas fa-strikethrough fa-fw"></i>
+          </button>
+          <label style="color:grey; align-self:center; margin: 3px 0 0;" title='"Smart paste" wkleja linki jako markdown'>
+            <input type="checkbox" v-model="smartPaste">
+            Smart paste
+          </label>
         </div>
       </div>
     </vue-tabs>
 
     <vue-prompt v-show="isContent" :source="promptUrl">
-      <textarea
-        v-autosize
-        v-model="valueLocal"
-        v-paste:success="addAsset"
-        v-paste:error="errorNotification"
-        :class="{'is-invalid': error !== null}"
-        :tabindex="tabIndex"
-        @keydown.ctrl.enter="save"
-        @keydown.meta.enter="save"
-        @keydown.esc="cancel"
-        name="text"
-        class="form-control"
-        ref="input"
-        rows="4"
-        placeholder="Kliknij, aby dodać treść"
-      ></textarea>
-
+      <div :class="['form-control', {'is-invalid': error !== null}]" style="height:inherit; outline:none; box-shadow:none; border:none;">
+        <vue-editor
+          ref="editor"
+          v-model="valueLocal"
+          placeholder="Kliknij, aby dodać treść..."
+          :autocompleteSource="autocomplete"
+          :smartPaste="smartPaste"
+          @submit="save"
+          @cancel="cancel"
+          @state="updateState"
+          @image="image => log(image)"/>
+      </div>
       <vue-error :message="error"></vue-error>
     </vue-prompt>
 
@@ -77,7 +56,8 @@
           <span class="small">{{ progress }}%</span>
         </template>
 
-        <a v-else :aria-label="uploadTooltip" tabindex="-1" data-balloon-length="large" data-balloon-pos="up-left" data-balloon-nofocus href="javascript:" class="small text-muted" @click="chooseFile">
+        <a v-else :aria-label="uploadTooltip" tabindex="-1" data-balloon-length="large" data-balloon-pos="up-left"
+           data-balloon-nofocus href="javascript:" class="small text-muted" @click="chooseFile">
           <i class="far fa-image"></i>
 
           <span class="d-none d-sm-inline">Kliknij, aby dodać załącznik lub wklej ze schowka.</span>
@@ -87,13 +67,15 @@
       </div>
 
       <div class="small ml-auto">
-        <a href="#js-wiki-help" tabindex="-1" data-bs-toggle="collapse" class="small text-muted"><i class="fa fab fa-markdown"></i> Markdown jest obsługiwany.</a>
+        <a href="#js-wiki-help" tabindex="-1" data-bs-toggle="collapse" class="small text-muted">
+          <i class="fa fab fa-markdown"></i> Markdown jest obsługiwany.</a>
       </div>
     </div>
 
     <div v-if="assets.length" class="row pt-3 pb-3">
       <div v-for="item in assets" :key="item.id" class="col-sm-2">
-        <vue-thumbnail :url="item.url" @delete="deleteAsset(item)" @insert="insertAssetAtCaret(item)" :aria-label="item.name" data-balloon-pos="down" name="asset"></vue-thumbnail>
+        <vue-thumbnail :url="item.url" @delete="deleteAsset(item)" @insert="insertAssetAtCaret(item)"
+                       :aria-label="item.name" data-balloon-pos="down" name="asset"></vue-thumbnail>
       </div>
     </div>
 
@@ -104,13 +86,15 @@
           <div class="card-body">
             <h2>Pogrubienie, kursywa...</h2>
 
-            <p>Możesz używać pogrubienia czy kursywy, aby usprawnić czytelność tekstu: <code>**to jest pogrubienie**</code>, a to
+            <p>Możesz używać pogrubienia czy kursywy, aby usprawnić czytelność tekstu: <code>**to jest
+              pogrubienie**</code>, a to
               <code>*kursywa*</code>.
             </p>
 
             <h2>Kod źródłowy</h2>
 
-            <p>Wszelkie jednolinijkowe instrukcje języka programowania (fragmenty kodu) powinny być zawarte pomiędzy obrócone
+            <p>Wszelkie jednolinijkowe instrukcje języka programowania (fragmenty kodu) powinny być zawarte pomiędzy
+              obrócone
               apostrofy
               lub podwójny cudzysłów, czyli: <code>`kod instrukcji języka programowania`</code>.</p>
 
@@ -134,8 +118,10 @@
 
             <h2>Wypunktowanie i numerowanie</h2>
 
-            <p>Możliwe jest tworzenie listy numerowanych oraz wypunktowanych. Wystarczy, że pierwszym znakiem linii będzie
-              <code>*</code> lub <code>1. </code></p>
+            <p>
+              Możliwe jest tworzenie listy numerowanych oraz wypunktowanych. Wystarczy, że pierwszym znakiem linii
+              będzie <code>*</code> lub <code>1. </code>
+            </p>
 
             <p>
               <code>1. Lista numerowana</code><br/>
@@ -152,7 +138,8 @@
 
             <p>URL umieszczony w tekście zostanie przez system automatycznie wykryty i zamieniony na znacznik <code>
               &lt;a&gt;</code>.<br/>
-              Jeżeli chcesz, możesz samodzielnie sformatować link: <code>&lt;a href="http://4programmers.net">kliknij tutaj&lt;/a&gt;</code>
+              Jeżeli chcesz, możesz samodzielnie sformatować link: <code>&lt;a href="http://4programmers.net">kliknij
+                tutaj&lt;/a&gt;</code>
             </p>
 
             <p>Możesz umieścić odnośnik do wewnętrznej podstrony, używając następującej składnii: <code>[[Delphi/Kompendium]]</code>
@@ -161,11 +148,14 @@
             <h2>Znaczniki HTML</h2>
 
             <p>Dozwolone jest używanie podstawowych znaczników HTML: &lt;a&gt;, &lt;b&gt;, &lt;i&gt;, &lt;del&gt;, &lt;strong&gt;,
-              &lt;tt&gt;, &lt;dfn&gt;, &lt;ins&gt;, &lt;pre&gt;, &lt;blockquote&gt;, &lt;hr&gt;, &lt;sub&gt;, &lt;sup&gt;, &lt;img&gt;</p>
+              &lt;tt&gt;, &lt;dfn&gt;, &lt;ins&gt;, &lt;pre&gt;, &lt;blockquote&gt;, &lt;hr&gt;, &lt;sub&gt;, &lt;sup&gt;,
+              &lt;img&gt;</p>
 
             <h2>Indeks górny oraz dolny</h2>
 
-            <p>Przykład: wpisując <code>m&lt;sub&gt;2&lt;/sub&gt;,, i m&lt;sup&gt;2&lt;/sup&gt;</code> otrzymasz: m<sub>2</sub> i m<sup>2</sup>.</p>
+            <p>Przykład: wpisując <code>m&lt;sub&gt;2&lt;/sub&gt;,, i m&lt;sup&gt;2&lt;/sup&gt;</code> otrzymasz:
+              m<sub>2</sub>
+              i m<sup>2</sup>.</p>
 
             <h2>Składnia Tex</h2>
 
@@ -184,18 +174,19 @@
   import Component from "vue-class-component";
   import { Asset } from '@/types/models';
   import { Ref, Prop, Emit, Watch } from "vue-property-decorator";
-  import { default as Textarea, languages } from '../../libs/textarea';
+  import { languages } from '../../libs/textarea';
   import { mixin as clickaway } from 'vue-clickaway';
   import { default as mixin } from '../mixins/form';
-  import VueDropdownMenu from '../dropdown-menu.vue';
   import VueAutosize from '../../plugins/autosize';
   import VuePrompt from '../forms/prompt.vue';
   import VueTabs from '../tabs.vue';
   import VueThumbnail from "../thumbnail.vue";
   import VueError from '../forms/error.vue';
+  import VueEditor from './editor.vue';
   import axios from 'axios';
   import Prism from 'prismjs';
   import IsImage from '../../libs/assets';
+  import { Editor4Play } from "@riddled/4play/src/Editor.js";
 
   const CONTENT = 'Treść';
   const PREVIEW = 'Podgląd';
@@ -203,32 +194,35 @@
   Vue.use(VueAutosize);
 
   @Component({
-    mixins: [ clickaway, mixin ],
+    mixins: [clickaway, mixin],
     components: {
-      'vue-dropdown': VueDropdownMenu,
-      'vue-prompt': VuePrompt ,
+      'vue-prompt': VuePrompt,
       'vue-tabs': VueTabs,
       'vue-thumbnail': VueThumbnail,
-      'vue-error': VueError
+      'vue-error': VueError,
+      'vue-editor': VueEditor
     },
   })
   export default class VueMarkdown extends Vue {
-    textarea!: Textarea;
     searchText: string = '';
     previewHtml: string = '';
     currentTab: string = CONTENT;
     isProcessing = false;
     progress = 0;
     tabs: string[] = [CONTENT, PREVIEW];
+    state = {
+      canBold: false,
+      canItalics: false,
+      canStrikeThrough: false,
+      canList: false
+    }
+    smartPaste = false;
 
-    @Ref('input')
-    readonly input!: HTMLTextAreaElement;
+    @Ref('editor')
+    readonly editor!: Editor4Play;
 
     @Ref('search')
     readonly search!: HTMLInputElement;
-
-    @Ref('dropdown')
-    readonly dropdown!: VueDropdownMenu;
 
     @Prop()
     value!: string;
@@ -261,10 +255,12 @@
     readonly isInvalid!: boolean;
 
     @Emit('save')
-    save() {}
+    save() {
+    }
 
     @Emit('cancel')
-    cancel() {}
+    cancel() {
+    }
 
     @Emit('paste')
     addAsset(asset: Asset) {
@@ -276,7 +272,11 @@
     }
 
     insertAssetAtCaret(asset: Asset) {
-      this.insertAtCaret((IsImage(asset.name!) ? '!' : '') + '[' + asset.name + '](' + asset.url + ')', '');
+      if (IsImage(asset.name!)) {
+        this.editor.insertImage(asset.url, asset.name);
+      } else {
+        // is this ever called for non-images?
+      }
     }
 
     @Watch('value')
@@ -286,12 +286,23 @@
       }
     }
 
-    deleteAsset(asset: Asset) {
-      this.assets.splice(this.assets.indexOf(asset), 1);
+    updateState(state) {
+      this.state.canBold = state.canBold;
+      this.state.canItalics = state.canItalics;
+      this.state.canStrikeThrough = state.canStrikeThrough;
+      this.state.canList = state.canList;
     }
 
-    mounted() {
-      this.textarea = new Textarea(this.input);
+    autocomplete(nick) {
+      return Promise.resolve([]);
+    }
+
+    log(a) {
+      console.log(a);
+    }
+
+    deleteAsset(asset: Asset) {
+      this.assets.splice(this.assets.indexOf(asset), 1);
     }
 
     chooseFile() {
@@ -308,37 +319,28 @@
       Thumbnail.openDialog();
     }
 
-    insertAtCaret(startsWith, endsWith) {
-      this.textarea.insertAtCaret(startsWith.replace(/<br>/g, "\n"), endsWith.replace(/<br>/g, "\n"), this.textarea.isSelected() ? this.textarea.getSelection() : '');
-
-      if (this.dropdown.isDropdownVisible) {
-        this.hideDropdown();
-      }
-
+    makeBold() {
+      this.editor.makeBold();
       this.updateModel();
     }
 
-    insertCitation() {
-      this.textarea.insertAtCaret('> ', '', this.textarea.getSelection().replace(/\r\n/g, "\n").replace(/\n/g, "\n> "));
-
+    makeItalics() {
+      this.editor.makeItalics();
       this.updateModel();
     }
 
-    toggleDropdown() {
-      this.dropdown.toggle();
-      this.$nextTick(() => this.search.focus());
-    }
-
-    hideDropdown() {
-      this.dropdown.toggle();
+    makeStrikeThrough() {
+      this.editor.makeStrikeThrough();
+      this.updateModel();
     }
 
     updateModel() {
-      this.input.dispatchEvent(new Event('input', {'bubbles': true}));
+      // this.input.dispatchEvent(new Event('input', {'bubbles': true}));
+      // mam bubblować event w górę? Czy to było tylko pod <textarea>?
     }
 
     focus() {
-      this.input.focus();
+      this.editor.focus();
     }
 
     switchTab(index: number) {
