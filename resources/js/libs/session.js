b46726ba6df968f6555017b9bb51586840873a98
adam <adam@boduch.net>
2021-12-16 14:20:46 +0100

    obsluga bledow

--- resources/js/libs/session.js
+++ resources/js/libs/session.js
@@ -1,51 +1,45 @@
-class Session
-{
-    static isSupported() {
-        return ('localStorage' in window && window['localStorage'] !== null);
+class Session {
+  static isSupported() {
+    try {
+      return ('localStorage' in window && window['localStorage'] !== null);
+    } catch (err) {
+      console.log(err);
+
+      return false;
+    }
+  }
+
+  static setItem(key, value) {
+    if (this.isSupported()) {
+      localStorage.setItem(key, value);
+    }
+  }
+
+  static getItem(key, _default = null) {
+    let value = null;
+
+    if (this.isSupported()) {
+      value = localStorage.getItem(key);
     }
 
-    static setItem(key, value) {
-        if (this.isSupported()) {
-            try {
-                localStorage.setItem(key, value);
-            }
-            catch (e) {
-                console.error(e);
-            }
-        }
+    return value === null ? _default : value;
+  }
+
+  static removeItem(key) {
+    if (this.isSupported()) {
+      localStorage.removeItem(key);
     }
+  }
 
-    static getItem(key, _default = null) {
-        let value = null;
-
-        if (this.isSupported()) {
-            try {
-                value = localStorage.getItem(key);
-            }
-            catch (e) {
-                console.error(e);
-            }
-        }
-
-        return value === null ? _default : value;
+  static addListener(callback) {
+    if (this.isSupported()) {
+      try {
+        window.addEventListener('storage', callback, true);
+      } catch (e) {
+        console.error(e);
+      }
     }
-
-    static removeItem(key) {
-        if (this.isSupported()) {
-            localStorage.removeItem(key);
-        }
-    }
-
-    static addListener(callback) {
-        if (this.isSupported()) {
-            try {
-                window.addEventListener('storage', callback, true);
-            }
-            catch (e) {
-                console.error(e);
-            }
-        }
-    }
+  }
 }
 
 module.exports = Session;
