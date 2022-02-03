981bf32d062e81e1edceeeef0471c6d2f7607446
Daniel <wilkowski.kontakt@gmail.com>
2022-02-03 01:42:16 +0100

    Update help.vue

--- resources/js/components/forms/help.vue
+++ resources/js/components/forms/help.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="markdown-help">
     <div class="card card-info">
-      <div class="card-header">Pomoc <span class="card-version">1.9.7</span></div>
+      <div class="card-header">Pomoc <span class="card-version">1.9.8</span></div>
       <div class="card-body">
         <div class="row">
           <div class="col-md-6">
