77bdd8f4e778f3cfdca89828841ebd6c1256e7d3
adam <adam@boduch.net>
2022-02-08 10:15:49 +0100

    łamanie linii dozwolone w stopkach

--- app/Services/Parser/Factories/SigFactory.php
+++ app/Services/Parser/Factories/SigFactory.php
@@ -4,5 +4,8 @@ namespace Coyote\Services\Parser\Factories;
 
 class SigFactory extends CommentFactory
 {
-    //
+    protected function getHtmlTags(): string
+    {
+        return implode(',', array_merge($this->htmlTags, ['br']));
+    }
 }
