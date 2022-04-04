9464db3b5cec5fae51fb2ab107dbcda4c2d5c68b
adam <adam@boduch.net>
2022-04-04 18:58:08 +0200

    dodanie losowej litery aby zmniejszyc ryzyko powstania zdbulowanego tagu

--- database/factories/TagFactory.php
+++ database/factories/TagFactory.php
@@ -5,7 +5,7 @@
 use Faker\Generator as Faker;
 
 $factory->define(\Coyote\Tag::class, function (Faker $faker) {
-    $name = $faker->word() . $faker->randomDigitNotNull;
+    $name = $faker->word() . $faker->randomLetter;
 
     return [
         'name' => strtolower($name),
