665d9cfebf1bd8fb0829058526c1dfe26007d75c
adam <adam@boduch.net>
2022-04-10 17:25:57 +0200

    zmiana limitu trsci komentarza zapisywanego do dziennika

--- app/Services/Stream/Objects/Comment.php
+++ app/Services/Stream/Objects/Comment.php
@@ -7,6 +7,8 @@ use Coyote\Comment as Model;
 
 class Comment extends ObjectAbstract
 {
+    private const EXCERPT_SIZE = 1024;
+
     /**
      * @param array ...$args
      * @return $this
@@ -33,7 +35,7 @@ class Comment extends ObjectAbstract
     {
         $this->id = $microblog->id;
         $this->url = UrlBuilder::microblogComment($microblog);
-        $this->displayName = excerpt($microblog->html);
+        $this->displayName = excerpt($microblog->html, self::EXCERPT_SIZE);
     }
 
     /**
@@ -44,7 +46,7 @@ class Comment extends ObjectAbstract
     private function post($post, $comment, $topic)
     {
         $this->id = $comment->id;
-        $this->displayName = excerpt($comment->html);
+        $this->displayName = excerpt($comment->html, self::EXCERPT_SIZE);
         $this->url = UrlBuilder::topic($topic) . '?p=' . $post->id . '#comment-' . $comment->id;
     }
 
@@ -55,14 +57,14 @@ class Comment extends ObjectAbstract
     private function wiki($wiki, $comment)
     {
         $this->id = $comment->id;
-        $this->displayName = excerpt($comment->html);
+        $this->displayName = excerpt($comment->html, self::EXCERPT_SIZE);
         $this->url = UrlBuilder::wikiComment($wiki, $comment->id);
     }
 
     public function comment(Model $comment)
     {
         $this->id = $comment->id;
-        $this->displayName = excerpt($comment->html);
+        $this->displayName = excerpt($comment->html, self::EXCERPT_SIZE);
         $this->url = UrlBuilder::url($comment->resource) . '#comment-' . $comment->id;
 
         return $this;
