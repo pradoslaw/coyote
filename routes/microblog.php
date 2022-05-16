7040d07afdce8ca6638c009a8cb985150299f548
adam <adam@boduch.net>
2022-05-16 11:29:52 +0200

    jeden wpis na 1h

--- routes/microblog.php
+++ routes/microblog.php
@@ -4,7 +4,7 @@
 /** @var $this \Illuminate\Routing\Router */
 $this->group(['namespace' => 'Microblog', 'prefix' => 'Mikroblogi', 'as' => 'microblog.'], function () {
     $this->get('/', ['uses' => 'HomeController@index', 'as' => 'home']);
-    $this->post('Edit/{microblog?}', ['uses' => 'SubmitController@save', 'as' => 'save', 'middleware' => ['auth', 'throttle.submission:1,5']]);
+    $this->post('Edit/{microblog?}', ['uses' => 'SubmitController@save', 'as' => 'save', 'middleware' => ['auth', 'throttle.submission:1,60']]);
     $this->post('Preview', ['uses' => 'SubmitController@preview', 'middleware' => 'auth']);
 
     $this->get('View/{id}', ['uses' => 'HomeController@show', 'as' => 'view']);
