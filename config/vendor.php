8956b47f58b6d74ce2444502667423c485fb4ff2
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2023-09-13 07:46:55 +0200

    Update company address

--- config/vendor.php
+++ config/vendor.php
@@ -1,11 +1,11 @@
 <?php
 
 return [
-    'name' => 'Makana Sp. z o.o.',
-    'address' => 'Stoszowska 23',
-    'city' => 'Wrocław',
-    'postal_code' => '54-060',
-    'vat_id' => 'PL894-313-94-60',
-    'country' => 'Polska',
+    'name'             => 'Makana Sp. z o.o.',
+    'address'          => 'Krupnicza 13',
+    'city'             => 'Wrocław',
+    'postal_code'      => '50-075',
+    'vat_id'           => 'PL894-313-94-60',
+    'country'          => 'Polska',
     'default_vat_rate' => 1.23
 ];
