93827ba8886e711f627b33d5d0e518119dacb483
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2023-10-03 12:57:37 +0200

    Remove "Coyote" menu item

--- config/laravel-menu.php
+++ config/laravel-menu.php
@@ -40,10 +40,6 @@ return [
                 'CSS' => ['url' => 'CSS']
             ]
         ],
-        'Coyote' => [
-            'url' => 'https://github.com/pradoslaw/coyote',
-            'title' => 'Link do repozytorium na Github, dyskusja o 4programmers.net',
-        ],
     ],
     // _ na poczatku gdyz ten plugin korzysta z metody share() klasy View, a nazwa "forum" moze
     // wchodzic w konflikt z innymi zmiennymi przekazywanymi do twiga
