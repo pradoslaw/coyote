0ee3176550b9bfd65ac405ca11aea71ef2d5cd5b
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2023-11-18 15:43:31 +0100

    Remove commented test

--- tests/Feature/Listeners/DispatchPostCommentNotificationTest.php
+++ tests/Feature/Listeners/DispatchPostCommentNotificationTest.php
@@ -4,8 +4,8 @@ namespace Tests\Feature\Listeners;
 
 use Coyote\Events\CommentSaved;
 use Coyote\Forum;
-use Coyote\Notifications\Post\CommentedNotification;
 use Coyote\Notifications\Post\Comment\UserMentionedNotification;
+use Coyote\Notifications\Post\CommentedNotification;
 use Coyote\Post\Comment;
 use Coyote\Topic;
 use Coyote\User;
@@ -17,15 +17,14 @@ class DispatchPostCommentNotificationTest extends TestCase
 {
     use DatabaseTransactions;
 
-    private Forum $forum;
     private Topic $topic;
 
     public function setUp(): void
     {
         parent::setUp();
 
-        $this->forum = factory(Forum::class)->create();
-        $this->topic = factory(Topic::class)->create(['forum_id' => $this->forum->id]);
+        $forum = factory(Forum::class)->create();
+        $this->topic = factory(Topic::class)->create(['forum_id' => $forum->id]);
 
         Notification::fake();
     }
@@ -61,22 +60,4 @@ class DispatchPostCommentNotificationTest extends TestCase
 
         Notification::assertSentTo($subscriber, UserMentionedNotification::class);
     }
-
-//    public function testDispatchNotificationToFollowers()
-//    {
-//        /** @var User $user */
-//        $user = factory(User::class)->create();
-//        $follower = factory(User::class)->create();
-//
-//        $this->topic->refresh();
-//
-//        $follower->relations()->create(['related_user_id' => $user->id, 'is_blocked' => false]);
-//        $comment = factory(Comment::class)->create(['user_id' => $user->id, 'post_id' => $this->topic->first_post_id]);
-//
-//        Notification::fake();
-//
-//        event(new CommentSaved($comment));
-//
-//        Notification::assertSentTo($follower, CommentedNotification::class);
-//    }
 }
