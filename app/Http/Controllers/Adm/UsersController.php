29ee0a4a4b99ebc48c0ab81c6568be594b0acd4a
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2023-12-28 10:05:13 +0100

    Clean Adm/UsersController

--- app/Http/Controllers/Adm/UsersController.php
+++ app/Http/Controllers/Adm/UsersController.php
@@ -1,75 +1,53 @@
 <?php
-
 namespace Coyote\Http\Controllers\Adm;
 
+use Boduch\Grid\Source\EloquentSource;
 use Coyote\Events\UserDeleted;
+use Coyote\Events\UserSaved;
 use Coyote\Http\Forms\User\AdminForm;
 use Coyote\Http\Grids\Adm\UsersGrid;
-use Coyote\Repositories\Contracts\UserRepositoryInterface as UserRepository;
-use Boduch\Grid\Source\EloquentSource;
 use Coyote\Repositories\Criteria\WithTrashed;
+use Coyote\Repositories\Eloquent\UserRepository;
+use Coyote\Services\FormBuilder\Form;
 use Coyote\Services\Stream\Activities\Update;
 use Coyote\Services\Stream\Objects\Person;
-use Coyote\Events\UserSaved;
+use Coyote\User;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\View\View;
 
 class UsersController extends BaseController
 {
-    /**
-     * @var UserRepository
-     */
-    private $user;
-
-    /**
-     * @param UserRepository $user
-     */
-    public function __construct(UserRepository $user)
+    public function __construct(private UserRepository $user)
     {
         parent::__construct();
-
-        $this->user = $user;
         $this->breadcrumb->push('Użytkownicy', route('adm.users'));
     }
 
-    /**
-     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
-     */
-    public function index()
+    public function index(): View
     {
         $this->user->pushCriteria(new WithTrashed());
         $this->user->applyCriteria();
-
         $grid = $this->gridBuilder()->createGrid(UsersGrid::class);
         $grid->setSource(new EloquentSource($this->user->newQuery()));
-
         return $this->view('adm.users.home', ['grid' => $grid]);
     }
 
-    /**
-     * @param \Coyote\User $user
-     * @return \Illuminate\View\View
-     */
-    public function edit($user)
+    public function edit(User $user): View
     {
         $this->breadcrumb->push($user->name, route('adm.users.save', [$user->id]));
-
         return $this->view('adm.users.save', [
             'user' => $user,
-            'form' => $this->getForm($user)
+            'form' => $this->getForm($user),
         ]);
     }
 
-    /**
-     * @param \Coyote\User $user
-     * @return \Illuminate\Http\RedirectResponse
-     */
-    public function save($user)
+    public function save(User $user): RedirectResponse
     {
         $form = $this->getForm($user);
         $form->validate();
 
         $this->transaction(function () use ($user, $form) {
             $data = $form->all();
-
             if ($form->get('delete_photo')->isChecked()) {
                 $data['photo'] = null;
             }
@@ -78,32 +56,19 @@ class UsersController extends BaseController
             // we can do that because $form->all() returns only fields in form. $request->all() returns
             // all fields in HTTP POST so it's not secure.
             $user->forceFill(array_except($data, ['submit', 'skills', 'groups', 'delete_photo']))->save();
-//            $user->skills()->delete();
-//
-//            if (!empty($data['skills'])) {
-//                foreach ($data['skills'] as $idx => $skill) {
-//                    $user->skills()->create($skill + ['order' => $idx + 1]);
-//                }
-//            }
 
-            $user->groups()->sync((array) $data['groups']);
-
+            $user->groups()->sync((array)$data['groups']);
             stream(Update::class, new Person($user));
-
             event($user->deleted_at ? new UserDeleted($user) : new UserSaved($user));
         });
 
         return back()->with('success', 'Zmiany zostały poprawie zapisane');
     }
 
-    /**
-     * @param \Coyote\User $user
-     * @return \Coyote\Services\FormBuilder\Form
-     */
-    protected function getForm($user)
+    protected function getForm(User $user): Form
     {
         return $this->createForm(AdminForm::class, $user, [
-            'url' => route('adm.users.save', [$user->id])
+            'url' => route('adm.users.save', [$user->id]),
         ]);
     }
 }
