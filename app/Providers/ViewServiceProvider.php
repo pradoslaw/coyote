c9d777608541bda43f0b0190712d2822aa4ff7a5
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2024-01-02 12:23:24 +0100

    Move GDPR from BaseController to ViewServiceProvider

--- app/Providers/ViewServiceProvider.php
+++ app/Providers/ViewServiceProvider.php
@@ -1,13 +1,18 @@
 <?php
 namespace Coyote\Providers;
 
+use Coyote\Domain\User\UserSettings;
 use Coyote\Http\Composers\InitialStateComposer;
 use Coyote\Http\Factories\CacheFactory;
 use Coyote\Services\Forum\UserDefined;
 use Coyote\Services\Guest;
-use Illuminate\Contracts\View\View;
+use Coyote\User;
+use Coyote\View\Twig\TwigLiteral;
 use Illuminate\Contracts\Cache;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\Request;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\View\Factory;
 use Lavary\Menu\Builder;
 use Lavary\Menu\Menu;
 
@@ -18,16 +23,35 @@ class ViewServiceProvider extends ServiceProvider
     public function boot(): void
     {
         $cache = app(Cache\Repository::class);
-        $this->app['view']->composer(['layout', 'adm.home'], InitialStateComposer::class);
-        $this->app['view']->composer('layout', function (View $view) use ($cache) {
+        /** @var Factory $view */
+        $view = $this->app['view'];
+        $view->composer(['layout', 'adm.home'], InitialStateComposer::class);
+        $view->composer('layout', function (View $view) use ($cache) {
             $view->with([
                 '__master_menu' => $this->buildMasterMenu(),
 
                 // temporary code
                 '__dark_theme'  => $this->app[Guest::class]->getSetting('dark.theme', true),
-                'github_stars'  => $cache->remember('homepage:github_stars', 30 * 60, fn() => $this->githubStars())
+                'github_stars'  => $cache->remember('homepage:github_stars', 30 * 60, fn() => $this->githubStars()),
+                
+                'gdpr'          => [
+                    'content'  => TwigLiteral::fromHtml((new UserSettings)->cookieAgreement()),
+                    'accepted' => $this->gdprAccepted(),
+                ],
             ]);
         });
+    }
+
+    private function gdprAccepted(): bool
+    {
+        /** @var Request $request */
+        $request = $this->app['request'];
+        $user = $request->user();
+        if ($user) {
+            /** @var User $user */
+            return (bool)$user->gdpr;
+        }
+        return false;
     }
 
     private function buildMasterMenu(): Builder
@@ -79,7 +103,7 @@ class ViewServiceProvider extends ServiceProvider
             'https://api.github.com/repos/pradoslaw/coyote',
             false,
             \stream_context_create([
-                'http' => ['header' => 'User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36']
+                'http' => ['header' => 'User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'],
             ]));
         if ($result !== false) {
             $data = @\json_decode($result, true);
