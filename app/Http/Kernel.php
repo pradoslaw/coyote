ce0d417b8365790a636f875a7a98ad50d16a76a8
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2024-01-03 11:56:09 +0100

    Clean Kernel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -1,96 +1,93 @@
 <?php
-
 namespace Coyote\Http;
 
 use Coyote\Http\Middleware\ForceRootUrl;
 use Coyote\Http\Middleware\ThrottleSubmission;
-use Illuminate\Foundation\Http\Kernel as HttpKernel;
+use Fruitcake\Cors\HandleCors;
+use Illuminate\Auth\Middleware\AuthenticateWithBasicAuth;
+use Illuminate\Auth\Middleware\Authorize;
+use Illuminate\Auth\Middleware\EnsureEmailIsVerified;
+use Illuminate\Auth\Middleware\RequirePassword;
+use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
+use Illuminate\Foundation;
+use Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode;
+use Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull;
+use Illuminate\Foundation\Http\Middleware\ValidatePostSize;
+use Illuminate\Http\Middleware\SetCacheHeaders;
+use Illuminate\Routing\Middleware\SubstituteBindings;
+use Illuminate\Routing\Middleware\ThrottleRequests;
+use Illuminate\Routing\Middleware\ValidateSignature;
+use Illuminate\Session\Middleware\AuthenticateSession;
+use Illuminate\Session\Middleware\StartSession;
+use Illuminate\View\Middleware\ShareErrorsFromSession;
 
-class Kernel extends HttpKernel
+class Kernel extends Foundation\Http\Kernel
 {
-    /**
-     * The application's global HTTP middleware stack.
-     *
-     * @var array
-     */
+    /** @var array */
     protected $middleware = [
         Middleware\TrustProxies::class,
-        \Fruitcake\Cors\HandleCors::class,
-        \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
-        \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
+        HandleCors::class,
+        CheckForMaintenanceMode::class,
+        ValidatePostSize::class,
         Middleware\TrimStrings::class,
-        \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class
+        ConvertEmptyStringsToNull::class,
     ];
 
-    /**
-     * The application's route middleware groups.
-     *
-     * @var array
-     */
+    /** @var array */
     protected $middlewareGroups = [
         'web' => [
             Middleware\EncryptCookies::class,
-            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-            \Illuminate\Session\Middleware\StartSession::class,
+            AddQueuedCookiesToResponse::class,
+            StartSession::class,
             Middleware\SetupGuestCookie::class,
-            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+            ShareErrorsFromSession::class,
             Middleware\VerifyCsrfToken::class,
-            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            SubstituteBindings::class,
             Middleware\DefaultBindings::class,
-            Middleware\FirewallBlacklist::class
+            Middleware\FirewallBlacklist::class,
         ],
         'api' => [
             'throttle:60,1',
-            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            SubstituteBindings::class,
             Middleware\DefaultBindings::class,
-            ForceRootUrl::class
+            ForceRootUrl::class,
         ],
     ];
 
-    /**
-     * The application's route middleware.
-     *
-     * @var array
-     */
+    /** @var array */
     protected $routeMiddleware = [
-        'auth'          => Middleware\Authenticate::class,
-        'auth.basic'    => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
-        'bindings'      => \Illuminate\Routing\Middleware\SubstituteBindings::class,
-        'throttle'      => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        'throttle.submission'      => ThrottleSubmission::class,
-        'can'           => \Illuminate\Auth\Middleware\Authorize::class,
-        'guest'         => Middleware\RedirectIfAuthenticated::class,
-        'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
-        'password.confirm' => \Illuminate\Auth\Middleware\RequirePassword::class,
-        'signed'        => \Illuminate\Routing\Middleware\ValidateSignature::class,
-        'verified'      => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'adm'           => Middleware\AdmAccess::class,
-        'forum.write'   => Middleware\ForumWrite::class,
-        'forum.url'     => Middleware\RedirectIfUrl::class,
-        'topic.access'  => Middleware\RedirectIfMoved::class,
-        'topic.scroll'  => Middleware\RedirectToPost::class,
-        'wiki.access'   => Middleware\WikiAccess::class,
-        'wiki.lock'     => Middleware\WikiLock::class,
-        'wiki.legacy'   => Middleware\WikiLegacy::class,
-        'page.hit'      => Middleware\PageHit::class,
-        'geocode'       => Middleware\GeocodeIp::class,
-        'json'          => Middleware\JsonResponse::class
+        'auth'                => Middleware\Authenticate::class,
+        'auth.basic'          => AuthenticateWithBasicAuth::class,
+        'bindings'            => SubstituteBindings::class,
+        'throttle'            => ThrottleRequests::class,
+        'throttle.submission' => ThrottleSubmission::class,
+        'can'                 => Authorize::class,
+        'guest'               => Middleware\RedirectIfAuthenticated::class,
+        'cache.headers'       => SetCacheHeaders::class,
+        'password.confirm'    => RequirePassword::class,
+        'signed'              => ValidateSignature::class,
+        'verified'            => EnsureEmailIsVerified::class,
+        'adm'                 => Middleware\AdmAccess::class,
+        'forum.write'         => Middleware\ForumWrite::class,
+        'forum.url'           => Middleware\RedirectIfUrl::class,
+        'topic.access'        => Middleware\RedirectIfMoved::class,
+        'topic.scroll'        => Middleware\RedirectToPost::class,
+        'wiki.access'         => Middleware\WikiAccess::class,
+        'wiki.lock'           => Middleware\WikiLock::class,
+        'wiki.legacy'         => Middleware\WikiLegacy::class,
+        'page.hit'            => Middleware\PageHit::class,
+        'geocode'             => Middleware\GeocodeIp::class,
+        'json'                => Middleware\JsonResponse::class,
     ];
 
-    /**
-     * The priority-sorted list of middleware.
-     *
-     * This forces non-global middleware to always be in the given order.
-     *
-     * @var array
-     */
+    /** @var array */
     protected $middlewarePriority = [
-        \Illuminate\Session\Middleware\StartSession::class,
-        \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+        StartSession::class,
+        ShareErrorsFromSession::class,
         Middleware\Authenticate::class,
-        \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        \Illuminate\Session\Middleware\AuthenticateSession::class,
-        \Illuminate\Routing\Middleware\SubstituteBindings::class,
-        \Illuminate\Auth\Middleware\Authorize::class,
+        ThrottleRequests::class,
+        AuthenticateSession::class,
+        SubstituteBindings::class,
+        Authorize::class,
     ];
 }
