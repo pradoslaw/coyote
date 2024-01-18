c625971c3e2093e850bb357611ef5edf6cd0bfe9
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2024-01-18 15:21:06 +0100

    Move Laravel\TestCase fixture from Laravel\Application trait to Laravel\PhpUnit\Listener

--- tests/Unit/BaseFixture/Server/Laravel/Application.php
+++ tests/Unit/BaseFixture/Server/Laravel/Application.php
@@ -1,42 +1,9 @@
 <?php
 namespace Tests\Unit\BaseFixture\Server\Laravel;
 
-use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Tests\Unit\BaseFixture\Server\Laravel;
 
 trait Application
 {
     var ?Laravel\TestCase $laravel = null;
-
-    /**
-     * @before
-     */
-    function initializeApplication(): void
-    {
-        $this->laravel = $this->instance();
-        $this->laravel->setUp();
-    }
-
-    /**
-     * @after
-     */
-    function finalizeApplication(): void
-    {
-        $this->laravel->tearDown();
-    }
-
-    function instance(): Laravel\TestCase
-    {
-        if ($this->hasTransactionalMarker()) {
-            return new class extends Laravel\TestCase {
-                use DatabaseTransactions;
-            };
-        }
-        return new Laravel\TestCase();
-    }
-
-    function hasTransactionalMarker(): bool
-    {
-        return \in_array(Transactional::class, \class_uses_recursive(static::class));
-    }
 }
--- tests/Unit/BaseFixture/Server/Laravel/PhpUnit/Listener.php
+++ tests/Unit/BaseFixture/Server/Laravel/PhpUnit/Listener.php
@@ -1,15 +1,45 @@
 <?php
 namespace Tests\Unit\BaseFixture\Server\Laravel\PhpUnit;
 
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use PHPUnit\Framework\Test;
+use Tests\Unit\BaseFixture\Server\Laravel;
+use Tests\Unit\BaseFixture\Server\Laravel\Transactional;
 
 class Listener extends Adapter
 {
+    private ?Laravel\TestCase $laravel = null;
+
     public function startTest(Test $test): void
     {
+        if (!\in_array(Laravel\Application::class, \class_uses_recursive($test))) {
+            return;
+        }
+        $this->laravel = $this->instance($test);
+        $this->laravel->setUp();
+
+        /** @var Laravel\Application $test */
+        $test->laravel = $this->laravel;
     }
 
     public function endTest(Test $test, float $time): void
     {
+        $this->laravel?->tearDown();
+        $this->laravel = null;
+    }
+
+    private function instance(Test $test): Laravel\TestCase
+    {
+        if ($this->hasTransactionalMarker($test)) {
+            return new class extends Laravel\TestCase {
+                use DatabaseTransactions;
+            };
+        }
+        return new Laravel\TestCase();
+    }
+
+    private function hasTransactionalMarker(Test $test): bool
+    {
+        return \in_array(Transactional::class, \class_uses_recursive($test));
     }
 }
