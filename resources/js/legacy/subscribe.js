d9e31d58a555707b9f62c0335ef091baeb64a30f
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2024-01-25 15:24:53 +0100

    Remove <a href=""> in wiki\show.twig and wiki\category.twig

--- resources/js/legacy/subscribe.js
+++ resources/js/legacy/subscribe.js
@@ -1,16 +1,18 @@
 $(() => {
-    'use strict';
-
-    $('.btn-subscribe').click((e) => {
-        let self = $(e.currentTarget);
-
-        $.post(self.attr('href'), () => {
-            self.toggleClass('on');
-            self.find('span').text(self.data(self.hasClass('on') ? 'off' : 'on'));
-        }).fail((e) => {
-            $('#modal-unauthorized').modal('show');
-        });
-
-        return false;
-    });
+  'use strict';
+  $('.btn-subscribe').click(event => {
+    const self = $(event.currentTarget);
+    if (!self.data('href')) {
+      return;
+    }
+    $
+      .post(self.data('href'), () => {
+        self.toggleClass('on');
+        self.find('span').text(self.data(self.hasClass('on') ? 'off' : 'on'));
+      })
+      .fail((e) => {
+        $('#modal-unauthorized').modal('show');
+      });
+    return false;
+  });
 });
