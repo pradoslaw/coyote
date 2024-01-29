edf4fc1c2ef51333b13134a11b8bc57ec4a034c0
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2024-01-29 10:33:41 +0100

    Update TopicsTableSeeder

--- database/seeders/TopicsTableSeeder.php
+++ database/seeders/TopicsTableSeeder.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace Database\Seeders;
 
 use Coyote\Post;
@@ -13,14 +12,33 @@ class TopicsTableSeeder extends Seeder
     public function run(): void
     {
         Log::reguard();
+        \factory(Topic::class, 30)
+            ->create()
+            ->each(fn(Topic $topic) => $this->seedPosts($topic));
+    }
 
-        \factory(Topic::class, 30)->create()->each(function (Topic $topic) {
-            for ($i = 1; $i <= rand(1, 10); $i++) {
-                $user = User::inRandomOrder()->first();
-                $post = \factory(Post::class)
-                    ->make(['forum_id' => $topic->forum_id, 'user_id' => $user->id]);
-                $topic->posts()->save($post);
-            }
-        });
+    private function seedPosts(Topic $topic): void
+    {
+        foreach ($this->randomRange(15) as $_) {
+            $this->seedPost($topic, $this->randomUser());
+        }
+    }
+
+    private function randomRange(int $length): array
+    {
+        return \range(0, \rand(0, $length - 1));
+    }
+
+    private function seedPost(Topic $topic, User $user): void
+    {
+        $post = \factory(Post::class)->make(['forum_id' => $topic->forum_id, 'user_id' => $user->id]);
+        $topic->posts()->save($post);
+    }
+
+    private function randomUser(): User
+    {
+        /** @var User $user */
+        $user = User::query()->inRandomOrder()->first();
+        return $user;
     }
 }
