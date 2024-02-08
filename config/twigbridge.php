a7b7095c6e92f12e52e17f91a3db8df344f1f127
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2024-02-08 17:04:50 +0100

    Remove unnecessary functions from config/twigbridge.php

--- config/twigbridge.php
+++ config/twigbridge.php
@@ -1,7 +1,6 @@
 <?php
 
 return [
-
     'twig'       => [
         /*
         |--------------------------------------------------------------------------
@@ -11,50 +10,28 @@ return [
         | File extension for Twig view files.
         |
         */
-        'extension'   => 'twig',
+        'extension' => 'twig',
 
-        /*
-        |--------------------------------------------------------------------------
-        | Accepts all Twig environment configuration options
-        |--------------------------------------------------------------------------
-        |
-        | http://twig.sensiolabs.org/doc/api.html#environment-options
-        |
-        */
         'environment' => [
-
-            // When set to true, the generated templates have a __toString() method
-            // that you can use to display the generated nodes.
-            // default: false
-            'debug'               => env('APP_DEBUG', false),
-
-            // The charset used by the templates.
-            // default: utf-8
-            'charset'             => 'utf-8',
-
             // An absolute path where to store the compiled templates, or false to disable caching. If null
             // then the cache file path is used.
             // default: cache file storage path
-            'cache'               => null,
+            'cache'            => null,
 
             // When developing with Twig, it's useful to recompile the template
             // whenever the source code changes. If you don't provide a value
             // for the auto_reload option, it will be determined automatically based on the debug value.
-            'auto_reload'         => true,
+            'auto_reload'      => true,
 
             // If set to false, Twig will silently ignore invalid variables
             // (variables and or attributes/methods that do not exist) and
             // replace them with a null value. When set to true, Twig throws an exception instead.
             // default: false
-            'strict_variables'    => false,
+            'strict_variables' => false,
 
             // If set to true, auto-escaping will be enabled by default for all templates.
             // default: 'html'
-            'autoescape'          => 'html',
-
-            // A flag that indicates which optimizations to apply
-            // (default to -1 -- all optimizations are enabled; set it to 0 to disable)
-            'optimizations'       => -1,
+            'autoescape'       => 'html',
         ],
 
         'safe_classes' => [
@@ -84,34 +61,7 @@ return [
             'Coyote\Services\TwigBridge\Extensions\FormBuilder',
             'Coyote\Services\TwigBridge\Extensions\Media',
         ],
-        /*
-        |--------------------------------------------------------------------------
-        | Facades
-        |--------------------------------------------------------------------------
-        |
-        | Available facades. Access like `{{ Config.get('foo.bar') }}`.
-        |
-        | Each facade can take an optional array of options. To mark the whole facade
-        | as safe you can set the option `'is_safe' => true`. Setting the facade as
-        | safe means that any HTML returned will not be escaped.
-        |
-        | It is advisable to not set the whole facade as safe and instead mark the
-        | each appropriate method as safe for security reasons. You can do that with
-        | the following syntax:
-        |
-        | <code>
-        |     'Form' => [
-        |         'is_safe' => [
-        |             'open'
-        |         ]
-        |     ]
-        | </code>
-        |
-        | The values of the `is_safe` array must match the called method on the facade
-        | in order to be marked as safe.
-        |
-        */
-        'facades'   => [],
+
         /*
         |--------------------------------------------------------------------------
         | Functions
@@ -142,58 +92,22 @@ return [
         */
         'functions' => [
             'config',
-            'elixir',
             'head',
             'last',
             'excerpt',
             'request',
             'cdn',
-            'old',
             'route',
             'url',
             'asset',
             'keywords',
-            'public_path',
-            'http_build_query',
             'secure_asset',
             'grid'        => ['is_safe' => ['html']],
             'grid_column' => ['is_safe' => ['html']],
             'grid_row'    => ['is_safe' => ['html']],
             'grid_cell'   => ['is_safe' => ['html']],
             'grid_filter' => ['is_safe' => ['html']],
-            'grid_empty'  => ['is_safe' => ['html']]
-        ],
-        /*
-        |--------------------------------------------------------------------------
-        | Filters
-        |--------------------------------------------------------------------------
-        |
-        | Available filters. Access like `{{ variable|filter }}`.
-        |
-        | Each filter can take an optional array of options. These options are
-        | passed directly to `Twig_SimpleFilter`.
-        |
-        | So for example, to mark a filter as safe you can do the following:
-        |
-        | <code>
-        |     'studly_case' => [
-        |         'is_safe' => ['html']
-        |     ]
-        | </code>
-        |
-        | The options array also takes a `callback` that allows you to name the
-        | filter differently in your Twig templates than what is actually called.
-        |
-        | <code>
-        |     'snake' => [
-        |         'callback' => 'snake_case'
-        |     ]
-        | </code>
-        |
-        */
-        'filters'   => [
-            'str_limit' => ['is_safe' => ['html']],
-            'http_build_query'
+            'grid_empty'  => ['is_safe' => ['html']],
         ],
     ],
 ];
