1aad4b0a96f59c7785f4eb7e91e011f6800fb375
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2024-02-12 18:46:28 +0100

    Clean Auth\OAuthController

--- app/Http/Controllers/Auth/OAuthController.php
+++ app/Http/Controllers/Auth/OAuthController.php
@@ -1,65 +1,42 @@
 <?php
-
 namespace Coyote\Http\Controllers\Auth;
 
 use Coyote\Events\UserSaved;
 use Coyote\Http\Controllers\Controller;
 use Coyote\Http\Factories\MediaFactory;
-use Coyote\Repositories\Contracts\UserRepositoryInterface as UserRepository;
-use Coyote\Services\Stream\Activities\Login as Stream_Login;
+use Coyote\Repositories\Eloquent\UserRepository;
 use Coyote\Services\Stream\Activities\Create as Stream_Create;
+use Coyote\Services\Stream\Activities\Login as Stream_Login;
 use Coyote\Services\Stream\Objects\Person as Stream_Person;
 use Coyote\User;
+use Illuminate\Http\RedirectResponse;
 use Laravel\Socialite\Contracts\Factory as Socialite;
 
 class OAuthController extends Controller
 {
     use MediaFactory;
 
-    /**
-     * @var UserRepository
-     */
-    private $user;
-
-    /**
-     * OAuthController constructor.
-     * @param UserRepository $user
-     */
-    public function __construct(UserRepository $user)
+    public function __construct(private UserRepository $users)
     {
         parent::__construct();
-
-        $this->user = $user;
     }
 
-    /**
-     * Redirect the user to the GitHub authentication page.
-     *
-     * @param string $provider
-     * @return \Symfony\Component\HttpFoundation\RedirectResponse
-     */
-    public function login($provider)
+    public function login(string $provider): \Symfony\Component\HttpFoundation\RedirectResponse
     {
         return $this->getSocialiteFactory()->driver($provider)->redirect();
     }
 
-    /**
-     * Obtain the user information from GitHub.
-     *
-     * @param string $provider
-     * @return \Illuminate\Http\RedirectResponse
-     */
-    public function callback($provider)
+    public function callback(string $provider): RedirectResponse
     {
         if (!$this->request->filled('code') || $this->request->filled('error')) {
             return redirect()->route('login', ['error' => $this->request->input('error_description')]);
         }
 
         $oauth = $this->getSocialiteFactory()->driver($provider)->stateless()->user();
-        $user = $this->user->findWhere(['provider' => $provider, 'provider_id' => $oauth->getId()])->first();
+        $user = $this->users->findWhere(['provider' => $provider, 'provider_id' => $oauth->getId()])->first();
 
         if (!$user) {
-            $user = $this->user->findByEmail($oauth->getEmail());
+            $user = $this->users->findByEmail($oauth->getEmail());
 
             if ($user !== null) {
                 // merge with existing user account
@@ -70,11 +47,9 @@ class OAuthController extends Controller
                 $name = trim($oauth->getName() ?: $oauth->getNickName());
 
                 // it's important to check login name using case insensitive...
-                if ($this->user->findByName($name)) {
-                    // komunikatu bledu nie mozemy przekazac w sesji poniewaz z jakiegos powodu
-                    // jest ona gubiona
+                if ($this->users->findByName($name)) {
                     return redirect()->route('register', [
-                        'error' => sprintf('Uuups. Niestety login %s jest już zajęty.', $name)
+                        'error' => "Uuups. Niestety login $name jest już zajęty.",
                     ]);
                 }
 
@@ -87,32 +62,26 @@ class OAuthController extends Controller
                     $filename = $media->getFilename();
                 }
 
-                $user = User::forceCreate([
-                    'name' => $name,
-                    'email' => $oauth->getEmail(),
-                    'photo' => $filename,
-                    'is_confirm' => 1,
-                    'provider' => $provider,
+                $user = User::query()->forceCreate([
+                    'name'        => $name,
+                    'email'       => $oauth->getEmail(),
+                    'photo'       => $filename,
+                    'is_confirm'  => 1,
+                    'provider'    => $provider,
                     'provider_id' => $oauth->getId(),
-                    'guest_id' => $this->request->session()->get('guest_id')
+                    'guest_id'    => $this->request->session()->get('guest_id'),
                 ]);
             }
         }
-
         if ($user->is_blocked) {
             return redirect()->route('login', ['error' => 'Konto zostało zablokowane.']);
         }
-
         auth()->login($user, true);
-
         if ($user->wasRecentlyCreated) {
             stream(Stream_Create::class, new Stream_Person($user));
-
             event(new UserSaved($user));
         }
-
         stream(Stream_Login::class);
-
         return redirect()->intended(route('home'));
     }
 
