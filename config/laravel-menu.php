0693da6dfac1a21dd15054c1d578bb95ad226dbe
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2024-02-21 13:18:50 +0100

    Add missing class="nav-item" to tab item

--- config/laravel-menu.php
+++ config/laravel-menu.php
@@ -2,7 +2,7 @@
 
 return [
     'settings' => [
-        'default' => [
+        'default'          => [
             'auto_activate'    => true,
             'activate_parents' => true,
             'active_class'     => 'active',
@@ -12,42 +12,44 @@ return [
             'active_element'   => 'link',  // item|link
         ],
         '__master_menu___' => [
-            'restful'           => true
+            'restful' => true,
         ],
-        '_forum' => [
-            'auto_activate'    => false,
-            'restful'          => true
-        ]
+        '_forum'           => [
+            'auto_activate' => false,
+            'restful'       => true,
+        ],
     ],
+
     '__master_menu___' => [
-        'Forum' => ['route' => 'forum.home', 'class' => 'nav-item dropdown'],
+        'Forum'      => ['route' => 'forum.home', 'class' => 'nav-item dropdown'],
         'Mikroblogi' => ['route' => 'microblog.home', 'class' => 'nav-item'],
-        'Praca' => ['route' => 'job.home', 'class' => 'nav-item'],
+        'Praca'      => ['route' => 'job.home', 'class' => 'nav-item'],
         'Kompendium' => [
-            'url' => 'Kategorie',
-            'class' => 'nav-item dropdown',
+            'url'      => 'Kategorie',
+            'class'    => 'nav-item dropdown',
             'children' => [
-                'Delphi' => ['url' => 'Delphi'],
-                'C/C++' => ['url' => 'C'],
-                'C#' => ['url' => 'C_sharp'],
-                'Python' => ['url' => 'Python'],
-                'Java' => ['url' => 'Java'],
+                'Delphi'       => ['url' => 'Delphi'],
+                'C/C++'        => ['url' => 'C'],
+                'C#'           => ['url' => 'C_sharp'],
+                'Python'       => ['url' => 'Python'],
+                'Java'         => ['url' => 'Java'],
                 'Turbo Pascal' => ['url' => 'Turbo_Pascal'],
                 'Z pogranicza' => ['url' => 'Z_pogranicza'],
-                'Assembler' => ['url' => 'Assembler'],
-                'Algorytmy' => ['url' => 'Algorytmy'],
-                '(X)HTML' => ['url' => '(X)HTML'],
-                'CSS' => ['url' => 'CSS']
-            ]
+                'Assembler'    => ['url' => 'Assembler'],
+                'Algorytmy'    => ['url' => 'Algorytmy'],
+                '(X)HTML'      => ['url' => '(X)HTML'],
+                'CSS'          => ['url' => 'CSS'],
+            ],
         ],
     ],
+
     // _ na poczatku gdyz ten plugin korzysta z metody share() klasy View, a nazwa "forum" moze
     // wchodzic w konflikt z innymi zmiennymi przekazywanymi do twiga
-    '_forum' => [
-        'Kategorie' => ['route' => 'forum.categories', 'class' => 'nav-item'],
-        'Wszystkie' => ['route' => 'forum.all', 'class' => 'nav-item'],
-        'Obserwowane' => ['route' => 'forum.subscribes', 'data' => ['role' => true]],
-        'Moje' => ['route' => 'forum.mine', 'class' => 'nav-item', 'data' => ['role' => true], 'title' => 'Wątki w których brałem udział'],
-        'Moje tagi' => ['route' => 'forum.interesting', 'class' => 'nav-item', 'title' => 'Wątki zawierające moje tagi']
-    ]
+    '_forum'           => [
+        'Kategorie'   => ['route' => 'forum.categories', 'class' => 'nav-item'],
+        'Wszystkie'   => ['route' => 'forum.all', 'class' => 'nav-item'],
+        'Obserwowane' => ['route' => 'forum.subscribes', 'class' => 'nav-item', 'data' => ['role' => true]],
+        'Moje'        => ['route' => 'forum.mine', 'class' => 'nav-item', 'data' => ['role' => true], 'title' => 'Wątki w których brałem udział'],
+        'Moje tagi'   => ['route' => 'forum.interesting', 'class' => 'nav-item', 'title' => 'Wątki zawierające moje tagi'],
+    ],
 ];
