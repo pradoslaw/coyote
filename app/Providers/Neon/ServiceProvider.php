23d495bfe356dd46c4647066ea5a7a05019b62e3
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2024-03-19 17:35:45 +0100

    Pass page title to neon View instance

--- app/Providers/Neon/ServiceProvider.php
+++ app/Providers/Neon/ServiceProvider.php
@@ -6,11 +6,17 @@ use Neon\View;
 
 class ServiceProvider extends RouteServiceProvider
 {
+    public function register(): void
+    {
+        parent::register();
+        $this->app->instance(View::class, new View(''));
+    }
+
     public function loadRoutes(): void
     {
         $this->get('/events', [
             'uses' => function () {
-                $view = new View('');
+                $view = $this->app->get(View::class);
                 return $view->html();
             },
         ]);
--- neon/test/Unit/Http/Test.php
+++ neon/test/Unit/Http/Test.php
@@ -1,11 +1,13 @@
 <?php
 namespace Neon\Test\Unit\Http;
 
+use Neon\View;
 use PHPUnit\Framework\TestCase;
 use Tests\Unit\BaseFixture;
 
 class Test extends TestCase
 {
+    use BaseFixture\Server\Laravel\Application;
     use BaseFixture\Server\Http;
 
     /**
@@ -18,11 +20,12 @@ class Test extends TestCase
 
     private function assertRendersView(string $uri): void
     {
+        $this->laravel->app->instance(View::class, new View('Ours is the fury'));
         $response = $this->server->get($uri)
             ->assertSuccessful()
             ->getContent();
         $this->assertSame(
-            '<!DOCTYPE html><html><title></title></html>',
+            '<!DOCTYPE html><html><title>Ours is the fury</title></html>',
             $response);
     }
 }
