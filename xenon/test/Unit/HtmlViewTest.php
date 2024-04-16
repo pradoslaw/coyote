62e939b0d833e7e1c04ce4e84d395a226ea17ece
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2024-04-16 12:04:37 +0200

    Clean, extract common spa/ssr parts in tests

---
+++ xenon/test/Unit/HtmlViewTest.php
@@ -0,0 +1,57 @@
+<?php
+namespace Xenon\Test\Unit;
+
+use PHPUnit\Framework\TestCase;
+use Xenon\Tag;
+use Xenon\TagField;
+use Xenon\Xenon;
+
+class HtmlViewTest extends TestCase
+{
+    use Fixture;
+
+    private Xenon $xenon;
+
+    /**
+     * @before
+     */
+    public function htmlView(): void
+    {
+        $this->xenon = new Xenon([
+            new Tag('div', [
+                new TagField('p', 'favouriteColour'),
+                new TagField('p', 'leastFavouriteColour'),
+            ])],
+            ['favouriteColour' => 'red', 'leastFavouriteColour' => 'pink']);
+    }
+
+    /**
+     * @test
+     */
+    public function ssr(): void
+    {
+        $this->assertHtml($this->xenon, '<div><p>red</p><p>pink</p></div>');
+    }
+
+    /**
+     * @test
+     */
+    public function spa(): void
+    {
+        $this->assertHtmlRuntime($this->xenon, '<div><p>red</p><p>pink</p></div>');
+    }
+
+    /**
+     * @test
+     */
+    public function spaReactiveState(): void
+    {
+        $xenon = new Xenon(
+            [new TagField('i', 'favouriteColour')],
+            ['favouriteColour' => 'red']);
+        $this->executeAndAssertHtmlRuntime(
+            $xenon,
+            "xenon.setState('favouriteColour', 'green');",
+            '<i>green</i>');
+    }
+}
