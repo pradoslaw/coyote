c16e829b967b6347289e93c0f7425a7e7f842337
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2024-04-17 10:26:38 +0200

    Inline Page into HtmlView

--- neon/src/View/HtmlView.php
+++ neon/src/View/HtmlView.php
@@ -1,22 +1,41 @@
 <?php
 namespace Neon\View;
 
-use Neon\View\Html\Page;
+use Neon\View\Html\Head\Head;
+use Neon\View\Html\Head\Style;
+use Neon\View\Html\Item;
 use Neon\View\Html\Render;
 
-class HtmlView
+readonly class HtmlView
 {
-    private Page $page;
-
-    public function __construct(array $head, array $body)
+    public function __construct(private array $head, private array $body)
     {
-        $this->page = new Page($head, $body);
     }
 
     public function html(): string
     {
         $h = new Render();
-        $pageTag = $h->many($this->page->render($h));
-        return $pageTag->html();
+        $heads = [
+            ...$this->head,
+            // new Script('https://cdn.tailwindcss.com'), // for debug
+            new Style('css/neon.css?v6'),
+            new Style('fonts/switzer/switzer.css'),
+            new Style('fonts/inter/inter.css'),
+        ];
+        $page = $h->many([
+            $h->html('<!DOCTYPE html>'),
+            $h->tag('html', [], [
+                $h->tag('head', [], [
+                    $h->tag('meta', ['charset' => 'utf-8'], []),
+                    ...\array_map(fn(Head $head) => $head->render($h), $heads),
+                ]),
+                $h->tag('body',
+                    ['class' => 'bg-[#F0F2F5] font-[Switzer] px-2 lg:px-4'],
+                    \array_merge(...\array_map(
+                        fn(Item $item) => $item->render($h),
+                        $this->body))),
+            ]),
+        ]);
+        return $page->html();
     }
 }
