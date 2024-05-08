207f482857c9cca3b5d1508211f1f89849e22cce
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2024-05-08 19:07:52 +0200

    Update "intervention/image"

--- app/Services/Media/ImageWizard.php
+++ app/Services/Media/ImageWizard.php
@@ -2,7 +2,9 @@
 namespace Coyote\Services\Media;
 
 use Coyote\Services\Media\Filters\Thumbnail;
+use Intervention\Image\Drivers\Gd\Driver;
 use Intervention\Image\ImageManager;
+use Intervention\Image\Interfaces\ImageInterface;
 
 class ImageWizard
 {
@@ -10,24 +12,19 @@ class ImageWizard
 
     public function __construct()
     {
-        $this->manager = new ImageManager([
-            'driver'                    => 'gd',
-            'host'                      => env('CDN') ? '//' . env('CDN') : '',
-            'src_dirs'                  => [],
-            'url_parameter'             => '-image({options})',
-            'url_parameter_separator'   => '-',
-            'serve_image'               => true,
-            'serve_custom_filters_only' => false,
-            'write_image'               => true,
-            'memory_limit'              => '128M',
-        ]);
+        $this->manager = new ImageManager(new Driver());
     }
 
     public function resizedImage(Thumbnail $thumbnail, ?string $data): string
     {
-        return $this->manager
-            ->make($data)
-            ->filter(new ResizeFilter($thumbnail->width, $thumbnail->height))
-            ->encode();
+        return $this->resized($this->manager->read($data), $thumbnail)->encode();
+    }
+
+    private function resized(ImageInterface $image, Thumbnail $thumbnail): ImageInterface
+    {
+        if ($thumbnail->width === null) {
+            return $image->scaleDown(height:$thumbnail->height);
+        }
+        return $image->pad($thumbnail->width, $thumbnail->height);
     }
 }
