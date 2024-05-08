9509eb35b5c101e17b908967e39603a18263f8c2
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2024-05-08 12:51:49 +0200

    Clean Thumbnail

--- app/Services/Assets/Thumbnail.php
+++ app/Services/Assets/Thumbnail.php
@@ -8,35 +8,31 @@ use Intervention\Image\ImageManager;
 
 class Thumbnail
 {
-    private Filesystem $filesystem;
     private FilterInterface $filter;
-    private ImageManager $imageManager;
     private string $path;
 
-    public function __construct(ImageManager $imageManager, Filesystem $filesystem)
+    public function __construct(
+        private ImageManager $imageManager,
+        private Filesystem   $filesystem,
+    )
     {
-        $this->filesystem = $filesystem;
-        $this->imageManager = $imageManager;
     }
 
     public function setFilter(FilterInterface $filter): Thumbnail
     {
         $this->filter = $filter;
-
         return $this;
     }
 
-    public function open(string $path)
+    public function open(string $path): Thumbnail
     {
         $this->path = $path;
-
         return $this;
     }
 
     public function store(string $path): void
     {
         $image = $this->imageManager->make($this->filesystem->get($this->path));
-
         $this->filesystem->put($path, $image->filter($this->filter)->encode());
     }
 }
