0d48abc74d1d8570a9e671e664df2558f0201c94
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2024-05-10 14:07:20 +0200

    Refactor DateDifference not to use \strFTime()

--- app/Services/Helper/DateDifference.php
+++ app/Services/Helper/DateDifference.php
@@ -4,17 +4,10 @@ namespace Coyote\Services\Helper;
 
 use Carbon\Carbon;
 
-class DateDifference
+readonly class DateDifference
 {
-    /** @var string */
-    private $format;
-    /** @var bool */
-    private $diffForHumans;
-
-    public function __construct(string $format, bool $diffForHumans)
+    public function __construct(private string $format, private bool $diffForHumans)
     {
-        $this->format = $format;
-        $this->diffForHumans = $diffForHumans;
     }
 
     public function format(Carbon $earlier): string
@@ -22,7 +15,7 @@ class DateDifference
         if ($this->diffForHumans) {
             return $this->diffForHumans($earlier);
         }
-        return $earlier->formatLocalized($this->format);
+        return $this->formatDate($earlier);
     }
 
     private function diffForHumans(Carbon $earlier): string
@@ -36,6 +29,48 @@ class DateDifference
         if ($earlier->isYesterday()) {
             return 'wczoraj, ' . $earlier->format('H:i');
         }
-        return $earlier->formatLocalized($this->format);
+        return $this->formatDate($earlier);
+    }
+
+    private function formatDate(Carbon $earlier): string
+    {
+        return $earlier->format($this->replace($this->format, [
+            '%d' => 'd',
+            '%m' => 'm',
+            '%Y' => 'Y',
+            '%y' => 'y',
+            '%H' => 'H',
+            '%M' => 'i',
+            '%B' => $this->quoted($this->longMonth($earlier)),
+            '%b' => $this->quoted($this->shortMonth($earlier)),
+        ]));
+    }
+
+    private function replace(string $subject, array $search): string
+    {
+        return \str_replace(
+            \array_keys($search),
+            \array_values($search),
+            $subject);
+    }
+
+    private function longMonth(Carbon $date): string
+    {
+        return $this->month($date, ['styczeń', 'luty', 'marzec', 'kwiecień', 'maj', 'czerwiec', 'lipiec', 'sierpień', 'wrzesień', 'październik', 'listopad', 'grudzień']);
+    }
+
+    private function shortMonth(Carbon $date): string
+    {
+        return $this->month($date, ['sty', 'lut', 'mar', 'kwi', 'maj', 'cze', 'lip', 'sier', 'wrz', 'paź', 'lis', 'gru']);
+    }
+
+    private function month(Carbon $date, array $months): mixed
+    {
+        return $months[$date->month - 1];
+    }
+
+    private function quoted(string $string): string
+    {
+        return \implode(\array_map(fn($s) => "\\$s", \str_split($string)));
     }
 }
