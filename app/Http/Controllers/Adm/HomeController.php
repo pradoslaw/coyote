3ed98880914c78fbb485067a5daa347c09603dac
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2024-05-28 12:41:25 +0200

    Update login form in administrator panel

--- app/Http/Controllers/Adm/HomeController.php
+++ app/Http/Controllers/Adm/HomeController.php
@@ -3,32 +3,33 @@
 namespace Coyote\Http\Controllers\Adm;
 
 use Coyote\Http\Controllers\Controller;
+use Coyote\User;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class HomeController extends Controller
 {
-    /**
-     * @param Request $request
-     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View|\Illuminate\Http\RedirectResponse
-     */
-    public function index(Request $request)
+    public function index(Request $request): View|RedirectResponse
     {
         if ($request->session()->has('admin')) {
             return redirect()->route('adm.dashboard');
         }
-
+        /** @var User $user */
+        $user = auth()->user();
         if ($request->isMethod('post')) {
             $this->validate($request, ['password' => 'required']);
-
-            if (auth()->validate(['name' => auth()->user()->name, 'password' => $request->get('password')])) {
+            if (auth()->validate(['name' => $user->name, 'password' => $request->get('password')])) {
                 $request->session()->put('admin', true);
-
                 return redirect()->intended(route('adm.dashboard'));
-            } else {
-                return back()->withErrors('error');
             }
+            return back()->withErrors('error');
         }
-
-        return view('adm.home');
+        return view('adm.home', [
+            'user' => [
+                'name'   => $user->name,
+                'avatar' => $user->photo->getFilename() ? $user->photo->url() : cdn('/img/avatar.png'),
+            ],
+        ]);
     }
 }
