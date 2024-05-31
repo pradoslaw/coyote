7ebf8aed2cb072fcf8adfa6c9c56d1efce76f86e
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2024-05-31 16:07:01 +0200

    Report comments under post

--- resources/js/components/forum/comment.vue
+++ resources/js/components/forum/comment.vue
@@ -1,11 +1,15 @@
 <template>
   <div :id="anchor" :class="{'highlight-flash': highlight, 'not-read': comment.is_read === false}" class="post-comment">
+    <vue-flag v-for="flag in flags" :key="flag.id" :flag.sync="flag"></vue-flag>
+
     <template v-if="!comment.is_editing">
       <span v-html="comment.html"></span> &mdash;
 
       <vue-username :user="comment.user" :owner="comment.user.id === topic.owner_id"></vue-username>
 
-      <a :href="comment.url"><vue-timeago :datetime="comment.created_at" class="text-muted small"></vue-timeago></a>
+      <a :href="comment.url">
+        <vue-timeago :datetime="comment.created_at" class="text-muted small"></vue-timeago>
+      </a>
 
       <a v-if="comment.editable" @click="edit" href="javascript:" title="Edytuj ten komentarz" class="btn-comment">
         <i class="fas fa-pencil-alt"></i>
@@ -17,6 +21,10 @@
 
       <a v-if="comment.editable" @click="migrate" href="javascript:" title="Zamień w post" class="btn-comment">
         <i class="fas fa-compress"></i>
+      </a>
+
+      <a :data-metadata="comment.metadata" :data-url="comment.url" href="javascript:" class="btn-comment">
+        <i class="fas fa-flag"></i>
       </a>
     </template>
 
@@ -31,63 +39,76 @@
 </template>
 
 <script lang="ts">
-  import Vue from 'vue';
-  import VueUserName from '../user-name.vue';
-  import VueCommentForm from "./comment-form.vue";
-  import { default as mixins } from '../mixins/user';
-  import { Prop, Ref } from "vue-property-decorator";
-  import Component from "vue-class-component";
-  import { mixin as clickaway } from "vue-clickaway";
-  import { PostComment } from "@/types/models";
-  import { mapGetters } from "vuex";
+import {PostComment} from "@/types/models";
+import Vue from 'vue';
+import Component from "vue-class-component";
+import {mixin as clickaway} from "vue-clickaway";
+import {Prop, Ref} from "vue-property-decorator";
+import {mapGetters} from "vuex";
 
-  @Component({
-    name: 'comment',
-    mixins: [clickaway, mixins],
-    components: { 'vue-username': VueUserName, 'vue-comment-form': VueCommentForm },
-    computed: mapGetters('topics', ['topic'])
-  })
-  export default class VueComment extends Vue {
-    @Ref('comment-form')
-    readonly commentForm!: VueCommentForm;
+import store from "../../store/index.ts";
+import VueFlag from "../flags/flag.vue";
+import {default as mixins} from '../mixins/user.js';
+import VueUserName from '../user-name.vue';
+import VueCommentForm from "./comment-form.vue";
 
-    @Prop(Object)
-    comment!: PostComment;
+@Component({
+  name: 'comment',
+  mixins: [clickaway, mixins],
+  components: {
+    'vue-username': VueUserName,
+    'vue-comment-form': VueCommentForm,
+    'vue-flag': VueFlag,
+  },
+  computed: mapGetters('topics', ['topic']),
+})
+export default class VueComment extends Vue {
+  @Ref('comment-form')
+  readonly commentForm!: VueCommentForm;
 
-    edit() {
-      this.$store.commit('posts/edit', this.comment);
+  @Prop(Object)
+  comment!: PostComment;
 
-      if (this.comment.is_editing) {
-        this.$nextTick(() => this.commentForm.focus());
-      }
-    }
+  edit() {
+    this.$store.commit('posts/edit', this.comment);
 
-    deleteComment() {
-      this.$confirm({
-        message: 'Tej operacji nie będzie można cofnąć.',
-        title: 'Usunąć komentarz?',
-        okLabel: 'Tak, usuń'
-      })
-      .then(() => this.$store.dispatch('posts/deleteComment', this.comment));
-    }
-
-    migrate() {
-      this.$confirm({
-        message: 'Tej operacji nie będzie można cofnąć.',
-        title: 'Zamienić na post?',
-        okLabel: 'Tak, zamień'
-      })
-      .then(() => {
-        this.$store.dispatch('posts/migrateComment', this.comment).then(response => window.location.href = response.data.url);
-      });
-    }
-
-    get anchor() {
-      return `comment-${this.comment.id}`;
-    }
-
-    get highlight() {
-      return '#' + this.anchor === window.location.hash;
+    if (this.comment.is_editing) {
+      this.$nextTick(() => this.commentForm.focus());
     }
   }
+
+  deleteComment() {
+    this.$confirm({
+      message: 'Tej operacji nie będzie można cofnąć.',
+      title: 'Usunąć komentarz?',
+      okLabel: 'Tak, usuń',
+    })
+      .then(() => this.$store.dispatch('posts/deleteComment', this.comment));
+  }
+
+  migrate() {
+    this.$confirm({
+      message: 'Tej operacji nie będzie można cofnąć.',
+      title: 'Zamienić na post?',
+      okLabel: 'Tak, zamień',
+    })
+      .then(() => {
+        this.$store
+          .dispatch('posts/migrateComment', this.comment)
+          .then(response => window.location.href = response.data.url);
+      });
+  }
+
+  get anchor() {
+    return `comment-${this.comment.id}`;
+  }
+
+  get highlight() {
+    return '#' + this.anchor === window.location.hash;
+  }
+
+  get flags() {
+    return store.getters['flags/filter'](this.comment.id, 'Coyote\\Comment');
+  }
+}
 </script>
