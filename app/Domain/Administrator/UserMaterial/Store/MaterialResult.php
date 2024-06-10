885b464bef5943fd5e9eeacba31d492288c34dfe
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2024-06-10 11:44:07 +0200

    Add PhpDoc to MaterialResult

--- app/Domain/Administrator/UserMaterial/Store/MaterialResult.php
+++ app/Domain/Administrator/UserMaterial/Store/MaterialResult.php
@@ -1,8 +1,14 @@
 <?php
 namespace Coyote\Domain\Administrator\UserMaterial\Store;
 
+use Coyote\Domain\Administrator\UserMaterial\Material;
+
 class MaterialResult
 {
+    /**
+     * @param Material[] $materials
+     * @param int $total
+     */
     public function __construct(public array $materials, public int $total)
     {
     }
