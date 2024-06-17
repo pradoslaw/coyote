a05efd92ec3e86df8780b1f722bcdfdba6b6eab2
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2024-06-17 15:24:35 +0200

    Clean Material

--- app/Domain/Administrator/UserMaterial/Material.php
+++ app/Domain/Administrator/UserMaterial/Material.php
@@ -6,14 +6,14 @@ use Carbon\Carbon;
 readonly class Material
 {
     public function __construct(
-        public int  $id,
+        public int     $id,
         public string  $type,
         public Carbon  $createdAt,
         public ?Carbon $deletedAt,
         public string  $authorUsername,
         public ?string $authorImageUrl,
         public string  $contentMarkdown,
-        public bool $reported,
+        public bool    $reported,
     )
     {
     }
