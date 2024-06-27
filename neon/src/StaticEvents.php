8d8c15a172c4c211d58d6c03ce822accae5b774e
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2024-06-27 10:08:57 +0200

    Update StaticEvents

--- neon/src/StaticEvents.php
+++ neon/src/StaticEvents.php
@@ -11,36 +11,6 @@ class StaticEvents implements Events
     public function fetchEvents(): array
     {
         return [
-            new Domain\Event\Event(
-                'Kościuszkon',
-                'Kraków',
-                true,
-                ['hackaton', 'studenci'],
-                new Date(2024, 6, 8),
-                EventKind::Hackaton,
-                'https://kosciuszkon.pk.edu.pl/',
-                'https://4programmers.net/Mikroblogi/View/141749',
-            ),
-            new Event(
-                'Code Europe',
-                'Kraków',
-                false,
-                ['dane', 'qa', 'produkt'],
-                new Date(2024, 6, 10),
-                EventKind::Conference,
-                'https://www.codeeurope.pl/pl/',
-                'https://4programmers.net/Mikroblogi/View/141355',
-            ),
-            new Event(
-                'BeefUp',
-                'Katowice',
-                false,
-                ['networking', 'biznes'],
-                new Date(2024, 6, 13),
-                EventKind::Meetup,
-                'https://www.beefup.pro/',
-                'https://4programmers.net/Mikroblogi/View/141784',
-            ),
         ];
     }
 }
