7861e74f51bc8eeba92561e6395c8b6e6144fa1e
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2024-07-09 11:01:27 +0200

    Uninstall "jest"

--- resources/js/libs/assets.spec.js
+++
@@ -1,9 +0,0 @@
-import isImage from "./assets";
-
-test('declination test', () => {
-  expect(isImage('foo.png')).toBe(true);
-  expect(isImage('foo.gif')).toBe(true);
-  expect(isImage('foo.jpg')).toBe(true);
-  expect(isImage('foo.bar.jpg')).toBe(true);
-  expect(isImage('http://foo.com/path/to/some.jpg')).toBe(true);
-});
--- resources/js/libs/declination.spec.js
+++
@@ -1,6 +0,0 @@
-import declination from "./declination";
-
-test('declination test', () => {
-  expect(declination(1, ['wątek', 'wątków', 'wątków'])).toBe('wątek');
-  expect(declination(2, ['głos', 'głosy', 'głosów'])).toBe('głosy');
-});
