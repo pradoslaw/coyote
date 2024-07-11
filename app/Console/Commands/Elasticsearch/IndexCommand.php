ecab38bd9d56400e091d22316ffe99c08743c4ed
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2024-07-11 13:21:33 +0200

    Revert "Rewrite IndexCommand for elastic search"
    This reverts commit 2de54cc7b902d3ef32e60fa62d32f24d6c2ef4e8.

--- app/Console/Commands/Elasticsearch/IndexCommand.php
+++ app/Console/Commands/Elasticsearch/IndexCommand.php
@@ -1,175 +1,122 @@
 <?php
+
 namespace Coyote\Console\Commands\Elasticsearch;
 
+use Coyote\Repositories\Contracts\JobRepositoryInterface;
+use Coyote\Repositories\Contracts\PostRepositoryInterface;
 use Coyote\Services\Elasticsearch\Crawler;
 use Illuminate\Console\Command;
 use Illuminate\Container\Container as App;
-use Illuminate\Database\Eloquent;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Query\Expression;
-use Symfony\Component\Console\Helper\ProgressBar;
 
 class IndexCommand extends Command
 {
-    protected $signature = 'es:index {--model=} {--force} {--limit=}';
+    use EsTrait;
+
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'es:index {--model=} {--force} {--offset=}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
     protected $description = 'Index data in Elasticsearch.';
 
-    private App $app;
-    private array $elasticSearchModels;
+    /**
+     * @var JobRepositoryInterface
+     */
+    protected $job;
 
+    /**
+     * @var PostRepositoryInterface
+     */
+    protected $post;
+
+    /**
+     * @var App
+     */
+    protected $app;
+
+    /**
+     * @param App $app
+     */
     public function __construct(App $app)
     {
         parent::__construct();
+
         $this->app = $app;
-        $this->elasticSearchModels = [
-            'firm'      => \Coyote\Firm::class,
-            'job'       => \Coyote\Job::class,
-            'microblog' => \Coyote\Microblog::class,
-            'page'      => \Coyote\Page::class,
-            'post'      => \Coyote\Post::class,
-            'stream'    => \Coyote\Stream::class,
-            'tag'       => \Coyote\Tag::class,
-            'topic'     => \Coyote\Topic::class,
-            'user'      => \Coyote\User::class,
-            'wiki'      => \Coyote\Wiki::class,
-        ];
     }
 
-    public function handle(): int
+    /**
+     * Execute the console command.
+     */
+    public function handle()
     {
-        $limit = $this->option('limit');
-        $modelBuilders = $this->modelBuilders();
-        if ($this->presentAndConfirm($modelBuilders, $limit)) {
-            foreach ($modelBuilders as $model) {
-                $this->indexModelSince($model, $limit);
-            }
+        if ($this->option('force') || $this->confirm('Do you want to index data in Elasticsearch?', true)) {
+            $this->dispatch();
             $this->info('Done.');
         }
+
         return 0;
     }
 
-    private function presentAndConfirm(array $modelBuilders, ?int $limit): bool
+    /**
+     * @param string $model
+     */
+    private function one($model)
     {
-        $this->printCountTable($modelBuilders, $limit);
-        if ($this->option('force')) {
-            return true;
-        }
-        return $this->confirm('Do you want to index the given data in ElasticSearch?', true);
+        $className = 'Coyote\\' . ucfirst(strtolower($model));
+
+        $this->index($className);
     }
 
-    private function printCountTable(array $modelBuilders, ?int $limit): void
+    private function all()
     {
-        $rows = [];
-        foreach ($modelBuilders as $slug => $builder) {
-            $rows[] = [$slug, $this->builderCount($builder, $limit)];
+        foreach ($this->getSuitableModels() as $className) {
+            $this->index($className);
         }
-        $this->table(['Model', 'Remaining rows'], $rows, 'box-double');
     }
 
-    private function indexModelSince(Eloquent\Builder $builder, ?int $limit): void
+    /**
+     * @param string $className
+     */
+    private function index($className)
     {
-        $this->printStatusStart($builder->getModel());
-        $this->startIndexing($builder, $limit);
-        $this->printStatusEnd($builder->getModel());
-    }
+        $model = $this->app->make($className);
+        $this->line("Indexing $className ...");
 
-    private function startIndexing(Eloquent\Builder $builder, ?int $limit): void
-    {
-        $bar = $this->output->createProgressBar($this->builderCount($builder, $limit));
-        $page = 0;
-        $pageSize = 20000;
-        $alreadyLoaded = 0;
-        while (true) {
-            $leftToLoad = $limit - $alreadyLoaded;
-            if ($leftToLoad <= 0) {
-                break;
-            }
-            $models = $builder->forPage($page, \min($pageSize, $leftToLoad))->get();
-            if ($models->count() === 0) {
-                break;
-            }
-            $alreadyLoaded += $models->count();
-            $this->nextChunk($models, $bar);
-            if ($models->count() < $limit) {
-                break;
-            }
-            ++$page;
+        $offset = (int) $this->option('offset');
+
+        $builder = $model->select()->orderBy('id', 'desc');
+        $objectName = get_class($model);
+
+        // ugly hack for job offers...
+        if ($objectName === 'Coyote\Job') {
+            $builder = $builder->where('deadline_at', '>=', new Expression('NOW()'))->where('is_publish', 1)->with('firm');
+        } elseif ($objectName === 'Coyote\Microblog') {
+            $builder = $builder->whereNull('parent_id');
         }
+
+        $bar = $this->output->createProgressBar($builder->count() - $offset);
+
+        $builder->skip($offset)->chunk(20000, function ($rowset) use ($bar) {
+            $crawler = new Crawler();
+
+            foreach ($rowset as $row) {
+                $crawler->index($row);
+
+                $bar->advance();
+            }
+
+            unset($crawler);
+        });
+
         $bar->finish();
-    }
-
-    function nextChunk(Eloquent\Collection $models, ProgressBar $bar): void
-    {
-        $crawler = new Crawler();
-        foreach ($models as $model) {
-            $crawler->index($model);
-            $bar->advance();
-        }
-    }
-
-    private function modelBuilder(Model $model): Eloquent\Builder
-    {
-        $builder = $model->newQuery()
-            ->select()
-            ->orderBy('id', 'desc');
-
-        if (get_class($model) === \Coyote\Job::class) {
-            return $builder
-                ->where('deadline_at', '>=', new Expression('NOW()'))
-                ->where('is_publish', 1)
-                ->with('firm');
-        }
-        if (get_class($model) === \Coyote\Microblog::class) {
-            return $builder->whereNull('parent_id');
-        }
-        return $builder;
-    }
-
-    private function printStatusStart(Model $model): void
-    {
-        $this->line("Indexing " . \get_class($model) . " ...");
-    }
-
-    private function printStatusEnd(Model $model): void
-    {
-        $this->info("\n" . \get_class($model) . '... Done.');
-    }
-
-    private function modelBuilders(): array
-    {
-        $builders = [];
-        foreach ($this->modelClassNamesToIndex() as $slug => $builder) {
-            $builders[$slug] = $this->modelBuilder($this->app->make($builder));
-        }
-        return $builders;
-    }
-
-    private function modelClassNamesToIndex(): array
-    {
-        $modelSlug = $this->option('model');
-        if ($modelSlug === null) {
-            return $this->elasticSearchModels;
-        }
-        return [$modelSlug => $this->modelClassNameBySlug($modelSlug)];
-    }
-
-    private function modelClassNameBySlug(string $model): string
-    {
-        if (\array_key_exists($model, $this->elasticSearchModels)) {
-            return $this->elasticSearchModels[$model];
-        }
-        throw new \Exception(\implode("\n", [
-            "Failed to find model: $model",
-            'Existing models: ' . implode(', ', \array_keys($this->elasticSearchModels)) . '.',
-        ]));
-    }
-
-    private function builderCount(Eloquent\Builder $builder, ?int $limit): int
-    {
-        $totalCount = $builder->count();
-        if ($limit === null) {
-            return $totalCount;
-        }
-        return \min($totalCount, $limit);
+        $this->info("\n" . $className . '... Done.');
     }
 }
