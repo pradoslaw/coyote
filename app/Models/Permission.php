41587bc1337cd71c55bca416e3a495e30ff48078
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2024-07-23 14:19:03 +0200

    Revert "Add notification icon with reports, visible only for 'adm-access'"
    This reverts commit 992f1612

--- app/Models/Permission.php
+++ app/Models/Permission.php
@@ -1,14 +1,17 @@
 <?php
+
 namespace Coyote;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Cache;
 
-/**
- * @property int $id
- */
 class Permission extends Model
 {
+    /**
+     * Nie bedziemy dodawac nowych uprawnien z poziomu Laravel
+     *
+     * @var array
+     */
     protected $fillable = ['name', 'description', 'default'];
     public $timestamps = false;
 
