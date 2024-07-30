ae6fb5a2b29c53adb5ab5df8586890561524a585
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2024-07-30 13:30:05 +0200

    Add docker-swarm.local.yaml and docker-swarm.staging.yaml

--- docker-snapshot.php
+++ docker-snapshot.php
@@ -7,3 +7,11 @@ if (!\file_exists('docker-snapshot')) {
     'docker-snapshot/production.yaml',
     `docker stack config -c docker-swarm.yaml -c docker-swarm.production.yaml`,
 );
+\file_put_contents(
+    'docker-snapshot/staging.yaml',
+    `docker stack config -c docker-swarm.yaml -c docker-swarm.staging.yaml`,
+);
+\file_put_contents(
+    'docker-snapshot/local.yaml',
+    `docker stack config -c docker-swarm.yaml -c docker-swarm.local.yaml`,
+);
