9c2d1bb6470fb0a3a720ebae68447c5280c45d2e
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2024-08-09 15:59:56 +0200

    Add assert.test.ts

---
+++ survey/test/assert.test.ts
@@ -0,0 +1,37 @@
+import {describe, test} from "@jest/globals";
+import {strict as assert} from "node:assert";
+
+import {assertEquals, assertFalse, assertMatch, assertTrue} from "./assert";
+
+describe('assert', () => {
+  describe('assertTrue', () => {
+    test('pass', () => passes(() => assertTrue(true)));
+    test('fail', () => fails(() => assertTrue(false)));
+  });
+
+  describe('assertFalse', () => {
+    test('pass', () => passes(() => assertFalse(false)));
+    test('fail', () => fails(() => assertFalse(true)));
+  });
+
+  describe('assertEquals', () => {
+    test('pass', () => passes(() => assertEquals('foo', 'foo')));
+    test('fail', () => fails(() => assertEquals('foo', 'bar')));
+
+    test('pass for array', () => passes(() => assertEquals(['foo'], ['foo'])));
+    test('fail for array', () => fails(() => assertEquals(['foo'], ['bar'])));
+  });
+
+  describe('assertMatch', () => {
+    test('pass', () => passes(() => assertMatch('foo', /foo/)));
+    test('fail', () => fails(() => assertMatch('foo', /bar/)));
+  });
+
+  function passes(operation: () => void): void {
+    operation();
+  }
+
+  function fails(operation: () => void): void {
+    assert.throws(operation, assert.AssertionError);
+  }
+});
