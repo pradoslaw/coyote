9eb4ef11757ba368f7ccbe593b615fe1332773f6
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2024-08-20 16:09:10 +0200

    Only run survey on page load, to mitigate runtime errors

--- survey/src/survey.ts
+++ survey/src/survey.ts
@@ -7,78 +7,80 @@ import {ExperimentOpt} from "./screen/steps/participate";
 import SurveyTally, {type State} from "./tally";
 import {trial} from "./trial";
 
-const darkTheme: boolean = document.body.classList.contains('theme-dark');
-const survey: Survey = JSON.parse(document.getElementById('survey')!.textContent!);
+window.addEventListener('load', () => {
+  const darkTheme: boolean = document.body.classList.contains('theme-dark');
+  const survey: Survey = JSON.parse(document.getElementById('survey')!.textContent!);
 
-interface Survey {
-  surveyState: State;
-  surveyChoice: ExperimentOpt;
-}
+  interface Survey {
+    surveyState: State;
+    surveyChoice: ExperimentOpt;
+  }
 
-interface Data {
-  state: State,
-  experiment: Experiment;
-}
+  interface Data {
+    state: State,
+    experiment: Experiment;
+  }
 
-const app = new Vue({
-  name: 'Survey',
-  el: '#js-survey',
-  components: {'vue-survey-tally': SurveyTally},
-  template: `
-    <vue-survey-tally
-      :state="state"
-      :experiment="experiment"
-      @experimentOpt="experimentOpt"
-      @experimentPreview="experimentPreview"
-      @change="change"/>
-  `,
-  data(): Data {
-    const experiment: Experiment = {
-      ...trial,
-      ...darkTheme ? trial.dark : trial.light,
-      optedIn: survey.surveyChoice,
-    };
-    return {
-      state: survey.surveyState,
-      experiment,
-    };
-  },
-  methods: {
-    experimentOpt(optIn: ExperimentOpt): void {
-      this.experiment.optedIn = optIn;
-      experimentChangeStyle(optIn);
+  const app = new Vue({
+    name: 'Survey',
+    el: '#js-survey',
+    components: {'vue-survey-tally': SurveyTally},
+    template: `
+      <vue-survey-tally
+        :state="state"
+        :experiment="experiment"
+        @experimentOpt="experimentOpt"
+        @experimentPreview="experimentPreview"
+        @change="change"/>
+    `,
+    data(): Data {
+      const experiment: Experiment = {
+        ...trial,
+        ...darkTheme ? trial.dark : trial.light,
+        optedIn: survey.surveyChoice,
+      };
+      return {
+        state: survey.surveyState,
+        experiment,
+      };
     },
-    experimentPreview(opt: ExperimentOpt): void {
-      storeSurveyPreview(opt);
+    methods: {
+      experimentOpt(optIn: ExperimentOpt): void {
+        this.experiment.optedIn = optIn;
+        experimentChangeStyle(optIn);
+      },
+      experimentPreview(opt: ExperimentOpt): void {
+        storeSurveyPreview(opt);
+      },
+      change(state: State): void {
+        storeSurveyState(state);
+        this.state = state;
+      },
+      setTheme(dark: boolean): void {
+        const theme = dark ? trial.dark : trial.light;
+        this.experiment.imageLegacy = theme.imageLegacy;
+        this.experiment.imageModern = theme.imageModern;
+      },
     },
-    change(state: State): void {
-      storeSurveyState(state);
-      this.state = state;
-    },
-    setTheme(dark: boolean): void {
-      const theme = dark ? trial.dark : trial.light;
-      this.experiment.imageLegacy = theme.imageLegacy;
-      this.experiment.imageModern = theme.imageModern;
-    },
-  },
-});
+  });
 
-store.subscribe((mutation, state) => {
-  if (mutation.type === 'theme/CHANGE_THEME') {
-    const dark = state.theme.darkTheme;
-    app.setTheme(dark)
+  store.subscribe((mutation, state) => {
+    if (mutation.type === 'theme/CHANGE_THEME') {
+      const dark = state.theme.darkTheme;
+      app.setTheme(dark);
+    }
+  });
+
+  function experimentChangeStyle(style: ExperimentOpt): void {
+    store.commit('user/changePostStyle', style);
+    axios.post('/survey', {surveyChoice: style});
+  }
+
+  function storeSurveyState(surveyState: State): void {
+    axios.post('/survey', {surveyState});
+  }
+
+  function storeSurveyPreview(surveyChoicePreview: ExperimentOpt): void {
+    axios.post('/survey', {surveyChoicePreview});
   }
 });
-
-function experimentChangeStyle(style: ExperimentOpt): void {
-  store.commit('user/changePostStyle', style);
-  axios.post('/survey', {surveyChoice: style});
-}
-
-function storeSurveyState(surveyState: State): void {
-  axios.post('/survey', {surveyState});
-}
-
-function storeSurveyPreview(surveyChoicePreview: ExperimentOpt): void {
-  axios.post('/survey', {surveyChoicePreview});
-}
