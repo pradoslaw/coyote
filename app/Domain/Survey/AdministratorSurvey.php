1d81be147910b8cb21e6a9d50b8546045e20c97c
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2024-08-26 13:00:34 +0200

    Refactor AdministratorSurvey.membersStatistic() to execute a single query with groupBy

--- app/Domain/Survey/AdministratorSurvey.php
+++ app/Domain/Survey/AdministratorSurvey.php
@@ -35,22 +35,20 @@ class AdministratorSurvey
 
     public function membersStatistic(Survey $survey): array
     {
-        return [
-            'survey-accepted' => $this->membersCountOfState($survey, 'survey-accepted'),
-            'survey-declined' => $this->membersCountOfState($survey, 'survey-declined'),
-            'survey-invited'  => $this->membersCountOfState($survey, 'survey-invited'),
-        ];
-    }
-
-    public function membersCountOfState(Survey $survey, string $state): int
-    {
         return $survey->users()
             ->join('guests', function (JoinClause $join): void {
                 $join
                     ->on('guests.user_id', '=', 'users.id')
                     ->orOn('guests.id', '=', 'users.guest_id');
             })
-            ->whereRaw("guests.settings->>'surveyState' = ?", [$state])
-            ->count();
+            ->selectRaw("guests.settings->>'surveyState' as survey_state, COUNT(*) as count")
+            ->groupBy('survey_state')
+            ->pluck('count', 'survey_state')
+            ->toArray();
+    }
+
+    public function membersCountOfState(Survey $survey, string $string): int
+    {
+        return $this->membersStatistic($survey)[$string];
     }
 }
