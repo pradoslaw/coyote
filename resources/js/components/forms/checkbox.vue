78b5b9655bed4a5915693e59ab62b17c8642f645
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2024-09-17 10:38:12 +0200

    Clean resources/js/components/forms/checkbox.vue

--- resources/js/components/forms/checkbox.vue
+++ resources/js/components/forms/checkbox.vue
@@ -1,24 +1,24 @@
 <template>
-    <input :name="name" type="checkbox" v-model="valueLocal" :value="checkedValue">
+  <input :name="name" type="checkbox" v-model="valueLocal" :value="checkedValue">
 </template>
 
 <script>
-    import { default as mixins } from '../mixins/form';
+import {default as mixins} from '../mixins/form';
 
-    export default {
-        props: {
-            name: {
-                type: String,
-                require: true
-            },
-            value: {
-                type: [String, Number, Boolean]
-            },
-            checkedValue: {
-                require: false,
-                default: 1
-            }
-        },
-        mixins: [ mixins ]
-    };
+export default {
+  props: {
+    name: {
+      type: String,
+      require: true,
+    },
+    value: {
+      type: [String, Number, Boolean],
+    },
+    checkedValue: {
+      require: false,
+      default: 1,
+    },
+  },
+  mixins: [mixins],
+};
 </script>
