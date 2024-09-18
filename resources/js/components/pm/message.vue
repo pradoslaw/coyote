499fa13f23c9a21446d7d20157f9682a64cc10d2
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2024-09-18 09:50:38 +0200

    Update private messages so they don't exceed container

--- resources/js/components/pm/message.vue
+++ resources/js/components/pm/message.vue
@@ -1,6 +1,6 @@
 <template>
   <div :class="{'sequential': message.sequential, 'unread': ! isRead}" class="media">
-    <a v-if="!message.sequential" v-profile="message.user.id" class="i-45 me-2 d-none d-sm-block">
+    <a v-if="!message.sequential" v-profile="message.user.id" class="i-45 me-2 d-none d-sm-block flex-shrink-0">
       <vue-avatar :photo="message.user.photo" :name="message.user.name" class="mw-100 img-thumbnail media-object"/>
     </a>
 
