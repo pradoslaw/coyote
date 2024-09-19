07acf2462fb3e03afdca0f81ae8bfc4ae5dda659
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2024-09-19 11:37:54 +0200

    Clean components/forms/autocomplete.vue

--- resources/js/components/forms/autocomplete.vue
+++ resources/js/components/forms/autocomplete.vue
@@ -18,81 +18,63 @@
       @keyup.esc="toggleDropdown(false)"
       @keydown.enter.prevent="changeItem"
     >
-
-    <vue-dropdown ref="dropdown" :items="items" @select="selectItem"></vue-dropdown>
-    <vue-error :message="errors"></vue-error>
+    <vue-dropdown ref="dropdown" :items="items" @select="selectItem"/>
+    <vue-error :message="errors"/>
   </div>
 </template>
 
 <script>
-  import { default as mixins } from '../mixins/form';
-  import VueDropdown from './dropdown.vue';
-  import VueError from './error.vue';
-  import store from '../../store';
+import store from '../../store/index';
+import {default as mixins} from '../mixins/form.js';
+import VueDropdown from './dropdown.vue';
+import VueError from './error.vue';
 
-  export default {
-    mixins: [ mixins ],
-    store,
-    components: { 'vue-dropdown': VueDropdown, 'vue-error': VueError },
-    props: {
-      placeholder: {
-        type: String
+export default {
+  mixins: [mixins],
+  store,
+  components: {'vue-dropdown': VueDropdown, 'vue-error': VueError},
+  props: {
+    placeholder: {type: String},
+    id: {type: String},
+    value: {type: String, default: ''},
+    tabindex: {type: Number},
+    errors: {type: Array, default: () => []},
+    handler: {
+      type: Function,
+      default: (value) => {
+        return store.dispatch('prompt/request', {value, source: '/completion/prompt/users'});
       },
-      id: {
-        type: String
-      },
-      value: {
-        type: String,
-        default: ''
-      },
-      tabindex: {
-        type: Number
-      },
-      errors: {
-        type: Array,
-        default: () => []
-      },
-      handler: {
-        type: Function,
-        default: (value) => {
-          return store.dispatch('prompt/request', { value, source: '/completion/prompt/users' });
-        }
-      }
     },
-    data: () => ({
-      items: []
-    }),
-    mounted() {
-      this.$refs.autocomplete.addEventListener('search', this.changeItem);
+  },
+  data: () => ({
+    items: [],
+  }),
+  mounted() {
+    this.$refs.autocomplete.addEventListener('search', this.changeItem);
+  },
+  methods: {
+    emitFocus() {
+      this.$emit('focus');
     },
-    methods: {
-      emitFocus() {
-        this.$emit('focus');
-      },
-
-      selectItem(item) {
-        this.$emit('select', item);
-        this.toggleDropdown(false);
-      },
-
-      changeItem() {
-        this.$emit('select', this.$refs.dropdown.getSelected() ?? {name: this.value});
-
-        this.toggleDropdown(false);
-      },
-
-      toggleDropdown(flag) {
-        this.$refs.dropdown.toggleDropdown(flag);
-      }
+    selectItem(item) {
+      this.$emit('select', item);
+      this.toggleDropdown(false);
     },
-    watch: {
-      valueLocal: function (newValue, oldValue) {
-        if (newValue && oldValue && newValue.toLowerCase() === oldValue.toLowerCase()) {
-          return;
-        }
-
-        this.handler(newValue).then(items => this.items = items);
+    changeItem() {
+      this.$emit('select', this.$refs.dropdown.getSelected() ?? {name: this.value});
+      this.toggleDropdown(false);
+    },
+    toggleDropdown(flag) {
+      this.$refs.dropdown.toggleDropdown(flag);
+    },
+  },
+  watch: {
+    valueLocal: function (newValue, oldValue) {
+      if (newValue && oldValue && newValue.toLowerCase() === oldValue.toLowerCase()) {
+        return;
       }
-    }
-  }
+      this.handler(newValue).then(items => this.items = items);
+    },
+  },
+};
 </script>
