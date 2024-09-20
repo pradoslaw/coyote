2797d3410fc4e77438d3f39d4a5e7b06f5b4ae62
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2024-09-20 09:18:09 +0200

    Remove unused store from pages/forum/log.ts

--- resources/js/pages/forum/log.ts
+++ resources/js/pages/forum/log.ts
@@ -1,10 +1,8 @@
 import VuePostLog from "../../components/forum/post-log.vue";
-import store from '../../store/index';
 
 export default {
   delimiters: ['${', '}'],
   components: {'vue-log': VuePostLog},
-  store,
   data: () => ({
     logs: window.logs,
     topicLink: window.topicLink,
