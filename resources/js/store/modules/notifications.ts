b33af584fe9f492259d238b11f39ae74a00ea4ed
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2024-09-23 15:12:32 +0200

    Clean modules/notifications.ts

--- resources/js/store/modules/notifications.ts
+++ resources/js/store/modules/notifications.ts
@@ -3,16 +3,16 @@ import axios from "axios";
 const state = {
   notifications: null, // initial value must be null to show fa-spinner
   count: 0,
-  offset: 0
+  offset: 0,
 };
 
 const getters = {
   isEmpty: state => state.notifications === null,
-  unreadNotifications: state => state.notifications === null ? [] : state.notifications.filter(notification => !notification.is_read)
+  unreadNotifications: state => state.notifications === null ? [] : state.notifications.filter(notification => !notification.is_read),
 };
 
 const mutations = {
-  init (state, { notifications, count }) {
+  init(state, {notifications, count}) {
     state.notifications = Array.isArray(state.notifications) ? state.notifications.concat(notifications) : notifications;
     state.count = count;
     state.offset = notifications !== null ? state.notifications.length : 0;
@@ -24,47 +24,47 @@ const mutations = {
     state.notifications.splice(index, 1);
   },
 
-  reset (state) {
+  reset(state) {
     state.notifications = null; // reset notifications list. user needs to click again to get all notifications from server
     state.offset = 0;
   },
 
-  increment (state) {
+  increment(state) {
     state.count += 1;
   },
 
-  decrement (state) {
+  decrement(state) {
     state.count -= 1;
   },
 
-  count (state, count) {
+  count(state, count) {
     state.count = count;
   },
 
-  mark (state, notification) {
+  mark(state, notification) {
     notification.is_read = true;
-  }
+  },
 };
 
 const actions = {
-  load({ state, commit }) {
+  load({state, commit}) {
     return axios.get('/User/Notifications/Ajax', {params: {offset: state.offset}})
       .then(result => {
-        commit('init', result.data)
+        commit('init', result.data);
 
         return result;
       });
   },
 
-  remove ({ commit }, notification) {
+  remove({commit}, notification) {
     return axios.delete(`/User/Notifications/Delete/${notification.id}`).then(() => commit('remove', notification));
   },
 
-  markAll({ commit, state }) {
+  markAll({commit, state}) {
     return axios.post('/User/Notifications/Mark').then(() => {
       state.notifications.forEach(notification => commit('mark', notification));
     });
-  }
+  },
 };
 
 export default {
@@ -72,5 +72,5 @@ export default {
   state,
   getters,
   mutations,
-  actions
+  actions,
 };
