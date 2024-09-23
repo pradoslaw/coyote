ff9af48597e5562b0aa22d5f3444f688b1cb2fd4
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2024-09-23 11:53:29 +0200

    Inline <vue-prompt/> in <vue-comment-autocomplete/> to avoid accessing internals of <slot/>

--- resources/js/components/CommentAutocomplete.vue
+++ resources/js/components/CommentAutocomplete.vue
@@ -1,5 +1,5 @@
 <template>
-  <vue-prompt :source="source">
+  <div class="position-relative">
     <textarea
       v-autosize
       class="form-control"
@@ -16,18 +16,28 @@
       @keydown.meta.enter="save"
       @keydown.esc="cancel"
 
+      @keyup="onKeyUp"
+      @click="onKeyUp"
+      @keydown="onKeyDown"
+
       ref="textarea"
     />
     <slot/>
-  </vue-prompt>
+    <vue-dropdown ref="dropdown" :items="items" @select="selectItem"/>
+  </div>
 </template>
 
 <script lang="ts">
+import useBrackets from "../libs/prompt";
+import Textarea from "../libs/textarea";
 import {autosizeDirective} from "../plugins/autosize.js";
 import {pasteDirective} from "../plugins/paste.js";
-import VuePrompt from "./forms/prompt.vue";
+import store from "../store/index";
+import {SpecialKeys} from "../types/keys";
+import VueDropdown from "./forms/dropdown.vue";
 
 export default {
+  store,
   props: {
     value: {type: String},
     source: {type: String, required: true},
@@ -37,30 +47,145 @@ export default {
     allowPaste: {type: Boolean, default: false},
   },
   components: {
-    'vue-prompt': VuePrompt,
+    'vue-dropdown': VueDropdown,
   },
   directives: {
     paste: pasteDirective('/assets'),
     autosize: autosizeDirective,
   },
+  data() {
+    return {
+      items: [],
+    };
+  },
+  computed: {
+    isDropdownVisible() {
+      return this.$refs.dropdown?.isDropdownVisible;
+    },
+  },
   methods: {
-    input(event) {
+    input(event: InputEvent): void {
       this.$emit('input', event.target.value);
     },
-    focus() {
+    focus(): void {
       this.$refs.textarea.focus();
     },
-    save() {
+    save(): void {
       this.$emit('save');
     },
-    cancel() {
+    cancel(): void {
       this.$emit('cancel');
     },
-    paste(event) {
+    paste(event): void {
       this.$emit('paste', event);
     },
     inspect(inspector: (element: HTMLTextAreaElement) => void): void {
       inspector(this.$refs.textarea);
+    },
+
+    onKeyUp(event: KeyboardEvent) {
+      const caretPosition = this.getCaretPosition();
+      const startIndex = this.getUserNamePosition(caretPosition);
+
+      if (this.isDropdownVisible) {
+        if (event.keyCode === SpecialKeys.ESC) {
+          this.items = [];
+          return;
+        }
+        if (event.keyCode === SpecialKeys.DOWN) {
+          this.$refs.dropdown.goDown();
+          return;
+        }
+        if (event.keyCode === SpecialKeys.UP) {
+          this.$refs.dropdown.goUp();
+          return;
+        }
+        if (event.keyCode === SpecialKeys.ENTER || event.keyCode === SpecialKeys.TAB) {
+          const item = this.$refs.dropdown.getSelected();
+          if (item) {
+            this.applySelected(item.name, startIndex, caretPosition);
+          }
+          return; // item was selected so there is not point to look up for user name.
+        }
+      }
+
+      // min length to search is 1 character
+      if (startIndex > -1 && caretPosition - startIndex >= 1) {
+        this.lookupName(this.$refs.textarea.value.substring(startIndex, caretPosition));
+        return;
+      }
+
+      if (this.items?.length) {
+        store.commit('prompt/cancel');
+        this.items = [];
+      }
+    },
+
+    selectItem(item: object): void {
+      const caretPosition = this.getCaretPosition();
+      const startIndex = this.getUserNamePosition(caretPosition);
+      this.applySelected(item.name, startIndex, caretPosition);
+    },
+
+    onKeyDown(event: KeyboardEvent): void {
+      if (this.isDropdownVisible && [SpecialKeys.ENTER, SpecialKeys.TAB, SpecialKeys.DOWN, SpecialKeys.UP, SpecialKeys.ESC].indexOf(event.keyCode) !== -1) {
+        event.preventDefault();
+      }
+    },
+
+    getCaretPosition(): number {
+      return this.$refs.textarea.selectionStart;
+    },
+
+    getUserNamePosition(caretPosition: number) {
+      let i = caretPosition;
+      let result = -1;
+
+      while (i > caretPosition - 50 && i > 0) {
+        let char = this.$refs.textarea.value[i - 1];
+
+        if (char === ' ' || char === "\n") {
+          break;
+        }
+        // we must check if @ is not a part of email address
+        else if (char === '@' && (i === 1 || (this.$refs.textarea.value[i - 2] === ' ' || this.$refs.textarea.value[i - 2] === "\n"))) {
+          result = i;
+          break;
+        }
+
+        i--;
+      }
+
+      return result;
+    },
+
+    lookupName(name: string): void {
+      store.dispatch('prompt/request', {source: this.source, value: name}).then(items => {
+        this.items = items;
+
+        const rect = new Textarea(this.$refs.textarea).getCaretCoordinates();
+
+        this.$refs['dropdown'].$el.style.top = rect.top + 'px';
+        this.$refs['dropdown'].$el.style.left = rect.left + 'px';
+      });
+    },
+
+    applySelected(text: string, startIndex: number, caretPosition: number): void {
+      if (!text.length) {
+        this.items = [];
+        return;
+      }
+
+      const append = useBrackets(text) + (startIndex === 1 ? ': ' : ' '); // add space at the end
+
+      this.$refs.textarea.value = this.$refs.textarea.value.substr(0, startIndex) + append + this.$refs.textarea.value.substring(caretPosition);
+      this.$refs.textarea.focus(); // when user clicks the item, we must restore focus on input
+      this.$refs.textarea.dispatchEvent(new Event('input', {'bubbles': true}));
+      this.items = []; // setting to empty array will trigger dropdown watcher
+
+      let caret = startIndex + append.length;
+
+      this.$refs.textarea.setSelectionRange(caret, caret);
     },
   },
 };
