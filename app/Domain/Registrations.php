b8247583cdbf74fb4f6a67a9045971bf123ce2bb
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2024-09-30 14:11:49 +0200

    Truncate datetime to date using sql query, instead of php strStr()

--- app/Domain/Registrations.php
+++ app/Domain/Registrations.php
@@ -14,21 +14,14 @@ class Registrations
             ->where('created_at', '<=', "$to 24:00:00");
 
         $collection = $query
-            ->selectRaw("date_trunc('week', created_at) as created_at_week, Count(*) AS count")
+            ->selectRaw("date_trunc('week', created_at)::date as created_at_week, Count(*) AS count")
             ->groupByRaw("date_trunc('week', created_at)")
             ->get(['created_at_week', 'total']);
 
         $array = $collection->toArray();
         return \array_combine(
-            \array_map(
-                $this->datetimeToDate(...),
-                \array_column($array, 'created_at_week')),
+            \array_column($array, 'created_at_week'),
             \array_column($array, 'count'),
         );
-    }
-
-    private function datetimeToDate(string $datetime): string
-    {
-        return \strStr($datetime, ' ', before_needle:true);
     }
 }
