ffd1556387085cda7136d9fac565c229826b5e3b
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2024-10-02 15:33:12 +0200

    Add Viewers.coalesceUser(int) and .coalesceGuest()

--- app/Domain/Online/Viewers.php
+++ app/Domain/Online/Viewers.php
@@ -6,4 +6,25 @@ readonly class Viewers
     public function __construct(public array $users, public int $guestsCount)
     {
     }
+
+    public function coalesceUser(int $userId): self
+    {
+        if (\in_array($userId, $this->users)) {
+            return $this;
+        }
+        return $this->mapUsers([...$this->users, $userId]);
+    }
+
+    public function coalesceGuest(): self
+    {
+        if ($this->guestsCount === 0) {
+            return new self($this->users, 1);
+        }
+        return $this;
+    }
+
+    private function mapUsers(array $users): self
+    {
+        return new self($users, $this->guestsCount);
+    }
 }
