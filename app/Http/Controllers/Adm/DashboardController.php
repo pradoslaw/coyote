9a4eb168bcc4c337c1bf9af1963550dfc2e889c4
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2024-10-17 14:16:19 +0200

    Include last visit chart in administrator panel

--- app/Http/Controllers/Adm/DashboardController.php
+++ app/Http/Controllers/Adm/DashboardController.php
@@ -32,24 +32,33 @@ class DashboardController extends BaseController
                 ],
             ],
 
-            'registrationsChartWeeks'  => $this->historyChartHtml($registrations, Period::Week),
-            'registrationsChartMonths' => $this->historyChartHtml($registrations, Period::Month),
-            'registrationsChartYears'  => $this->historyChartHtml($registrations, Period::Year),
+            'registrationsChartWeeks'  => $this->historyChartHtml($registrations, Period::Week, 'created_at'),
+            'registrationsChartMonths' => $this->historyChartHtml($registrations, Period::Month, 'created_at'),
+            'registrationsChartYears'  => $this->historyChartHtml($registrations, Period::Year, 'created_at'),
+
+            'leaveChartWeeks'  => $this->historyChartHtml($registrations, Period::Week, 'visited_at'),
+            'leaveChartMonths' => $this->historyChartHtml($registrations, Period::Month, 'visited_at'),
+            'leaveChartYears'  => $this->historyChartHtml($registrations, Period::Year, 'visited_at'),
         ]);
     }
 
-    private function historyChartHtml(UserRegistrations $registrations, Period $period): StringHtml
+    private function historyChartHtml(UserRegistrations $registrations, Period $period, string $column): StringHtml
     {
         return new StringHtml($this->view('adm.registrations-chart', [
-            'chart'              => $this->registrationsChart($registrations, $period),
+            'chart'              => $this->registrationsChart($registrations, $period, $column),
             'chartLibrarySource' => Chart::librarySourceHtml(),
+            'title'              => $column === 'created_at' ? 'Historia rejestracji' : 'Ostatnia wizyta użytkownika',
         ]));
     }
 
-    private function registrationsChart(UserRegistrations $registrations, Period $period): Chart
+    private function registrationsChart(UserRegistrations $registrations, Period $period, string $column): Chart
     {
         $range = new HistoryRange($this->dateNow(), $period, 30);
-        return $this->chart($period->name, $registrations->inRange($range));
+        return $this->chart(
+            "$period->name.$column",
+            $registrations->inRangeColumn($range, $column),
+            $column === 'created_at',
+        );
     }
 
     private function dateNow(): string
@@ -57,12 +66,12 @@ class DashboardController extends BaseController
         return Carbon::now()->toDateString();
     }
 
-    private function chart(string $chartId, array $registeredUsers): Chart
+    private function chart(string $chartId, array $registeredUsers, bool $positive): Chart
     {
         return new Chart(
             \array_keys($registeredUsers),
             \array_values($registeredUsers),
-            ['#ff9f40'],
+            [$positive ? '#ff9f40' : '#ff0000'],
             "registration-history-chart-$chartId",
         );
     }
