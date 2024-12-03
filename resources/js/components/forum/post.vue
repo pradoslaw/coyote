89f412ae0ff86930dfa41fdfa0fd26387a94560c
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2024-12-03 12:33:57 +0100

    Hide fold button for blocked users or deleted posts

--- resources/js/components/forum/post.vue
+++ resources/js/components/forum/post.vue
@@ -34,7 +34,7 @@
       <div class="row">
         <div class="col-2">
           <h5 class="mb-0 post-author">
-            <span @click="postFold" class="d-inline-block me-1 cursor-pointer text-muted" style="vertical-align:middle;">
+            <span @click="postFold" class="d-inline-block me-1 cursor-pointer text-muted" style="vertical-align:middle;" v-if="!authorBlocked && !post.deleted_at">
               <vue-icon name="postFold"/>
             </span>
             <vue-username v-if="post.user" :user="post.user" :owner="post.user_id === topic.owner_id"/>
