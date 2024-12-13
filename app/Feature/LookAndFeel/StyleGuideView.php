bcc08dae813dd485efcb76db261d078f5167a3b4
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2024-12-13 14:32:56 +0100

    Update LookAndFeel/StyleGuideView to access css variables

--- app/Feature/LookAndFeel/StyleGuideView.php
+++ app/Feature/LookAndFeel/StyleGuideView.php
@@ -8,6 +8,40 @@ class StyleGuideView
     public function view(array $colorGroups): string
     {
         return Blade::render('
+            <link rel="stylesheet" href="{{ cdn(\'css/stylesEager.css\') }}">
+            <script>
+                variableNames();
+                function variableNames() {
+                  const variableNames = Array.from(document.styleSheets)
+                    .filter(sheet => ownStyle(sheet))
+                    .reduce((acc, sheet) => [...acc, ...sheetCssVariables(sheet)], []);
+                  const unique = [...new Set(variableNames)];
+                  unique.sort();
+                  return unique;
+                }
+                function ownStyle(sheet) {
+                  return sheet.href === null || sheet.href.startsWith(window.location.origin);
+                }
+                function sheetCssVariables(sheet) {
+                  return Array.from(sheet.cssRules).reduce((acc, rule) => ruleCssVariables(rule, acc), []);
+                }
+                function ruleCssVariables(rule, acc) {
+                  if (rule.style) {
+                    if (rule.cssText.startsWith(\'@font-face\')) {
+                    } else {
+                      if (rule.selectorText.includes(\'look-and-feel\')) {
+                        return [...acc, ...ruleStyleCssVariables(rule)];
+                      }
+                    }
+                  }
+                  return acc;
+                }
+                function ruleStyleCssVariables(rule) {
+                  return Array.from(rule.style)
+                    .filter(name => name.startsWith("--"))
+                    .map(name => name.substr(2));
+                }
+            </script>
             <style>
               body { background: #e0e0e0; }
               .color-preview {
