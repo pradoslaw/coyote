451b17b599b8a591c2e4ba9103004c83e5b9fff9
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2024-12-17 15:57:25 +0100

    Clean PostCollection

--- app/Http/Resources/PostCollection.php
+++ app/Http/Resources/PostCollection.php
@@ -1,72 +1,43 @@
 <?php
-
 namespace Coyote\Http\Resources;
 
 use Coyote\Forum;
 use Coyote\Post;
 use Coyote\Services\Forum\Tracker;
 use Coyote\Topic;
+use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\ResourceCollection;
 
 class PostCollection extends ResourceCollection
 {
     /**
-     * @var Tracker
-     */
-    protected $tracker;
-
-    /**
-     * @var Topic
-     */
-    protected $topic;
-
-    /**
-     * @var Forum
-     */
-    protected $forum;
-
-    /**
      * DO NOT REMOVE! This will preserver keys from being filtered in data
-     *
-     * @var bool
      */
-    protected $preserveKeys = true;
+    protected bool $preserveKeys = true;
 
-    /**
-     * @param Tracker $tracker
-     * @return $this
-     */
-    public function setTracker(Tracker $tracker)
+    private Tracker $tracker;
+    private Topic $topic;
+    private Forum $forum;
+
+    public function setTracker(Tracker $tracker): self
     {
         $this->tracker = $tracker;
-
         return $this;
     }
 
-    /**
-     * @param Topic $topic
-     * @param Forum $forum
-     * @return $this
-     */
-    public function setRelations(Topic $topic, Forum $forum)
+    public function setRelations(Topic $topic, Forum $forum): self
     {
         $this->topic = $topic;
         $this->forum = $forum;
-
         return $this;
     }
 
-    /**
-     * Transform the resource collection into an array.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array
-     */
-    public function toArray($request)
+    public function toArray(Request $request): array
     {
         $collection = $this
             ->collection
             ->map(function (PostResource $resource) use ($request) {
+                /** @var Post $post */
                 $post = $resource->resource;
 
                 // set relations to avoid N+1 SQL loading. be aware we must use setRelation() method because setRelations() overwrites all already
