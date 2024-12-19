05df8c049f4df99a28947923126823110009aa97
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2024-12-19 15:12:47 +0100

    Clean PostResource

--- app/Http/Resources/PostResource.php
+++ app/Http/Resources/PostResource.php
@@ -73,52 +73,45 @@ class PostResource extends JsonResource
 
     private function postResourceToArray(Request $request): array
     {
+        /** @var Post $post */
+        $post = $this->resource;
+
         $this->applyCommentsRelation();
 
-        $only = $this->resource->only(['id', 'user_name', 'score', 'text', 'edit_count', 'is_voted', 'is_accepted', 'is_subscribed', 'user_id', 'deleter_name', 'delete_reason']);
+        $only = $post->only(['id', 'user_name', 'score', 'text', 'edit_count', 'is_voted', 'is_accepted', 'is_subscribed', 'user_id', 'deleter_name', 'delete_reason']);
         $html = $this->text !== null ? $this->html : null;
 
-        $commentsCount = count($this->resource->comments);
+        $commentsCount = count($post->comments);
         // show all comments if parameter "p" is present. it means that user wants to be redirected
         // to specific post and probably wants to see all comments (or specific comment)
-        $comments = $request->get('p') == $this->id ? $this->resource->comments : $this->resource->comments->slice(-5, null, true);
+        $comments = $request->get('p') == $this->id ? $post->comments : $post->comments->slice(-5, null, true);
 
         return array_merge($only, [
-            'created_at' => $this->created_at->toIso8601String(),
-            'updated_at' => $this->updated_at?->toIso8601String(),
-            'deleted_at' => $this->deleted_at ? Carbon::parse($this->deleted_at)->toIso8601String() : null,
-            'user'       => UserResource::make($this->user),
-            'html'       => $html,
-            'url'        => UrlBuilder::post($this->resource, true),
-            'is_locked'  => $this->topic->is_locked || $this->forum->is_locked,
-
-            $this->mergeWhen($this->tracker !== null, function () {
-                return ['is_read' => $this->tracker->getMarkTime() >= $this->created_at];
-            }),
-
-            // only for moderators
-            $this->mergeWhen($this->gate->allows('delete', $this->forum), [
-                'ip'      => $this->ip,
-                'browser' => $this->browser,
+            'created_at'     => $this->created_at->toIso8601String(),
+            'updated_at'     => $this->updated_at?->toIso8601String(),
+            'deleted_at'     => $this->deleted_at ? Carbon::parse($this->deleted_at)->toIso8601String() : null,
+            'user'           => UserResource::make($this->user),
+            'html'           => $html,
+            'url'            => UrlBuilder::post($post, true),
+            'is_locked'      => $this->topic->is_locked || $this->forum->is_locked,
+            $this->mergeWhen($this->tracker !== null, fn() => [
+                'is_read' => $this->tracker->getMarkTime() >= $this->created_at,
             ]),
-
-            $this->mergeWhen($this->editor !== null, function () {
-                return ['editor' => UserResource::make($this->editor)];
-            }),
-
-            'permissions' => [
+            $this->mergeWhen($this->editor !== null, fn() => [
+                'editor' => UserResource::make($this->editor),
+            ]),
+            'permissions'    => [
                 'write'      => $this->gate->allows('write', $this->topic) && $this->gate->allows('write', $this->forum),
-                'delete'     => $this->gate->allows('delete', $this->resource) || $this->gate->allows('delete', $this->forum),
-                'update'     => $this->gate->allows('update', $this->resource),
+                'delete'     => $this->gate->allows('delete', $post) || $this->gate->allows('delete', $this->forum),
+                'update'     => $this->gate->allows('update', $post),
                 'merge'      => $this->gate->allows('merge', $this->forum),
                 'sticky'     => $this->gate->allows('sticky', $this->forum),
                 'adm_access' => $this->gate->allows('adm-access'),
-                'accept'     => $this->gate->allows('accept', $this->resource),
+                'accept'     => $this->gate->allows('accept', $post),
             ],
-
             'comments'       => PostCommentResource::collection($comments)->keyBy('id'),
             'comments_count' => $commentsCount,
-            'assets'         => AssetsResource::collection($this->resource->assets),
+            'assets'         => AssetsResource::collection($post->assets),
             'metadata'       => encrypt([
                 Post::class  => $this->id,
                 Topic::class => $this->topic_id,
@@ -126,8 +119,7 @@ class PostResource extends JsonResource
             ]),
             'has_review'     => false,
             'review_style'   => 'info',
-
-            'parentPostId'   => $this->tree_parent_post_id,
+            'parentPostId'   => $post->tree_parent_post_id,
             'childrenFolded' => false,
             'type'           => 'regular',
         ]);
--- tests/Legacy/Resources/PostResourceTest.php
+++ tests/Legacy/Resources/PostResourceTest.php
@@ -1,91 +1,50 @@
 <?php
-
 namespace Tests\Legacy\Resources;
 
 use Coyote\Forum;
 use Coyote\Http\Resources\PostResource;
-use Coyote\Post;
 use Coyote\Repositories\Contracts\TopicRepositoryInterface;
 use Coyote\Services\Forum\Tracker;
 use Coyote\Services\Guest;
 use Coyote\Topic;
-use Coyote\User;
-use Faker\Factory;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Gate;
+use PHPUnit\Framework\Attributes\Before;
+use PHPUnit\Framework\Attributes\Test;
 use Tests\Legacy\TestCase;
 
 class PostResourceTest extends TestCase
 {
     use DatabaseTransactions;
 
-    /**
-     * @var \Faker\Generator
-     */
-    protected $faker;
+    private string $uuid;
 
-    public function setUp(): void
+    #[Before]
+    public function initialize(): void
     {
-        parent::setUp();
-
-        $this->faker = Factory::create();
+        $this->uuid = 'b4b94950-11bc-3ab5-bf5b-17d7df81703a';
         PostResource::withoutWrapping();
     }
 
+    #[Test]
     public function testPostUnreadForNewUser()
     {
         $forum = factory(Forum::class)->create();
         $topic = factory(Topic::class)->create(['forum_id' => $forum->id]);
-        $topic->refresh();
 
-        $guest = new Guest($this->faker->uuid);
+        $guest = new Guest($this->uuid);
         $guest->setDefaultSessionTime(now()->subMinutes(5)); // simulate session start
 
         $tracker = new Tracker($topic, $guest);
         $tracker->setRepository($this->app[TopicRepositoryInterface::class]);
 
-        $post = (new PostResource($topic->firstPost))->setTracker($tracker)->toResponse(request())->getData(true);
+        $resource = (new PostResource($topic->firstPost))->setTracker($tracker)->toResponse(request())->getData(true);
 
-        $this->assertFalse($post['is_read']);
+        $this->assertFalse($resource['is_read']);
 
         $tracker->asRead($topic->last_post_created_at);
 
-        $post = (new PostResource($topic->firstPost))->setTracker($tracker)->toResponse(request())->getData(true);
+        $resource = (new PostResource($topic->firstPost))->setTracker($tracker)->toResponse(request())->getData(true);
 
-        $this->assertTrue($post['is_read']);
-    }
-
-    public function testShowIpOnlyForModerators()
-    {
-        $user = factory(User::class)->make(['id' => $this->faker->numberBetween()]);
-
-        Auth::setUser($user);
-
-        Gate::define('forum-delete', function () {
-            return true;
-        });
-
-        $forum = factory(Forum::class)->make();
-        $topic = factory(Topic::class)->state('id')->make();
-
-        $topic->forum()->associate($forum);
-
-        $post = factory(Post::class)->make();
-
-        $post->topic()->associate($topic);
-        $post->forum()->associate($forum);
-
-        $guest = new Guest($this->faker->uuid);
-
-        /** @var Request $request */
-        $request = $this->app['request'];
-
-        $resource = (new PostResource($post))->setTracker(new Tracker($topic, $guest));
-        $result = $resource->resolve($request);
-
-        $this->assertArrayHasKey('ip', $result);
-        $this->assertArrayHasKey('browser', $result);
+        $this->assertTrue($resource['is_read']);
     }
 }
