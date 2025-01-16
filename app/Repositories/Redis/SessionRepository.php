210c3628c1e69c843f469836dc21934def4a90f8
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2025-01-16 15:19:59 +0100

    Clean SessionRepository

--- app/Repositories/Redis/SessionRepository.php
+++ app/Repositories/Redis/SessionRepository.php
@@ -2,102 +2,51 @@
 namespace Coyote\Repositories\Redis;
 
 use Coyote\Session;
-use Illuminate\Container\Container as App;
+use Illuminate\Container\Container;
+use Illuminate\Support;
 
 class SessionRepository
 {
-    const REDIS_KEY = 'sessions';
-
-    /**
-     * @var App
-     */
-    protected $app;
+    private const string REDIS_KEY = 'sessions';
 
     /**
      * @var mixed
      */
-    protected $redis;
+    private $redis;
 
-    /**
-     * @param App $app
-     */
-    public function __construct(App $app)
+    public function __construct(Container $app)
     {
-        $this->app = $app;
-
-        $this->makeRedis();
+        $this->redis = $app['redis'];
     }
 
-    /**
-     * @inheritdoc
-     */
     public function set(string $sessionId, array $payload)
     {
-        return $this->redis->hset(self::REDIS_KEY, $sessionId, serialize($payload));
+        return $this->redis->hset(self::REDIS_KEY, $sessionId, \serialize($payload));
     }
 
-    /**
-     * @inheritdoc
-     */
     public function get(string $sessionId)
     {
         return $this->redis->hget(self::REDIS_KEY, $sessionId);
     }
 
-    /**
-     * @inheritdoc
-     */
-    public function destroy(string $sessionId)
+    public function destroy(string $sessionId): void
     {
-        return $this->redis->hdel(self::REDIS_KEY, $sessionId);
+        $this->redis->hdel(self::REDIS_KEY, $sessionId);
     }
 
-    /**
-     * @inheritdoc
-     */
-    public function all()
+    public function all(): Support\Collection
     {
-        $result = $this->unserialize($this->redis->hvals(self::REDIS_KEY));
-
-        return $result->map(function ($item) {
-            return $this->makeModel($item);
-        });
+        return collect($this->redis->hvals(self::REDIS_KEY))
+            ->map(fn($item) => new Session(\unserialize($item)));
     }
 
-    /**
-     * @inheritdoc
-     */
-    public function gc(int $lifetime)
+    public function gc(int $lifetime): true
     {
         foreach ($this->all() as $item) {
             if ($item->expired($lifetime)) {
                 $this->redis->hdel(self::REDIS_KEY, $item['id']);
             }
         }
-
         return true;
-    }
-
-    /**
-     * @param array $item
-     * @return Session
-     */
-    protected function makeModel(array $item): Session
-    {
-        return new Session($item);
-    }
-
-    protected function makeRedis()
-    {
-        $this->redis = $this->app['redis'];
-    }
-
-    /**
-     * @param array $rowset
-     * @return \Illuminate\Support\Collection
-     */
-    private function unserialize(array $rowset)
-    {
-        return collect(array_map('unserialize', $rowset));
     }
 }
