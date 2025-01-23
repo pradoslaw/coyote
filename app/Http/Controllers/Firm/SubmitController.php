4c8d7a45695741ef262ac17316ba22515e1f6bbb
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2025-01-23 15:53:49 +0100

    Correct uploading job board firm logo

--- app/Http/Controllers/Firm/SubmitController.php
+++ app/Http/Controllers/Firm/SubmitController.php
@@ -1,30 +1,23 @@
 <?php
-
 namespace Coyote\Http\Controllers\Firm;
 
 use Coyote\Http\Controllers\Controller;
 use Coyote\Http\Factories\MediaFactory;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class SubmitController extends Controller
 {
     use MediaFactory;
 
-    /**
-     * @param Request $request
-     * @return \Illuminate\Http\JsonResponse
-     * @throws \Illuminate\Validation\ValidationException
-     */
-    public function logo(Request $request)
+    public function logo(Request $request): JsonResponse
     {
         $this->validate($request, [
-            'logo'             => 'required|mimes:jpeg,jpg,png,gif'
+            'logo' => 'required|mimes:jpeg,jpg,png,gif',
         ]);
-
         $media = $this->getMediaFactory()->make('logo')->upload($request->file('logo'));
-
         return response()->json([
-            'url'       => (string) $media->url()
+            'url' => url((string)$media->url()),
         ]);
     }
 }
--- app/Http/Resources/FirmFormResource.php
+++ app/Http/Resources/FirmFormResource.php
@@ -1,25 +1,24 @@
 <?php
-
 namespace Coyote\Http\Resources;
 
 use Coyote\Firm;
+use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class FirmFormResource extends JsonResource
 {
-    /**
-     * @param \Illuminate\Http\Request $request
-     * @return array
-     * @throws \Exception
-     */
-    public function toArray($request)
+    public function toArray(Request $request): array
     {
         $data = array_except(parent::toArray($request), ['benefits']);
-
-        return array_merge($data, [
-            'benefits'      => $this->resource->benefits->pluck('name')->toArray(),
-            'assets'        => AssetsResource::collection($this->resource->assets),
-            'logo'          => (string) $this->resource->logo->url()
-        ]);
+        /** @var Firm $firm */
+        $firm = $this->resource;
+        return [
+            ...$data,
+            'benefits' => $firm->benefits->pluck('name')->toArray(),
+            'assets'   => AssetsResource::collection($firm->assets),
+            'logo'     => $firm->logo->getFilename() === null
+                ? ''
+                : url((string)$firm->logo->url()),
+        ];
     }
 }
