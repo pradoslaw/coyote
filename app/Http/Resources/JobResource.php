d7d9f297d0cecd9a6dc1540f17adb453e844de22
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2025-01-27 12:18:18 +0100

    Clean JobResource

--- app/Http/Resources/JobResource.php
+++ app/Http/Resources/JobResource.php
@@ -1,13 +1,16 @@
 <?php
-
 namespace Coyote\Http\Resources;
 
 use Carbon\Carbon;
 use Coyote\Currency;
+use Coyote\Firm;
 use Coyote\Http\Factories\MediaFactory;
 use Coyote\Job;
+use Coyote\Job\Feature;
 use Coyote\Services\UrlBuilder;
 use Coyote\Tag;
+use Illuminate\Database\Eloquent;
+use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 /**
@@ -16,8 +19,8 @@ use Illuminate\Http\Resources\Json\JsonResource;
  * @property Carbon $boost_at
  * @property Carbon $created_at
  * @property Job\Location[] $locations
- * @property Tag[] $tags
- * @property \Coyote\Firm $firm
+ * @property Tag[]|Eloquent\Collection $tags
+ * @property Firm $firm
  * @property float $salary_from
  * @property float $salary_to
  * @property Currency $currency
@@ -28,54 +31,43 @@ use Illuminate\Http\Resources\Json\JsonResource;
  * @property int $remote_range
  * @property string $description
  * @property string $recruitment
- * @property \Coyote\Job\Feature[] $features
+ * @property Feature[] $features
  */
 class JobResource extends JsonResource
 {
     use MediaFactory;
 
-    /**
-     * Transform the resource into an array.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array
-     */
-    public function toArray($request)
+    public function toArray(Request $request): array
     {
         $only = $this->resource->only('id', 'title', 'is_remote', 'remote_range', 'score', 'subscribe_on', 'comments_count', 'is_highlight', 'is_on_top', 'is_publish', 'is_gross');
-
-        return array_merge($only, [
-            'url'         => UrlBuilder::job($this->resource, true),
-            'created_at'  => format_date($this->created_at, false),
-            'boost_at'    => format_date($this->boost_at),
-            'is_new'      => carbon($this->boost_at)->diffInDays(Carbon::now()) <= 2,
-            'salary_from' => $this->money($this->salary_from),
-            'salary_to'   => $this->money($this->salary_to),
-            'rate_label'  => Job::getRatesList()[$this->rate] ?? null,
-            'locations'   => LocationResource::collection($this->locations),
-            'tags'        => TagResource::collection($this->tags->sortByDesc('pivot.priority')),
-            'is_medal'    => $this->score >= 150,
-            'currency'      => $this->currency->name,
+        return [
+            ...$only,
+            'url'             => UrlBuilder::job($this->resource, true),
+            'created_at'      => format_date($this->created_at, false),
+            'boost_at'        => format_date($this->boost_at),
+            'is_new'          => carbon($this->boost_at)->diffInDays(Carbon::now()) <= 2,
+            'salary_from'     => $this->money($this->salary_from),
+            'salary_to'       => $this->money($this->salary_to),
+            'rate_label'      => Job::getRatesList()[$this->rate] ?? null,
+            'locations'       => LocationResource::collection($this->locations),
+            'tags'            => TagResource::collection($this->tags->sortByDesc('pivot.priority')),
+            'is_medal'        => $this->score >= 150,
+            'currency'        => $this->currency->name,
             'currency_symbol' => $this->currency->symbol,
-            'remote'      => [
-                'range'         => $this->remote_range,
-                'enabled'       => $this->is_remote,
-                'url'           => route('job.remote')
+            'remote'          => [
+                'range'   => $this->remote_range,
+                'enabled' => $this->is_remote,
+                'url'     => route('job.remote'),
             ],
-            'text'        => $this->description,
-            'recruitment' => $this->recruitment,
-            'firm'        => $this->when($this->firm->exists, new FirmResource($this->firm), (object) ['logo' => '', 'name' => '']),
-
-            'features'    => $this->whenLoaded('features', function () {
+            'text'            => $this->description,
+            'recruitment'     => $this->recruitment,
+            'firm'            => $this->when($this->firm->exists, new FirmResource($this->firm), (object)['logo' => '', 'name' => '']),
+            'features'        => $this->whenLoaded('features', function () {
                 return FeatureResource::collection($this->features);
-            })
-        ]);
+            }),
+        ];
     }
 
-    /**
-     * @param float|null $number
-     * @return string|null
-     */
     private function money(?float $number): ?float
     {
         return $number ? $this->resource->monthlySalary($number) : null;
